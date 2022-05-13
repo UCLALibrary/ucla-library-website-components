d42a91c336f1084e1d94da7f9f0887829c4767e6
Axa Mei Liauw <aliauw@library.ucla.edu>
2022-05-13 08:54:48 -0400

    fix: imports global styles into each component

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,3 +1,5 @@
+import "@/styles/variables-css.scss";
+
 /* eslint-disable import/prefer-default-export */
 export { default as UclaLibraryWebsiteComponentSample } from './ucla-library-website-component-sample.vue';
 export { default as DividerGeneral } from './DividerGeneral.vue';
