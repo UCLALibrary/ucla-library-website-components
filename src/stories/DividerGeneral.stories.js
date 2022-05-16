2b71e82264b283b4bcddfb39f25354537dd2610c
pghorpade <pghorpade@library.ucla.edu>
2022-05-16 12:20:02 -0700

    feat: Install storybook and add DividerGeneral stories

---
+++ src/stories/DividerGeneral.stories.js
@@ -0,0 +1,16 @@
+import DividerGeneral from "../lib-components/DividerGeneral";
+
+export default {
+    title: "DIVIDER / General",
+    component: DividerGeneral,
+}
+
+export const Default = () => ({
+    components: { DividerGeneral },
+    template: `<divider-general/>`,
+})
+
+export const isBold = () => ({
+    components: { DividerGeneral },
+    template: `<divider-general :is-bold="true"/>`,
+})
