811ba7cb6c2a844b2e0eb2619905a2a66a1f152b
Matheus Gomes <matheusgomes062@gmail.com>
2022-06-01 13:04:37 -0300

    feat: indexed the import

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -2,4 +2,5 @@ import "@/styles/global.scss"
 import "@/styles/variables-css.scss"
 
 export { default as UclaLibraryWebsiteComponentSample } from "./ucla-library-website-component-sample.vue"
+export { default as AlphabeticalBrowseBy } from "./AlphabeticalBrowseBy.vue"
 export { default as DividerGeneral } from "./DividerGeneral.vue"
--- src/stories/AlphabeticalBrowseBy.spec.js
+++ src/stories/AlphabeticalBrowseBy.spec.js
@@ -7,4 +7,28 @@ describe("GENERAL / AlphabeticalBrowseBy", () => {
 
         cy.percySnapshot("GENERAL / AlphabeticalBrowseBy: Default")
     })
+
+    // it("Desktop version should show only one row", () => {
+    //     cy.viewport(1920, 1080)
+    //     cy.visit(
+    //         "/iframe.html?id=alphabetical-browse-by--desktop&args=&viewMode=story"
+    //     )
+    //     cy.get(".alphabetical-browse-by").should("exist")
+    // })
+
+    // it("Tablet version should show only two rows", () => {
+    //     cy.viewport(768, 1024)
+    //     cy.visit(
+    //         "/iframe.html?id=alphabetical-browse-by--desktop&args=&viewMode=story"
+    //     )
+    //     cy.get(".alphabetical-browse-by").should("exist")
+    // })
+
+    // it("Smartphone version should show only three rows", () => {
+    //     cy.viewport(360, 740)
+    //     cy.visit(
+    //         "/iframe.html?id=alphabetical-browse-by--desktop&args=&viewMode=story"
+    //     )
+    //     cy.get(".alphabetical-browse-by").should("exist")
+    // })
 })
