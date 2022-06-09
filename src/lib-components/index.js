1043f2cce3c97635d5afb31335fcc71a54ded214
Matheus Gomes <matheusgomes062@gmail.com>
2022-06-09 16:50:43 -0300

    feat: buttonLink component added and started mixins

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -2,3 +2,4 @@ import "@/styles/variables-css.scss"
 
 export { default as UclaLibraryWebsiteComponentSample } from "./ucla-library-website-component-sample.vue"
 export { default as DividerGeneral } from "./DividerGeneral.vue"
+export { default as ButtonLink } from "./ButtonLink.vue"
