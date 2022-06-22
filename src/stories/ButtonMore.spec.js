85177b5c2b1446c64fe67ea727a4aaa5ad08e5b2
Matheus Gomes <matheusgomes062@gmail.com>
2022-06-22 13:55:06 -0300

    feat: added button more

---
+++ src/stories/ButtonMore.spec.js
@@ -0,0 +1,8 @@
+describe("BUTTON / More", () => {
+    it("Default", () => {
+        cy.visit("/iframe.html?id=button-more--default&args=&viewMode=story")
+        cy.get(".button-more").should("exist")
+
+        cy.percySnapshot("BUTTON / More: Default")
+    })
+})
