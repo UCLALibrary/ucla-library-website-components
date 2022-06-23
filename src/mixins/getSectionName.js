acfef059c1cb6fde222951e5282bb660acfad2e1
Matheus Gomes <matheusgomes062@gmail.com>
2022-06-23 17:14:04 -0300

    feat: added blockCardWithIllustration

---
+++ src/mixins/getSectionName.js
@@ -0,0 +1,31 @@
+export default {
+    /**
+     * Take a URI and figure out what "section" of the site it is pointing too
+     * @param {String} uri
+     * @returns {String}
+     */
+
+    methods: {
+        getSectionName(uri = "") {
+            let output = "default"
+
+            switch (true) {
+                case uri.includes("/help"):
+                    output = "help"
+                    break
+
+                case uri.includes("/about"):
+                case uri.includes("/impact"):
+                    output = "about"
+                    break
+
+                case uri.includes("/visit"):
+                case uri.includes("/events-exhibits"):
+                    output = "visit"
+                    break
+            }
+
+            return output
+        },
+    },
+}
