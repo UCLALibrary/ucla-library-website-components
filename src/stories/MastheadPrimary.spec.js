c8a851e5c0dceb9047a7a6548f714f94af9386fe
jendiamond <thejendiamond@gmail.com>
2022-06-28 15:32:57 -0700

    feat: ccopy all files

---
+++ src/stories/MastheadPrimary.spec.js
@@ -0,0 +1,10 @@
+describe("MASTHEAD / Primary", () => {
+    it("Default", () => {
+        cy.visit(
+            "/iframe.html?id=masthead-primary--default&args=&viewMode=story"
+        )
+        cy.get(".masthead-primary").should("exist")
+
+        cy.percySnapshot("MASTHEAD / Primary: Default")
+    })
+})
