d0505bb967d9984bfdc67f99e3078f1aa845f59b
jendiamond <thejendiamond@gmail.com>
2022-06-30 12:09:25 -0700

    feat: update storybook

--- src/stories/MastheadPrimary.stories.js
+++ src/stories/MastheadPrimary.stories.js
@@ -1,6 +1,9 @@
+import MastheadPrimary from "@/lib-components/MastheadPrimary"
+
 // Storybook default settings
 export default {
     title: "MASTHEAD / Primary",
+    component: MastheadPrimary,
 }
 export const Default = () => ({
     template: `<masthead-primary/>`,
