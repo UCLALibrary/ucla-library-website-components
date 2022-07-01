501ec4832b2d22629c8d24b9b8cb58b727d946d2
Matheus Gomes <matheusgomes062@gmail.com>
2022-07-01 13:46:19 -0300

    fix: svg is now ok, not being cropped

--- .storybook/main.js
+++ .storybook/main.js
@@ -42,7 +42,19 @@ module.exports = {
 
     config.module.rules.push({
         test: /\.svg$/,
-        use: ["vue-svg-loader"],
+        use: [
+          {
+            loader: "vue-svg-loader",
+            options: {
+              svgo: {
+                plugins: [
+                  { removeViewBox: false }
+                ]
+              }
+            }
+          }
+        ]
+        
     })
 
     // return the updated Storybook configuration
