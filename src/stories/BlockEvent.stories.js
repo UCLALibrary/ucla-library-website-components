31087daead5b5ef889498fb937db3b266563bdd8
Matheus Gomes <matheusgomes062@gmail.com>
2022-07-08 16:23:58 -0300

    fix: fix campusMap building-access-text, fix mediaWithText svg download button, add event and postSmall

---
+++ src/stories/BlockEvent.stories.js
@@ -0,0 +1,101 @@
+// Import mock api data
+import * as API from "@/stories/mock-api.json"
+import BlockEvent from "@/lib-components/BlockEvent"
+import StoryRouter from "storybook-vue-router"
+
+// Storybook default settings
+export default {
+    title: "BLOCK / Event",
+    component: BlockEvent,
+    decorators: [StoryRouter()],
+}
+
+export const ShortText = () => ({
+    data() {
+        return {
+            item: { ...API.bricks[0] },
+        }
+    },
+    components: { BlockEvent },
+    template: `
+        <block-event
+            :category="item.category"
+            :title="item.title"
+            :prompt="item.prompt"
+            :dates="item.dates"
+            :to="item.to"
+        />
+    `,
+})
+
+export const LongText = () => ({
+    data() {
+        return {
+            item: { ...API.bricks[1] },
+        }
+    },
+    components: { BlockEvent },
+    template: `
+        <block-event
+            :category="item.category"
+            :title="item.title"
+            :prompt="item.prompt"
+            :dates="item.dates"
+            :to="item.to"
+        />
+    `,
+})
+
+export const Visit = () => ({
+    data() {
+        return {
+            item: { ...API.bricks[1] },
+        }
+    },
+    components: { BlockEvent },
+    template: `
+        <block-event
+            :category="item.category"
+            :title="item.title"
+            :prompt="item.prompt"
+            :dates="item.dates"
+            to="/visit/foo/bar/"
+        />
+    `,
+})
+
+export const About = () => ({
+    data() {
+        return {
+            item: { ...API.bricks[1] },
+        }
+    },
+    components: { BlockEvent },
+    template: `
+        <block-event
+            :category="item.category"
+            :title="item.title"
+            :prompt="item.prompt"
+            :dates="item.dates"
+            to="/about/foo/bar/"
+        />
+    `,
+})
+
+export const Help = () => ({
+    data() {
+        return {
+            item: { ...API.bricks[1] },
+        }
+    },
+    components: { BlockEvent },
+    template: `
+        <block-event
+            :category="item.category"
+            :title="item.title"
+            :prompt="item.prompt"
+            :dates="item.dates"
+            to="/help/foo/bar/"
+        />
+    `,
+})
