8b30435a4685f9c12a9ab5a21109fde980cc1641
jendiamond <thejendiamond@gmail.com>
2022-07-08 14:13:39 -0700

    feat: add files

---
+++ src/stories/SectionStaffList.stories.js
@@ -0,0 +1,101 @@
+// Import component
+import SectionStaffList from "@/lib-components/SectionStaffList.vue"
+
+// A storybook decorator that allows you to use routing-aware components in your stories
+import StoryRouter from "storybook-vue-router"
+
+// Storybook default settings
+export default {
+    title: "SECTION / Staff / List",
+    component: SectionStaffList,
+    decorators: [StoryRouter()],
+}
+
+const mock = [
+    {
+        to: "/staff/foo",
+        jobTitle: "Ullamco",
+        staffName: "Fames ac turpis",
+        department: "Inceptos Himenaeos",
+        email: "loreum@ipsum.foo",
+        locations: [
+            {
+                title: "cupidatat non proident",
+                to: "/location/bar",
+            },
+            {
+                title: "tristique",
+                to: "/location/baz",
+            },
+        ],
+        phone: "(222) 444-5555",
+    },
+    {
+        to: "/staff/foo",
+        jobTitle: "Ullamco",
+        staffName: "Fames ac turpis",
+        department: "Inceptos Himenaeos",
+        email: "loreum@ipsum.foo",
+        locations: [
+            {
+                title: "cupidatat non proident",
+                to: "/location/bar",
+            },
+            {
+                title: "tristique",
+                to: "/location/baz",
+            },
+        ],
+        phone: "(222) 444-5555",
+    },
+    {
+        to: "/staff/foo",
+        jobTitle: "Ullamco",
+        staffName: "Fames ac turpis",
+        department: "Inceptos Himenaeos",
+        email: "loreum@ipsum.foo",
+        locations: [
+            {
+                title: "cupidatat non proident",
+                to: "/location/bar",
+            },
+            {
+                title: "tristique",
+                to: "/location/baz",
+            },
+        ],
+        phone: "(222) 444-5555",
+    },
+    {
+        to: "/staff/foo",
+        image: API.image,
+        jobTitle: "Ullamco",
+        staffName: "Fames ac turpis",
+        department: "Inceptos Himenaeos",
+        email: "loreum@ipsum.foo",
+        locations: [
+            {
+                title: "cupidatat non proident",
+                to: "/location/bar",
+            },
+            {
+                title: "tristique",
+                to: "/location/baz",
+            },
+        ],
+        phone: "(222) 444-5555",
+    },
+]
+
+// Variations of stories below
+export const Default = () => ({
+    data() {
+        return { items: mock }
+    },
+    components: { SectionStaffList },
+    template: `
+      <section-staff-list
+        :items="items"
+      />
+  `,
+})
