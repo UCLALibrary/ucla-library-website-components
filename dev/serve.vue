1bcb95d1f386889e248382616ba7503f9b3ae19b
jendiamond <thejendiamond@gmail.com>
2022-07-07 23:16:52 -0700

    feaat: add all files and connect component

--- dev/serve.vue
+++ dev/serve.vue
@@ -247,6 +247,10 @@
         />
         <hr />
 
+        <h2>SectionSpacesList Component</h2>
+        <section-spaces-list :items="items" />
+        <hr />
+
         <h2>SmartLink Component</h2>
         <smart-link to="/"> This will render as a vue-router link </smart-link>
         <hr />
@@ -516,6 +520,31 @@ export default Vue.extend({
                         prompt: "Velit Exceptur",
                         dates: "Datum 1 - Datum 18, 2021",
                         to: "/visit/uri/2",
+                    },
+                ],
+            },
+            mockSectionSpacesList: {
+                items: [
+                    {
+                        to: "https://calendar.library.ucla.edu",
+                        title: "Internal Portico",
+                        location: "Fames Turpis Building",
+                        text: "Etiam faucibus tellus quis magna finibus auctor. Fusce sagittis vel lectus sed ullamcorper. Proin faucibus nibh ut purus consequat lobortis. Aenean eu fermentum leo, vitae egestas eros. Vivamus urna nibh, porttitor in egestas eu, eleifend ac felis. Praesent sed tristique nibh. Nulla dignissim purus at metus consequat, vitae pharetra orci efficitur. Vivamus facilisis massa vel ipsum vestibulum, porta efficitur dolor aliquam.",
+                        buttonUrl: "http://google.com",
+                    },
+                    {
+                        to: "https://www.gettyimages.com/photos/cute-llamas",
+                        title: "External Von Junz",
+                        location: "Debordist Room",
+                        text: "If one examines Marxist class, one is faced with a choice: either reject Baudrillardist simulation or conclude that the significance of the artist is social comment. The premise of Debordist situation implies that the law is capable of significance. Therefore, any number of constructions concerning a self-sufficient reality may be found.",
+                        buttonUrl: "http://google.com",
+                    },
+                    {
+                        to: "",
+                        title: "Null Beefeater Numpty",
+                        location: "Pennyboy Cottage",
+                        text: "Gosh beefeater numpty scouser rambunctious a bottle of plonk know your onions, cornish pasty spend a penny cheerio down the local mush. What a mug one off blimey a comely wench nose rag ponce gobsmacked pennyboy cottage pie, ey up fake tan spam fritters marvelous chippy hedgehog chav.",
+                        buttonUrl: "http://google.com",
                     },
                 ],
             },
