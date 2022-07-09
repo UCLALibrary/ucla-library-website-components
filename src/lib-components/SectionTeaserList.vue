61ea1f53443ba597a145e1cc01cdd91b205a52bf
jendiamond <thejendiamond@gmail.com>
2022-07-08 18:32:22 -0700

    feat: add section-teaser files

---
+++ src/lib-components/SectionTeaserList.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="section-teaser-list">
+        <div v-for="item in items" :key="item.to" class="meta">
+            <block-highlight
+                :image="item.image"
+                :to="item.to"
+                :category="item.category"
+                :title="item.title"
+                :start-date="item.startDate"
+                :end-date="item.endDate"
+                :text="item.text"
+                :locations="item.locations"
+                class="block"
+            />
+        </div>
+    </section>
+</template>
+
+<script>
+import BlockHighlight from "@/lib-components/BlockBlockHighlight.vue"
+
+export default {
+    name: "SectionTeaserList",
+    components: {
+        BlockHighlight,
+    },
+    props: {
+        items: {
+            //Array of objects {image, to, category, title, dates, times, text}
+            type: Array,
+            default: () => [],
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.section-teaser-list {
+    background-color: var(--color-white);
+    // padding: 0 var(--unit-gutter);
+    max-width: $container-l-main + px;
+
+    .meta {
+        // display: flex;
+        // flex-direction: column;
+        // flex-wrap: nowrap;
+        // justify-content: center;
+        // align-content: center;
+        // align-items: center;
+
+        border-bottom: 2px dotted var(--color-secondary-grey-02);
+
+        &:last-child {
+            border: 0;
+        }
+    }
+
+    .block {
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+}
+</style>
