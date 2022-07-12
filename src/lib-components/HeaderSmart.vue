65bbaa49479f36e82d06f51fd65385756f095367
Matheus Gomes <matheusgomes062@gmail.com>
2022-07-12 14:42:58 -0300

    feat: add headerMainResponsive, headerSmart and fix some

---
+++ src/lib-components/HeaderSmart.vue
@@ -0,0 +1,45 @@
+<template>
+    <header class="header-smart">
+        <site-brand-bar class="brand-bar" />
+        <component
+            :is="whichHeader"
+            :class="isMobile ? 'mobile-header' : 'desktop-header'"
+            :primary-nav="primaryMenuItems"
+            :secondary-nav="secondaryMenuItems"
+        />
+    </header>
+</template>
+
+<script>
+import SiteBrandBar from "@/lib-components/SiteBrandBar"
+
+export default {
+    name: "HeaderSmart",
+    components: {
+        SiteBrandBar,
+    },
+    computed: {
+        primaryMenuItems() {
+            return this.$store.state.header.primary
+        },
+        secondaryMenuItems() {
+            return this.$store.state.header.secondary
+        },
+        isMobile() {
+            return this.$store.state.winWidth <= 1024 ? true : false
+        },
+        whichHeader() {
+            return this.isMobile ? "header-main-responsive" : "header-main"
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@media #{$medium} {
+    .brand-bar {
+        position: absolute;
+        width: 100%;
+    }
+}
+</style>
