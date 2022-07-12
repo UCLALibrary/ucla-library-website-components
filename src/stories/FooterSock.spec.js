7a1451b5cb0373dd97e8af43e25474262668b0cf
jendiamond <thejendiamond@gmail.com>
2022-07-12 14:20:47 -0700

    feat: add files

---
+++ src/stories/FooterSock.spec.js
@@ -0,0 +1,8 @@
+describe("FOOTER / Sock", () => {
+    it("Default", () => {
+        cy.visit("/iframe.html?id=footer-sock--default&args=&viewMode=story")
+        cy.get(".footer-sock").should("exist")
+
+        cy.percySnapshot("FOOTER / Sock: Default")
+    })
+})
---
+++ src/stories/FooterSock.stories.js
@@ -0,0 +1,74 @@
+import Vue from "vue"
+import Vuex from "vuex"
+import FooterSock from "@/lib-components/FooterSock"
+import StoryRouter from "storybook-vue-router"
+
+Vue.use(Vuex)
+
+// Storybook default settings
+export default {
+    title: "FOOTER / Sock",
+    component: FooterSock,
+    decorators: [StoryRouter()],
+}
+
+const mock = {
+    nodes: [
+        {
+            id: "1628",
+            name: "EEEmergency",
+            to: "https://test-craft.library.ucla.edu/emergency",
+            classes: null,
+            target: "",
+        },
+        {
+            id: "1627",
+            name: "AAAccessibility",
+            to: "https://test-craft.library.ucla.edu/accessibility",
+            classes: null,
+            target: "",
+        },
+        {
+            id: "1629",
+            name: "PPPrivacy & Terms of Use",
+            to: "https://test-craft.library.ucla.edu/privacy-terms-of-use",
+            classes: null,
+            target: "",
+        },
+        {
+            id: "9511",
+            name: "CCCreative Commons Attribution 4.0",
+            to: "https://creativecommons.org/licenses/by/4.0/",
+            classes: null,
+            target: "1",
+        },
+    ],
+}
+
+// Variations of stories below
+export const Default = () => ({
+    store: new Vuex.Store({
+        state: {
+            footerSock: {
+                nodes: mock.nodes,
+            },
+        },
+    }),
+    data() {
+        return {
+            ...mock,
+        }
+    },
+    components: { FooterSock },
+    computed: {
+        parsedSockItems() {
+            return this.items.map((obj) => {
+                return {
+                    ...obj,
+                    target: formatLinkTarget(obj.target),
+                }
+            })
+        },
+    },
+    template: `<footer-sock :sock-items="parsedSockItems" />`,
+})
