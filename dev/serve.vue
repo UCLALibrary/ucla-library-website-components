4e721d0184d82b1368cd21404b13e61d5ee2403c
jendiamond <thejendiamond@gmail.com>
2022-07-14 16:09:40 -0700

    feat: tfix typo on StickyHeader

--- dev/serve.vue
+++ dev/serve.vue
@@ -191,7 +191,6 @@
         <divider-way-finder color="visit" />
         <hr />
 
-
         <h2>HeaderMainResponsive Component</h2>
         <header-main-responsive
             :primary-nav="parsedNavPrimary"
@@ -209,11 +208,11 @@
         it's working
         <h2>HeaderSticky Component</h2>
         <header-sticky
-            :primary-items="mockHeaderStickPrimaryItems.items"
-            :secondary-items="mockHeaderStickSecondaryItems.items"
+            :primary-items="mockHeaderStickyPrimaryItems.items"
+            :secondary-items="mockHeaderStickySecondaryItems.items"
         />
         <hr />
-  
+
         <h2>FooterPrimary</h2>
         <footer-primary
             :social-items="socialItems"
@@ -368,8 +367,8 @@
 import Vue from "vue"
 import * as API from "@/stories/mock-api.json"
 import * as IMPACT_API from "@/stories/impact-report_index.json"
-import NavMenuItem from "../src/lib-components/NavMenuItem.vue"
-import HeaderSticky from "../src/lib-components/HeaderSticky.vue"
+// import NavMenuItem from "../src/lib-components/NavMenuItem.vue"
+// import HeaderSticky from "../src/lib-components/HeaderSticky.vue"
 
 export default Vue.extend({
     name: "ServeDev",
@@ -540,7 +539,7 @@ export default Vue.extend({
                     },
                 ],
             },
-            mockHeaderStickSecondaryItems: {
+            mockHeaderStickySecondaryItems: {
                 items: [
                     { ...API.links[0] },
                     { ...API.links[1] },
