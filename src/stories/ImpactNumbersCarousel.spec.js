fc7ada7de34d7c00bc07aa34f65f2bf016153a87
matheusgomes062 <matheusgomes062@gmail.com>
2022-07-19 15:59:10 -0300

    fix: storybook for impactNumberCarousel

--- src/stories/ImpactNumbersCarousel.spec.js
+++ src/stories/ImpactNumbersCarousel.spec.js
@@ -1,6 +1,8 @@
 describe("Impact Numbers Carousel", () => {
     it("Default", () => {
-        cy.visit("/iframe.html?id=impact-numbers-carousel--default")
+        cy.visit(
+            "/iframe.html?id=impact-numbers-carousel--default&args=&viewMode=story"
+        )
         cy.get(".impact-numbers-carousel").should("exist")
 
         cy.percySnapshot("Impact Numbers Carousel: Default")
