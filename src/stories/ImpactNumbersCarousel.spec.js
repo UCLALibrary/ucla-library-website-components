38a29abe94998d5b80f4d911d96442ad5636562a
Ashton Prigge <aprigge@library.ucla.edu>
2022-07-21 16:43:21 -0700

    fix: impact carosule story

--- src/stories/ImpactNumbersCarousel.spec.js
+++ src/stories/ImpactNumbersCarousel.spec.js
@@ -1,8 +1,6 @@
 describe("Impact Numbers Carousel", () => {
     it("Default", () => {
-        cy.visit(
-            "/iframe.html?id=impact-numbers-carousel--default&args=&viewMode=story"
-        )
+        cy.visit("/iframe.html?id=impact-numbers-carousel--default")
         cy.get(".impact-numbers-carousel").should("exist")
 
         cy.percySnapshot("Impact Numbers Carousel: Default")
