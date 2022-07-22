9ea9ff8bfb6b13c4730ff1a8f67a7616f092d885
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2022-07-22 10:42:51 -0700

    feat: add MEAP footer sponsor (#75)
    * feat: add FooterSponsor
    
    * feat: update css, add spec'
    
    * add to serve page
    
    * update size of svgs
    
    * fix: impact carosule story
    
    * update small breakpoint

---
+++ src/stories/FooterSponsor.spec.js
@@ -0,0 +1,8 @@
+describe("FOOTER/ Sponsor", () => {
+    it("Default", () => {
+        cy.visit("/iframe.html?id=footer-sponsor--default&args=&viewMode=story")
+        cy.get(".footer-sponsor").should("exist")
+
+        cy.percySnapshot("Footer / Sponsor: Default")
+    })
+})
