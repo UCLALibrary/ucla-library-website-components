1cce9523b1d8eae0c34a5018d4cf227a2630fc18
Parinita Mulak <pghorpade@library.ucla.edu>
2022-07-25 17:30:03 -0700

    feat: Add FlexibleAssociatedTopicCards components (#80)
    * feat: Add FlexibleAssociatedTopicCards components
    
    * fix: Add router and child component
    
    * style: Add full width and set margin-bottom to 0 for p tag used for showing summary/text

---
+++ src/lib-components/Flexible/AssociatedTopicCards.vue
@@ -0,0 +1,31 @@
+<template>
+    <section-cards-with-illustrations
+        :items="block.associatedTopicsFlexiblePageBlock[0].topics"
+        :title="block.sectionTitle"
+        :text="block.sectionSummary"
+        :is-horizontal="false"
+        class="associated-topic-cards"
+    />
+</template>
+
+<script>
+import SectionCardsWithIllustrations from "@/lib-components/SectionCardsWithIllustrations.vue"
+
+export default {
+    name: "FlexibleAssociatedTopicCards",
+    components: {
+        SectionCardsWithIllustrations,
+    },
+    props: {
+        block: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.associated-topic-cards {
+}
+</style>
