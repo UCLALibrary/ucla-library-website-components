a9ddf06498b55599cf08a07938b7c71729c31e97
jendiamond <thejendiamond@gmail.com>
2022-07-28 16:24:53 -0700

    feat: update stories

--- src/stories/NavSecondary.stories.js
+++ src/stories/NavSecondary.stories.js
@@ -14,14 +14,43 @@ export default {
 export const Default = () => ({
     data() {
         return {
-            items: [...API.secondaryNavLinks],
+            items: [
+                {
+                    id: "843",
+                    name: "Locations & Hours",
+                    to: "/locations",
+                    classes: "",
+                    target: "",
+                },
+                {
+                    id: "844",
+                    name: "Ask a Librarian",
+                    to: "/research-teaching-support/research-help",
+                    classes: null,
+                    target: "",
+                },
+                {
+                    id: "25315",
+                    name: "Support Us",
+                    to: "https://giving.ucla.edu/Standard/NetDonate.aspx?SiteNum=463",
+                    classes: "support-link",
+                    target: "1",
+                },
+                {
+                    id: "845",
+                    name: "My Account",
+                    to: "https://search.library.ucla.edu/discovery/login?vid=01UCS_LAL:UCLA",
+                    classes: "account-button",
+                    target: "1",
+                },
+            ],
         }
     },
     components: { NavSecondary },
     computed: {
         parsedItems() {
             // Restructuring item to support text key
-            return this.items.((obj) => {
+            return this.items((obj) => {
                 return {
                     ...obj,
                     text: obj.name,
@@ -35,7 +64,36 @@ export const Default = () => ({
 export const Microsite = () => ({
     data() {
         return {
-            items: [...API.secondaryNavMicrositeLinks],
+            items: [
+                {
+                    id: "25328",
+                    name: "Get Help With",
+                    to: "http://dev-uclalib-craft.nitro/services-and-resources",
+                    classes: "",
+                    target: "",
+                },
+                {
+                    id: "25329",
+                    name: "Visit",
+                    to: "http://dev-uclalib-craft.nitro/locations-and-hours",
+                    classes: "",
+                    target: "",
+                },
+                {
+                    id: "25352",
+                    name: "About",
+                    to: "http://dev-uclalib-craft.nitro/about-the-ucla-libraries",
+                    classes: "",
+                    target: "",
+                },
+                {
+                    id: "25353",
+                    name: "Support Us",
+                    to: "https://giving.ucla.edu/Standard/NetDonate.aspx?SiteNum=463",
+                    classes: "support-link",
+                    target: "1",
+                },
+            ],
         }
     },
     components: { NavSecondary },
