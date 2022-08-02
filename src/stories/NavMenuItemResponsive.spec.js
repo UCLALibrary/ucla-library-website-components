b3a0c6f20c0e6076c98ed16f8f5d0d520d9d432f
matheusgomes062 <matheusgomes062@gmail.com>
2022-08-02 13:20:27 -0300

    fix: fixed cypress error

--- src/stories/NavMenuItemResponsive.spec.js
+++ src/stories/NavMenuItemResponsive.spec.js
@@ -1,6 +1,8 @@
 describe("NAV / Menu Item Responsive", () => {
     it("Default", () => {
-        cy.visit("/iframe.html?id=nav-menu-item--default&args=&viewMode=story")
+        cy.visit(
+            "/iframe.html?id=nav-navmenuitemresponsive--default&args=&viewMode=story"
+        )
         cy.get(".nav-menu-item").should("exist")
 
         cy.percySnapshot("NAV / menu-item: Default")
