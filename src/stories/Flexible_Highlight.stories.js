dc06a372dc7f8521538a4738ee8c1c2c10b6e312
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-05 13:43:58 -0700

    update flexiblehighlight

--- src/stories/Flexible_Highlight.stories.js
+++ src/stories/Flexible_Highlight.stories.js
@@ -8,248 +8,108 @@ export default {
 }
 
 const mock = {
-    id: "3550",
+    id: "27359",
+    typeHandle: "highlight",
+    titleGeneral: "BlockHighlight",
+    summary: "<p>BlockHighlight</p>",
     highlight: [
         {
-            id: "3575",
+            id: "27360",
+            typeHandle: "internalContent",
             contentLink: [
                 {
-                    id: "1686",
-                    url: "https://test-craft.library.ucla.edu/visit/events-exhibits/a-new-exhibit",
-                    to: "visit/events-exhibits/a-new-exhibit",
-                    title: "a new exhibit",
-                    text: "Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Egestas integer eget aliquet",
-                    date: [
-                        {
-                            startDate: "2021-09-08T07:00:00+00:00",
-                            endDate: "2021-09-29T07:00:00+00:00",
-                        },
-                    ],
+                    id: "20628",
+                    category: "article",
+                    to: "about/news/la-niña",
+                    title: "La Niña",
+                    text: "<p>La Niña is an oceanic and atmospheric phenomenon that is the colder counterpart of El Niño, as part of the broader El Niño–Southern Oscillation climate pattern.</p>",
                     associatedLocations: [
                         {
-                            id: "523",
-                            title: "Powell Library",
-                            to: "visit/locations/powell-library",
+                            id: "11709",
+                            title: "William Andrews Clark Memorial Library",
+                            to: "visit/locations/william-andrews-clark-memorial-library",
+                        },
+                        {
+                            id: "11497",
+                            title: "Richard C. Rudolph East Asian Library",
+                            to: "visit/locations/east-asian-library",
                         },
                     ],
-                    image: [
+                    heroImage: [
                         {
-                            id: "1687",
+                            id: "20639",
                             image: [
                                 {
-                                    id: "1214",
-                                    src: "https://test-craft.library.ucla.edu/assets/images/_fullscreen/sea-turtle.jpg",
-                                    height: 1920,
+                                    id: "20637",
+                                    src: "https://static.library.ucla.edu/craftassetstest/_fullscreen/LaNina-Jet-Wintertime-Pattern.jpg",
+                                    height: 1997,
                                     width: 2560,
-                                    srcset: "https://test-craft.library.ucla.edu/assets/images/_375xAUTO_crop_center-center_none/sea-turtle.jpg 375w, https://test-craft.library.ucla.edu/assets/images/_960xAUTO_crop_center-center_none/sea-turtle.jpg 960w, https://test-craft.library.ucla.edu/assets/images/_1280xAUTO_crop_center-center_none/sea-turtle.jpg 1280w, https://test-craft.library.ucla.edu/assets/images/_1920xAUTO_crop_center-center_none/sea-turtle.jpg 1920w, https://test-craft.library.ucla.edu/assets/images/_2560xAUTO_crop_center-center_none/sea-turtle.jpg 2560w",
-                                    alt: "IMG 20210608 182730371",
+                                    srcset: "https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 2560w",
+                                    alt: "La Nina Jet Wintertime Pattern",
                                     focalPoint: [0.5, 0.5],
                                 },
                             ],
-                            altText: "flowers",
                         },
                     ],
                 },
             ],
         },
         {
-            id: "3576",
+            id: "27361",
+            typeHandle: "internalContent",
             contentLink: [
                 {
-                    id: "1542",
-                    url: "https://test-craft.library.ucla.edu/visit/events-exhibits/exhibit-to-test-card-with-image-content-link",
-                    to: "visit/events-exhibits/exhibit-to-test-card-with-image-content-link",
-                    title: "Exhibit to test card with image content link",
-                    date: [
-                        {
-                            startDate: "2021-09-03T08:00:00+00:00",
-                            endDate: "2021-10-08T07:10:00+00:00",
-                        },
-                    ],
+                    id: "16484",
+                    category: "article",
+                    to: "about/news/this-is-the-newest-article",
+                    title: "This is the newest article",
+                    text: '<h1 class="KrUxN nOQZC dKIty IPnV GoWGq"><span>Revlon, a cosmetics maker that broke racial barriers and dictated beauty trends for much of the last century, is filing for Chapter 11 bankruptcy protection</span><br /></h1>',
                     associatedLocations: [
                         {
-                            id: "523",
-                            title: "Powell Library",
-                            to: "visit/locations/powell-library",
-                        },
-                        {
-                            id: "3062",
-                            title: "Online",
-                            to: "visit/locations/online",
+                            id: "4695",
+                            title: "Louise M. Darling Biomedical Library",
+                            to: "visit/locations/biomed",
                         },
                     ],
-                    image: [
+                    heroImage: [
                         {
-                            id: "2971",
+                            id: "16537",
                             image: [
                                 {
-                                    id: "2536",
-                                    src: "https://test-craft.library.ucla.edu/assets/images/_fullscreen/old-turtle.jpg",
-                                    height: 1344,
+                                    id: "2442",
+                                    src: "https://static.library.ucla.edu/craftassetstest/_fullscreen/shortbread-cookies.jpg",
+                                    height: 1421,
                                     width: 2560,
-                                    srcset: "https://test-craft.library.ucla.edu/assets/images/_375xAUTO_crop_center-center_none/old-turtle.jpg 375w, https://test-craft.library.ucla.edu/assets/images/_960xAUTO_crop_center-center_none/old-turtle.jpg 960w, https://test-craft.library.ucla.edu/assets/images/_1280xAUTO_crop_center-center_none/old-turtle.jpg 1280w, https://test-craft.library.ucla.edu/assets/images/_1920xAUTO_crop_center-center_none/old-turtle.jpg 1920w, https://test-craft.library.ucla.edu/assets/images/_2560xAUTO_crop_center-center_none/old-turtle.jpg 2560w",
-                                    alt: "Hollywood",
+                                    srcset: "https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/shortbread-cookies.jpg 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/shortbread-cookies.jpg 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/shortbread-cookies.jpg 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/shortbread-cookies.jpg 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/shortbread-cookies.jpg 2560w",
+                                    alt: "Shortbread cookies",
                                     focalPoint: [0.5, 0.5],
                                 },
                             ],
-                            altText: "hollywood",
-                        },
-                    ],
-                },
-            ],
-        },
-    ],
-}
-
-export const Default = () => ({
-    data() {
-        return {
-            block: mock,
-        }
-    },
-    components: { FlexibleHighlight },
-    template: `
-        <flexible-highlight
-            :block="block"
-       />
-    `,
-})
-
-const mockTwo = {
-    id: "3550",
-    highlight: [
-        {
-            id: "3575",
-            contentLink: [
-                {
-                    id: "1686",
-                    url: "https://test-craft.library.ucla.edu/visit/events-exhibits/a-new-exhibit",
-                    to: "visit/events-exhibits/a-new-exhibit",
-                    title: "a new exhibit",
-                    date: [
-                        {
-                            startDate: "2021-09-08T07:00:00+00:00",
-                            endDate: "2021-09-29T07:00:00+00:00",
-                        },
-                    ],
-                    associatedLocations: [
-                        {
-                            id: "523",
-                            title: "Powell Library",
-                            to: "visit/locations/powell-library",
-                        },
-                    ],
-                    image: [
-                        {
-                            id: "1687",
-                            image: [
-                                {
-                                    id: "1214",
-                                    src: "https://test-craft.library.ucla.edu/assets/images/_fullscreen/sea-turtle.jpg",
-                                    height: 1920,
-                                    width: 2560,
-                                    srcset: "https://test-craft.library.ucla.edu/assets/images/_375xAUTO_crop_center-center_none/sea-turtle.jpg 375w, https://test-craft.library.ucla.edu/assets/images/_960xAUTO_crop_center-center_none/sea-turtle.jpg 960w, https://test-craft.library.ucla.edu/assets/images/_1280xAUTO_crop_center-center_none/sea-turtle.jpg 1280w, https://test-craft.library.ucla.edu/assets/images/_1920xAUTO_crop_center-center_none/sea-turtle.jpg 1920w, https://test-craft.library.ucla.edu/assets/images/_2560xAUTO_crop_center-center_none/sea-turtle.jpg 2560w",
-                                    alt: "IMG 20210608 182730371",
-                                    focalPoint: [0.5, 0.5],
-                                },
-                            ],
-                            altText: "flowers",
                         },
                     ],
                 },
             ],
         },
         {
-            id: "3576",
-            contentLink: [
+            id: "27384",
+            typeHandle: "externalContent",
+            title: "This is a title",
+            image: [
                 {
-                    id: "1542",
-                    url: "https://test-craft.library.ucla.edu/visit/events-exhibits/exhibit-to-test-card-with-image-content-link",
-                    to: "visit/events-exhibits/exhibit-to-test-card-with-image-content-link",
-                    title: "Exhibit to test card with image content link",
-                    date: [
-                        {
-                            startDate: "2021-09-03T08:00:00+00:00",
-                            endDate: "2021-10-08T07:10:00+00:00",
-                        },
-                    ],
-                    associatedLocations: [
-                        {
-                            id: "523",
-                            title: "Powell Library",
-                            to: "visit/locations/powell-library",
-                        },
-                        {
-                            id: "3062",
-                            title: "Online",
-                            to: "visit/locations/online",
-                        },
-                    ],
-                    image: [
-                        {
-                            id: "2971",
-                            image: [
-                                {
-                                    id: "2536",
-                                    src: "https://test-craft.library.ucla.edu/assets/images/_fullscreen/old-turtle.jpg",
-                                    height: 1344,
-                                    width: 2560,
-                                    srcset: "https://test-craft.library.ucla.edu/assets/images/_375xAUTO_crop_center-center_none/old-turtle.jpg 375w, https://test-craft.library.ucla.edu/assets/images/_960xAUTO_crop_center-center_none/old-turtle.jpg 960w, https://test-craft.library.ucla.edu/assets/images/_1280xAUTO_crop_center-center_none/old-turtle.jpg 1280w, https://test-craft.library.ucla.edu/assets/images/_1920xAUTO_crop_center-center_none/old-turtle.jpg 1920w, https://test-craft.library.ucla.edu/assets/images/_2560xAUTO_crop_center-center_none/old-turtle.jpg 2560w",
-                                    alt: "Hollywood",
-                                    focalPoint: [0.5, 0.5],
-                                },
-                            ],
-                            altText: "hollywood",
-                        },
-                    ],
+                    id: "26803",
+                    src: "https://static.library.ucla.edu/craftassetstest/_fullscreen/kids_2022-08-03-003133_ybnd.jpg",
+                    height: 1704,
+                    width: 2560,
+                    srcset: "https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/kids_2022-08-03-003133_ybnd.jpg 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/kids_2022-08-03-003133_ybnd.jpg 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/kids_2022-08-03-003133_ybnd.jpg 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/kids_2022-08-03-003133_ybnd.jpg 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/kids_2022-08-03-003133_ybnd.jpg 2560w",
+                    alt: "Kids",
+                    focalPoint: [0.5, 0.5],
                 },
             ],
-        },
-        {
-            id: "3576",
-            contentLink: [
-                {
-                    id: "1542",
-                    url: "https://test-craft.library.ucla.edu/visit/events-exhibits/exhibit-to-test-card-with-image-content-link",
-                    to: "visit/events-exhibits/exhibit-to-test-card-with-image-content-link",
-                    title: "Exhibit to test card with image content link",
-                    date: [
-                        {
-                            startDate: "2021-09-03T08:00:00+00:00",
-                            endDate: "2021-10-08T07:10:00+00:00",
-                        },
-                    ],
-                    associatedLocations: [
-                        {
-                            id: "523",
-                            title: "Powell Library",
-                            to: "visit/locations/powell-library",
-                        },
-                        {
-                            id: "3062",
-                            title: "Online",
-                            to: "visit/locations/online",
-                        },
-                    ],
-                    image: [
-                        {
-                            id: "2971",
-                            image: [
-                                {
-                                    id: "2536",
-                                    src: "https://test-craft.library.ucla.edu/assets/images/_fullscreen/old-turtle.jpg",
-                                    height: 1344,
-                                    width: 2560,
-                                    srcset: "https://test-craft.library.ucla.edu/assets/images/_375xAUTO_crop_center-center_none/old-turtle.jpg 375w, https://test-craft.library.ucla.edu/assets/images/_960xAUTO_crop_center-center_none/old-turtle.jpg 960w, https://test-craft.library.ucla.edu/assets/images/_1280xAUTO_crop_center-center_none/old-turtle.jpg 1280w, https://test-craft.library.ucla.edu/assets/images/_1920xAUTO_crop_center-center_none/old-turtle.jpg 1920w, https://test-craft.library.ucla.edu/assets/images/_2560xAUTO_crop_center-center_none/old-turtle.jpg 2560w",
-                                    alt: "Hollywood",
-                                    focalPoint: [0.5, 0.5],
-                                },
-                            ],
-                            altText: "hollywood",
-                        },
-                    ],
-                },
-            ],
+            byline1: "Ashton Prigge",
+            byline2: null,
+            category: "Kids",
+            location: null,
+            text: "Here is some text",
         },
     ],
 }
@@ -257,7 +117,7 @@ const mockTwo = {
 export const ThreeCards = () => ({
     data() {
         return {
-            block: mockTwo,
+            block: mock,
         }
     },
     components: { FlexibleHighlight },
