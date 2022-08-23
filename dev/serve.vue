4e06f16047dab5c4c56593f5958e5f787ddc8125
jendiamond <thejendiamond@gmail.com>
2022-08-23 14:08:47 -0700

    fix: update mFlexible_mediaGallery.js data

--- dev/serve.vue
+++ dev/serve.vue
@@ -1915,10 +1915,6 @@ export default Vue.extend({
                         "Media Gallery Section Title de Finibus Bonorum et Malorum",
                     sectionSummary:
                         "Section summary - Nemo enim ipsam voluptatem quia voluptas sit aspernatur.",
-                    title: null,
-                    slug: null,
-                    shortDescription:
-                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
                     mediaGallery: [
                         {
                             id: "4602",
@@ -2443,7 +2439,7 @@ export default Vue.extend({
                         {
                             id: "28287",
                             typeHandle: "externalContent",
-                            title: null,
+                            title: "Etch-a-Sketch",
                             image: [
                                 {
                                     id: "28251",
@@ -2458,7 +2454,6 @@ export default Vue.extend({
                             ],
                             byline1: "Byline 1: External Content",
                             byline2: null,
-                            title: "Etch-a-Sketch",
                             category: "About",
                             location: "France",
                             text: "<p>About text found here.</p>",
