659008c9374bd820f29448768634b5d8c78514d7
jendiamond <thejendiamond@gmail.com>
2022-08-23 18:25:38 -0700

    fix: style errors

--- src/lib-components/Flexible/MediaGallery/ThumbnailCard.vue
+++ src/lib-components/Flexible/MediaGallery/ThumbnailCard.vue
@@ -37,7 +37,7 @@ export default {
 <style lang="scss" scoped>
 .thumbnail-card {
     width: 100%;
-    
+
     .image {
         margin-bottom: var(--space-s);
     }
--- src/lib-components/Flexible/RichText.vue
+++ src/lib-components/Flexible/RichText.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="rich-text">
+        <h2
+            v-if="block.sectionTitle"
+            class="section-title"
+            v-html="block.sectionTitle"
+        />
+
         <rich-text :rich-text-content="parsedContent" />
     </div>
 </template>
@@ -25,4 +31,11 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.rich-text {
+    .section-title {
+        @include step-4;
+        color: var(--color-primary-blue-03);
+    }
+}
+</style>
