f9315081c9730eb775c7043a71b9c9c2877ac4a8
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-25 10:53:22 -0700

    Update uri data manipulation

--- src/stories/mock/Flexible_AssociatedTopicCards.js
+++ src/stories/mock/Flexible_AssociatedTopicCards.js
@@ -16,12 +16,19 @@ export const itemsVertical = {
                 {
                     title: "Types of Sinkholes",
                     text: "Dissolution sinkholes, Cover-subsidence sinkholes, Cover-collapse sinkholes",
-                    to: "help/types-sinkholes",
+                    uri: "help/types-sinkholes",
                 },
                 {
-                    to: "help/animals",
+                    uri: "help/animals",
                     title: "Animals eat food!",
                     text: "<p>Some animals eat plants and plant products like grass, leaves and fodder. Goat, cow, horse and sheep eat only plants.</p>",
+                },
+                {
+                    title: "Metadata Planning Worksheet",
+                    text: "<p>Use this worksheet to brainstorm how you might want to describe items\n within your collection and to hone in your choices before filling out \nthe spreadsheet.</p>",
+                    uri: null,
+                    externalResourceUrl:
+                        "https://docs.google.com/document/d/1hyzHX5cWE_Y7aML7XHI29tswnqV44Q0sJsnibu-Krp4/edit",
                 },
             ],
         },
