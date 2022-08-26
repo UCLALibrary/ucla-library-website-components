a1690e511c029fe25fdb6f3f7bbc5407d282c395
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-26 11:41:11 -0700

    update paths

--- src/lib-components/Flexible/BannerFeatured.vue
+++ src/lib-components/Flexible/BannerFeatured.vue
@@ -17,7 +17,7 @@
             v-if="block && block.content && !block.content[0].contentLink"
             class="flexible-banner-featured"
             :image="parseImage"
-            :to="`/${block.content[0].to}`"
+            :to="block.content[0].to"
             :title="block.content[0].title"
             :breadcrumb="parsedTypeHandle"
             :byline="parseByLine"
