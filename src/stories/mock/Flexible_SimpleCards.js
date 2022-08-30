47898cbc967ed97edad2ea1fd0f79eaf86635bbf
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-30 10:55:22 -0700

    update data

--- src/stories/mock/Flexible_SimpleCards.js
+++ src/stories/mock/Flexible_SimpleCards.js
@@ -35,21 +35,6 @@ export const mockTwoCards = {
                 },
             ],
         },
-        {
-            id: "42484",
-            typeHandle: "internalResource",
-            contentLink: [
-                {
-                    id: "41851",
-                    uri: null,
-                    title: "Guide for Archival Processing",
-                    summary:
-                        "<p>A guide to processing archival collections from the University of Florida.<br /></p>",
-                    externalResourceUrl:
-                        "https://guides.uflib.ufl.edu/archivalprocessing/surveying",
-                },
-            ],
-        },
     ],
 }
 
@@ -160,6 +145,21 @@ export const mockFourCards = {
                 },
             ],
         },
+        {
+            id: "42484",
+            typeHandle: "internalResource",
+            contentLink: [
+                {
+                    id: "41851",
+                    uri: null,
+                    title: "Guide for Archival Processing",
+                    summary:
+                        "<p>A guide to processing archival collections from the University of Florida.<br /></p>",
+                    externalResourceUrl:
+                        "https://guides.uflib.ufl.edu/archivalprocessing/surveying",
+                },
+            ],
+        },
     ],
 }
 
@@ -215,6 +215,36 @@ export const mockFiveCards = {
                 },
             ],
         },
+        {
+            id: "42484",
+            typeHandle: "internalResource",
+            contentLink: [
+                {
+                    id: "41851",
+                    uri: null,
+                    title: "Guide for Archival Processing",
+                    summary:
+                        "<p>A guide to processing archival collections from the University of Florida.<br /></p>",
+                    externalResourceUrl:
+                        "https://guides.uflib.ufl.edu/archivalprocessing/surveying",
+                },
+            ],
+        },
+        {
+            id: "42484",
+            typeHandle: "internalResource",
+            contentLink: [
+                {
+                    id: "41851",
+                    uri: null,
+                    title: "Guide for Archival Processing",
+                    summary:
+                        "<p>A guide to processing archival collections from the University of Florida.<br /></p>",
+                    externalResourceUrl:
+                        "https://guides.uflib.ufl.edu/archivalprocessing/surveying",
+                },
+            ],
+        },
     ],
 }
 
@@ -225,6 +255,51 @@ export const mockSixCards = {
     sectionSummary:
         "<p>These resources provide guidance related to documentation and survey projects.</p>",
     cards: [
+        {
+            id: "41059",
+            typeHandle: "internalResource",
+            contentLink: [
+                {
+                    id: "40999",
+                    uri: null,
+                    title: "Template for Survey Planning",
+                    summary:
+                        "<p>This template provides guidance related to documentation and survey projects. </p>",
+                    externalResourceUrl:
+                        "https://docs.google.com/document/d/1TU4ogqEiwWfSN7dyDkdYBm3gQYFvPCnYsV1jjMC2_6U/edit",
+                },
+            ],
+        },
+        {
+            id: "41060",
+            typeHandle: "internalResource",
+            contentLink: [
+                {
+                    id: "41001",
+                    uri: null,
+                    title: "Template for Item-Level Inventory",
+                    summary:
+                        "<p>This template provides guidance related to documentation and survey projects.</p>",
+                    externalResourceUrl:
+                        "https://docs.google.com/spreadsheets/d/1v3_fPBQSNXEPqRdMf0OQeQ3NB9tBwb_jTnPqcWtZYgg/edit#gid=15338334",
+                },
+            ],
+        },
+        {
+            id: "42484",
+            typeHandle: "internalResource",
+            contentLink: [
+                {
+                    id: "41851",
+                    uri: null,
+                    title: "Guide for Archival Processing",
+                    summary:
+                        "<p>A guide to processing archival collections from the University of Florida.<br /></p>",
+                    externalResourceUrl:
+                        "https://guides.uflib.ufl.edu/archivalprocessing/surveying",
+                },
+            ],
+        },
         {
             id: "41059",
             typeHandle: "internalResource",
