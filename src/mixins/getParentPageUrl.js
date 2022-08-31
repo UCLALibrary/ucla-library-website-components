1b08ea74a441765a730bc8d8e5f48ca60aac36f8
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2022-08-31 13:55:45 -0700

    fix: update mixin (#177)
    * update mixin
    
    * update mixin
    
    Co-authored-by: JenDiamond <thejendiamond@gmail.com>

--- src/mixins/getParentPageUrl.js
+++ src/mixins/getParentPageUrl.js
@@ -1,6 +1,7 @@
 export default {
     /**
      * Take a URI and figure out what "section" of the site it is pointing too
+     * If output = ["", "page"] then the parent page is the homepage
      * @param {String} uri
      * @returns {String}
      */
@@ -8,7 +9,7 @@ export default {
     methods: {
         getParentPageUrl(uri = "") {
             let output = uri.split("/")
-            if (output[0] === "") {
+            if (output.length === 2) {
                 return "/"
             } else {
                 output.pop()
