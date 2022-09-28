1049e16e35ea0efc052ee7724d67802f9ff9be0f
Andy Wallace <andrewwallace@library.ucla.edu>
2022-09-28 13:55:22 -0700

    compute smart header levels
    - <SectionWrapper>
    -- each instance has a "level", which defaults to its parent SectionWrapper's level + 1, or level 2 if there is no enclosing SectionWrapper (level 1 being the page).
    -- level can also be set using the "level" prop, which takes precedence
    -- uses new component <SectionHeader> for title
    
    - <SectionHeader>
    -- uses proper <h#> tag, based on level of enclosing <SectionWrapper> (defaults to <h1>)
    -- uses <h6> for levels 7 and higher, not that we'll ever actually use that
    -- accepts 'level' prop, which takes precedence

---
+++ src/lib-components/SectionHeader.vue
@@ -0,0 +1,80 @@
+<template>
+    <component
+        :is="titleTag"
+        :class="classes"
+    >
+        <slot />
+    </component>
+</template>
+
+<script>
+export default {
+    name: "SectionHeader",
+    props: {
+        theme: {
+            type: String,
+            default: "white",
+        },
+        level: {
+            type: Number,
+            default: 0,
+        },
+    },
+    inject: {
+        levelInjected: { from: "sectionLevel", default: 1 },
+    },
+    computed: {
+        classes() {
+            return ["section-header", `section-header${this.levelComputed}`]
+        },
+        levelComputed() {
+            return Number(this.level || this.levelInjected)
+        },
+        titleTag() {
+            return (this.levelComputed >= 1 && this.levelComputed <= 6) ? `h${this.levelComputed}` : "h6"
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.section-header {
+    color: var(--color-primary-blue-03);
+}
+
+h1 {
+    font-family: var(--font-primary);
+    font-size: var(--step-5);
+    font-weight: 400;
+}
+
+h2 {
+    font-family: var(--font-primary);
+    font-size: var(--step-4);
+    font-weight: 400;
+}
+
+h3 {
+    font-family: var(--font-primary);
+    font-size: var(--step-3);
+    font-weight: 400;
+}
+
+h4 {
+    font-family: var(--font-primary);
+    font-size: var(--step-3);
+    font-weight: 400;
+}
+
+h5 {
+    font-family: var(--font-primary);
+    font-size: var(--step-3);
+    font-weight: 400;
+}
+
+h6 {
+    font-family: var(--font-primary);
+    font-size: 20px;
+    font-weight: 500;
+}
+</style>
--- src/lib-components/SectionWrapper.vue
+++ src/lib-components/SectionWrapper.vue
@@ -1,7 +1,7 @@
 <template>
     <section :class="classes">
         <div v-if="sectionTitle" class="section-header">
-            <h2
+            <SectionHeader
                 v-if="sectionTitle"
                 class="section-title"
                 v-text="sectionTitle"
@@ -17,8 +17,11 @@
 </template>
 
 <script>
+import SectionHeader from "@/lib-components/SectionHeader.vue"
+
 export default {
     name: "SectionWrapper",
+    components: { SectionHeader },
     props: {
         sectionTitle: {
             type: String,
@@ -32,10 +35,27 @@ export default {
             type: String,
             default: "white",
         },
+        level: {
+            type: Number,
+            default: 0,
+        },
+    },
+    provide() {
+        return {
+            sectionLevel: this.levelComputed,
+            noParentSection: false,
+        }
+    },
+    inject: {
+        levelInjected: { from: "sectionLevel", default: 1 },
+        noParentSection: { default: true },
     },
     computed: {
         classes() {
-            return ["section-wrapper", `theme-${this.theme}`]
+            return ["section-wrapper", `section-wrapper${this.levelComputed}`, `theme-${this.theme}`, {"top-level": this.noParentSection}]
+        },
+        levelComputed() {
+            return Number(this.level || (this.levelInjected + 1))
         },
     },
 }
@@ -43,60 +63,62 @@ export default {
 
 <style lang="scss" scoped>
 .section-wrapper {
-    --color-theme: var(--color-white);
+    > .section-header {
+        margin-bottom: var(--space-xl);
 
-    &.theme-gray {
-        --color-theme: var(--color-secondary-grey-01);
-        padding: var(--space-3xl) var(--unit-gutter);
+        > .section-title {
+            color: var(--color-primary-blue-03);
+            margin-bottom: var(--space-m);
+        }
+
+        .section-summary {
+            @include step-0;
+
+            ::v-deep p {
+                margin: 0;
+            }
+        }
     }
 
-    &.theme-white {
-        padding: 0 var(--unit-gutter);
-        margin: var(--space-3xl) auto;
-        &.section-banner {
+    &.top-level {
+        --color-theme: var(--color-white);
+
+        &.theme-gray {
+            --color-theme: var(--color-secondary-grey-01);
+            padding: var(--space-3xl) var(--unit-gutter);
+        }
+
+        &.theme-white {
+            padding: 0 var(--unit-gutter);
+            margin: var(--space-3xl) auto;
+            &.section-banner {
+                margin-top: 0;
+            }
+        }
+
+        &.theme-divider {
+            padding: 0 var(--unit-gutter);
+        }
+
+        padding: var(--space-3xl) var(--unit-gutter);
+        margin: 0 auto;
+        background-color: var(--color-theme);
+
+        // Configure spacing of child components (individual components might override things like max-width)
+        > * {
+            max-width: #{$container-l-main}px;
+            padding: 0;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        > :first-child:not(.divider-way-finder) {
             margin-top: 0;
         }
-    }
 
-    &.theme-divider {
-        padding: 0 var(--unit-gutter);
-    }
-
-    padding: var(--space-3xl) var(--unit-gutter);
-    margin: 0 auto;
-    background-color: var(--color-theme);
-
-    .section-header {
-        margin-bottom: var(--space-xl);
-    }
-
-    .section-title {
-        @include step-3;
-        color: var(--color-primary-blue-03);
-        margin-bottom: var(--space-m);
-    }
-    .section-summary {
-        @include step-0;
-
-        ::v-deep p {
-            margin: 0;
+        > :last-child:not(.divider-way-finder) {
+            margin-bottom: 0;
         }
-    }
-
-    // Configure spacing of child components (individual components might override things like max-width)
-    > * {
-        max-width: #{$container-l-main}px;
-        padding: 0;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    > :first-child:not(.divider-way-finder) {
-        margin-top: 0;
-    }
-
-    > :last-child:not(.divider-way-finder) {
-        margin-bottom: 0;
     }
 }
 </style>
