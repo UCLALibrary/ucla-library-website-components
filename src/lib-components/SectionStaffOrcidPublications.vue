d9c49d59f2cf04920a63921f230de2efbbdf2b0b
dianneweinthal <44713143+dianneweinthal@users.noreply.github.com>
2022-10-14 10:04:16 -0700

    fix: update width for correct publication selector

--- src/lib-components/SectionStaffOrcidPublications.vue
+++ src/lib-components/SectionStaffOrcidPublications.vue
@@ -43,7 +43,7 @@ export default {
 
 <style lang="scss" scoped>
 .section-staff-orcid-publications {
-    max-width: $container-l-text + px;
+    max-width: $container-l-main + px;
     margin: 0 auto;
 
     .secondary-header {
@@ -68,6 +68,7 @@ export default {
 
     .publications {
         padding-right: 0;
+        max-width: $container-l-text + px;
     }
 
     @media #{$has-hover} {
