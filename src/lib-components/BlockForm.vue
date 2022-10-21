82eeaf0d59be84ff6c2acc6a6b140f0a4aa26497
matheusgomes062 <matheusgomes062@gmail.com>
2022-10-21 13:54:41 -0300

    feat: add libcal url inject to blockForm

--- src/lib-components/BlockForm.vue
+++ src/lib-components/BlockForm.vue
@@ -199,7 +199,7 @@ import SvgIconCheckbox from "ucla-library-design-tokens/assets/svgs/icon-checkbo
 
 export default {
     name: "BlockForm",
-    inject: ["eventId", "blockFormData"],
+    inject: ["eventId", "blockFormData", "libcalEndpoint"],
     components: {
         SvgGlyphClose,
         ButtonLink: () =>
@@ -309,7 +309,7 @@ export default {
                     process.env.VUE_APP_CALENDAR_LIBRARY_URL +
                     `${this.eventId}/register`
             } else {
-                url = `https://test.proxy.calendar.library.ucla.edu/api/1.1/events/${this.eventId}/register`
+                url = `${this.libcalEndpoint} + api/1.1/events/${this.eventId}/register`
             }
 
             fetch(url, {
