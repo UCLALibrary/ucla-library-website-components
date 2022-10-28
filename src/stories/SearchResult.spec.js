f465aa9787c0e94b0247335c39886c3786ff1e23
Ashton Prigge <aprigge@library.ucla.edu>
2022-10-28 13:24:14 -0700

    SearchResult component

--- src/stories/SearchResult.spec.js
+++ src/stories/SearchResult.spec.js
@@ -1,8 +1,6 @@
 describe("SEARCH / Search Result", () => {
     it("Default", () => {
-        cy.visit(
-            "/iframe.html?id=ssearch-search-result--default&viewMode=story#"
-        )
+        cy.visit("/iframe.html?id=search-search-result--default&viewMode=story")
         cy.get(".search-result-item").should("exist")
 
         cy.percySnapshot("SEARCH / Search Result: Default")
