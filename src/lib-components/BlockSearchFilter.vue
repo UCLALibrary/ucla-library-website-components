02152fd532fe4e7025879db0544141cc6821a7f8
matheusgomes062 <matheusgomes062@gmail.com>
2022-11-21 14:34:13 -0300

    feat: added component, storybook and spec

---
+++ src/lib-components/BlockSearchFilter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="block-search-filter">
+        {{ title }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BlockSearchFilter",
+    data() {
+        return {}
+    },
+    props: {
+        title: {
+            type: String,
+            default: "",
+        },
+        filterType: {
+            type: String,
+            default: "",
+        },
+    },
+    methods: {},
+}
+</script>
+
+<style scoped lang="scss">
+.block-search-filter {
+  padding: 12px,
+  margin-right: 10px
+}
+</style>
---
+++ src/stories/BlockSearchFilter.stories.js
@@ -0,0 +1,25 @@
+import BlockSearchFilter from "@/lib-components/BlockSearchFilter"
+
+export default {
+    title: "BLOCK / Search Filter",
+    component: BlockSearchFilter,
+}
+
+const mock = {
+    title: "",
+    filterType: "location || departments || subjects ||categories",
+}
+
+// Variations of stories below
+export const Default = () => ({
+    data() {
+        return { ...mock }
+    },
+    components: { BlockSearchFilter },
+    template: `
+        <block-search-filter
+            :title="title"
+            :filterType="filterType"
+        />
+    `,
+})
