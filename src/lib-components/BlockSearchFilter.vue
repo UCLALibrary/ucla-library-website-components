c03dd66ded91277fde7a3a9a44799e5d53260e27
Matheus Gomes <47605309+matheusgomes062@users.noreply.github.com>
2022-11-28 15:55:14 -0300

    feat: Apps-2013 selected search filters (#248)
    * feat: added component, storybook and spec
    
    * feat: added colors and component changes
    
    * feat: add section for searchfilter
    
    * feat: add remove function
    
    * fix: sectionlocationlist fix
    
    * feat: add color
    
    * fix: color fix
    
    * fix: storybook storyrouter
    
    * fix: storybook storyrouter
    
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>

---
+++ src/lib-components/BlockSearchFilter.vue
@@ -0,0 +1,98 @@
+<template>
+    <div :class="classes">
+        <a>
+            {{ title }}
+        </a>
+        <button type="button" @click="closeBlockFilter()">
+            <svg-glyph-close class="svg-glyph-close" />
+        </button>
+    </div>
+</template>
+
+<script>
+// Helpers
+import getSectionName from "@/mixins/getSectionName"
+
+import SvgGlyphClose from "ucla-library-design-tokens/assets/svgs/icon-close.svg"
+
+export default {
+    name: "BlockSearchFilter",
+    mixins: [getSectionName],
+    data() {
+        return {}
+    },
+    components: {
+        SvgGlyphClose,
+    },
+    props: {
+        title: {
+            type: String,
+            default: "",
+        },
+        filterType: {
+            type: String,
+            default: "",
+        },
+        index: {
+            type: Number,
+            default: 0,
+        },
+        color: {
+            type: String,
+            default: "", // This will be "visit", "about", "help".
+        },
+    },
+    computed: {
+        classes() {
+            return ["block-search-filter", `color-${this.sectionName}`]
+        },
+        sectionName() {
+            return this.color || this.getSectionName(this.$route.path)
+        },
+    },
+    methods: {
+        closeBlockFilter() {
+            this.$emit("removeBlockFilter", this.index)
+        },
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.block-search-filter {
+    padding: 12px;
+    display: flex;
+    flex-direction: row;
+    border: 1.5px #c099ff solid;
+    background: #ffffff;
+    border-radius: 4px;
+    align-items: center;
+    width: fit-content;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 100%;
+
+    &:hover {
+        background: #efe5ff;
+        cursor: pointer;
+    }
+
+    a {
+        margin-right: 8px;
+        color: #032d5b;
+    }
+
+    &.color-help {
+        --color-border: var(--color-help-green-03);
+    }
+    &.color-visit {
+        --color-border: var(--color-visit-fushia-03);
+    }
+    &.color-about {
+        --color-border: var(--color-about-purple-03);
+    }
+    &.color-default {
+        --color-border: var(--color-default-cyan-03);
+    }
+}
+</style>
