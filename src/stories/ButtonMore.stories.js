e39bace56b60b37d83f0923ed1b39ed52e5c25c8
matheusgomes062 <matheusgomes062@gmail.com>
2022-12-05 14:55:27 -0300

    feat: added smart-link to button-more

--- src/stories/ButtonMore.stories.js
+++ src/stories/ButtonMore.stories.js
@@ -15,3 +15,8 @@ export const WithAlternateText = () => ({
     components: { ButtonMore },
     template: `<button-more text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>`,
 })
+
+export const WithLink = () => ({
+    components: { ButtonMore },
+    template: `<button-more text="See More" to="visit/events-exhibits"/>`,
+})
