75faccf3d3f7a0a573b3877dbf0c5fb876420c4f
Andy Wallace <andrewwallace@library.ucla.edu>
2022-12-07 12:07:49 -0800

    SectionSpacesList

--- src/stories/SectionSpacesList.spec.js
+++ src/stories/SectionSpacesList.spec.js
@@ -4,7 +4,18 @@ describe("SECTION / Spaces / List", () => {
             "/iframe.html?id=section-spaces-list--default&args=&viewMode=story"
         )
         cy.get(".section-spaces-list").should("exist")
-
+        cy.contains("Null Beefeater Numpty").should("be.visible")
+        cy.get(".button-toggle").should("not.exist")
         cy.percySnapshot("SECTION / Spaces / List: Default")
+    })
+
+    it("Expandable", () => {
+        cy.visit(
+            "/iframe.html?id=section-spaces-list--expandable&args=&viewMode=story"
+        )
+        cy.get(".section-spaces-list").should("exist")
+        cy.contains("Null Beefeater Numpty").should("not.be.visible")
+        cy.get(".button-toggle").should("be.visible").click()
+        cy.contains("Null Beefeater Numpty").should("be.visible")
     })
 })
