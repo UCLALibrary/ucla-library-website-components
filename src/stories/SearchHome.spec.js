38551ea1c03bd3be74fb64432adc8b9d5daabf7d
JenDiamond <thejendiamond@gmail.com>
2023-01-06 18:46:11 -0800

    feat: APPS-2111 update search home to search primo (#291)
    * fix: link to title only sometimes
    
    * feat: update search
    
    * Update SearchHome.vue
    
    * feat: update link
    
    * feat: update search url
    
    Co-authored-by: Ashton Prigge <34147754+aprigge@users.noreply.github.com>

--- src/stories/SearchHome.spec.js
+++ src/stories/SearchHome.spec.js
@@ -1,6 +1,8 @@
-describe("Search / Home", () => {
+describe("Search / Search Home", () => {
     it("Default", () => {
-        cy.visit("/iframe.html?id=search-home--default&args=&viewMode=story")
+        cy.visit(
+            "/iframe.html?id=search-search-home--default&args=&viewMode=story"
+        )
         cy.get(".search-home").should("exist")
 
         cy.percySnapshot("Search / Home: Default")
