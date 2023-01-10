d8b57621e71a1c862de9c89baeb5b051ccf6113a
Ashton Prigge <aprigge@library.ucla.edu>
2023-01-10 10:02:44 -0800

    move mock for flexible_card with image to story

--- src/stories/Flexible_CardWithImage.stories.js
+++ src/stories/Flexible_CardWithImage.stories.js
@@ -1,12 +1,172 @@
 import FlexibleCardWithImage from "@/lib-components/Flexible/CardWithImage.vue"
 import StoryRouter from "storybook-vue-router"
 
-import { mock } from "./mock/Flexible_CardWithImage.js"
-
 export default {
     title: "FLEXIBLE / Card With Image",
     component: FlexibleCardWithImage,
     decorators: [StoryRouter()],
+}
+
+const mock = {
+    id: "28284",
+    typeHandle: "cardWithImage",
+    sectionTitle: "Section Title: Card with image",
+    sectionSummary: "<p>Section Summary: card with image</p>",
+    cardWithImage: [
+        {
+            id: "28285",
+            typeHandle: "internalContent",
+            contentLink: [
+                {
+                    id: "20628",
+                    contentType: "article",
+                    to: "about/news/la-niña",
+                    title: "La Niña",
+                    text: "<p>La Niña is an oceanic and atmospheric phenomenon that is the colder counterpart of El Niño, as part of the broader El Niño–Southern Oscillation climate pattern.</p>",
+                    articleCategory: [
+                        {
+                            title: "Featured",
+                        },
+                    ],
+                    articleByline1: [
+                        {
+                            id: "11827",
+                            to: "about/staff/brigid-abreu",
+                            title: "Brigid Abreu",
+                        },
+                        {
+                            id: "11910",
+                            to: "about/staff/sylvia-page",
+                            title: "Sylvia Page",
+                        },
+                    ],
+                    articleByline2: "2022-06-21T12:39:00-07:00",
+                    associatedLocations: [
+                        {
+                            id: "11709",
+                            title: "William Andrews Clark Memorial Library",
+                            to: "visit/locations/william-andrews-clark-memorial-library",
+                        },
+                        {
+                            id: "11497",
+                            title: "Richard C. Rudolph East Asian Library",
+                            to: "visit/locations/east-asian-library",
+                        },
+                    ],
+                    heroImage: [
+                        {
+                            id: "20639",
+                            image: [
+                                {
+                                    id: "20637",
+                                    src: "https://static.library.ucla.edu/craftassetstest/_fullscreen/LaNina-Jet-Wintertime-Pattern.jpg",
+                                    height: 1997,
+                                    width: 2560,
+                                    srcset: "https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/LaNina-Jet-Wintertime-Pattern.jpg 2560w",
+                                    alt: "La Nina Jet Wintertime Pattern",
+                                    focalPoint: [0.5, 0.5],
+                                    altText: "La Niña",
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: "28286",
+            typeHandle: "internalContent",
+            contentLink: [
+                {
+                    id: "16484",
+                    contentType: "article",
+                    to: "about/news/this-is-the-newest-article",
+                    title: "This is the newest article",
+                    text: '<p>Add "MEAP" to show article in the MEAP website</p>',
+                    articleCategory: [
+                        {
+                            title: "Featured",
+                        },
+                    ],
+                    articleByline1: [
+                        {
+                            id: "3522",
+                            to: "about/staff/dianne",
+                            title: "D. Weinthal",
+                        },
+                    ],
+                    articleByline2: "2022-05-19T13:59:00-07:00",
+                    associatedLocations: [
+                        {
+                            id: "4695",
+                            title: "Louise M. Darling Biomedical Library",
+                            to: "visit/locations/biomed",
+                        },
+                    ],
+                    heroImage: [
+                        {
+                            id: "16537",
+                            image: [
+                                {
+                                    id: "2442",
+                                    src: "https://static.library.ucla.edu/craftassetstest/_fullscreen/shortbread-cookies.jpg",
+                                    height: 1421,
+                                    width: 2560,
+                                    srcset: "https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/shortbread-cookies.jpg 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/shortbread-cookies.jpg 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/shortbread-cookies.jpg 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/shortbread-cookies.jpg 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/shortbread-cookies.jpg 2560w",
+                                    alt: "Shortbread cookies",
+                                    focalPoint: [0.5, 0.5],
+                                    altText: null,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: "39508",
+            typeHandle: "externalContent",
+            title: "Pandemic Spurs Virtual Screening Room, Bringing Moving Images to a Global Audience",
+            image: [
+                {
+                    id: "36751",
+                    src: "https://static.library.ucla.edu/craftassetstest/images/_fullscreen/virtual-screening-room-toll-sea.jpeg",
+                    height: 1869,
+                    width: 2560,
+                    srcset: "https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/virtual-screening-room-toll-sea.jpeg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/virtual-screening-room-toll-sea.jpeg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/virtual-screening-room-toll-sea.jpeg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/virtual-screening-room-toll-sea.jpeg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/virtual-screening-room-toll-sea.jpeg 2560w",
+                    alt: "Virtual screening room toll sea",
+                    focalPoint: [0.5, 0.5],
+                },
+            ],
+            byline1: null,
+            byline2: null,
+            category: null,
+            text: "When its theatrical home closed its doors in March 2020 due to COVID-19 the UCLA Film & Television Archive shifted public programming to an ambitious new online screening",
+            to: "https://www.cinema.ucla.edu/blogs/archive-blog/2022/01/31/pandemic-spurs-virtual-screening-room",
+        },
+        {
+            id: "28287",
+            typeHandle: "externalContent",
+            title: null,
+            image: [
+                {
+                    id: "28251",
+                    src: "https://static.library.ucla.edu/craftassetstest/_fullscreen/etch-a-sketch.jpeg",
+                    height: 2032,
+                    width: 2560,
+                    srcset: "https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/etch-a-sketch.jpeg 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/etch-a-sketch.jpeg 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/etch-a-sketch.jpeg 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/etch-a-sketch.jpeg 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/etch-a-sketch.jpeg 2560w",
+                    alt: "Etch a sketch",
+                    focalPoint: [0.5, 0.5],
+                    altText: null,
+                },
+            ],
+            byline1: "By External Content",
+            byline2: null,
+            category: "About",
+            location: "France",
+            text: "<p>About text found here.</p>",
+        },
+    ],
 }
 
 export const Default = () => ({
