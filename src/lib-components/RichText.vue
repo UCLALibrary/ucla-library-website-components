d639d713f2188b423ed8829a1935197a91641d12
jendiamond <thejendiamond@gmail.com>
2023-01-27 13:05:23 -0800

    refactor: add target blank with icon

--- src/lib-components/RichText.vue
+++ src/lib-components/RichText.vue
@@ -138,12 +138,19 @@ export default {
         }
     }
 
+    ::v-deep a[target="_blank"] {
+        position: relative;
+        margin-right: 25px;
+    }
     ::v-deep a[target="_blank"]:after {
         content: url("node_modules/ucla-library-design-tokens/assets/svgs/icon-external-link.svg");
         display: inline-block;
         margin-left: 0.2em;
         width: 1em;
         height: 1em;
+        position: absolute;
+        top: -5.5px;
+        right: -15px;
     }
 
     ::v-deep ul,
