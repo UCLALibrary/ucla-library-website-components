8593e8037709b27cde8929f463d40364fbdc9871
Andy Wallace <andrewwallace@library.ucla.edu>
2023-02-06 14:04:50 -0800

    fix: SimpleCard truncaton

--- src/stories/BlockSimpleCard.stories.js
+++ src/stories/BlockSimpleCard.stories.js
@@ -80,3 +80,16 @@ export const InternalLinkGuide = () => ({
         />
     `,
 })
+
+export const LongText = () => ({
+    data() {
+        return { card: {
+            to: "sink-hole",
+            title: "Sink Hole",
+            text:
+                "Adipisicing do enim voluptate amet nisi eiusmod ex aliqua exercitation nulla sint magna proident proident. Exercitation in et enim est esse consectetur ex dolore labore ut laborum non minim ea. In ad excepteur cillum commodo veniam dolore labore cupidatat. Ea fugiat occaecat et fugiat consectetur do consectetur anim cillum. Ex nulla est ex cillum esse. Aliquip deserunt consequat pariatur sunt labore occaecat. Excepteur nostrud ex ex anim ut irure tempor eu quis id cupidatat consectetur. Mollit adipisicing cupidatat occaecat labore cupidatat culpa sit pariatur. Anim reprehenderit dolore est aliqua id pariatur quis exercitation incididunt do magna. Reprehenderit irure duis aliqua ullamco sint incididunt commodo exercitation reprehenderit. Non esse quis consectetur eiusmod. Labore dolore ex commodo culpa incididunt ipsum nulla elit tempor in officia eiusmod. Velit incididunt Lorem eiusmod eu anim dolore voluptate elit nisi aliquip est elit qui occaecat. Consequat cillum consectetur pariatur magna incididunt tempor eu do commodo laboris proident id dolor. Pariatur sint incididunt Lorem anim est nostrud qui excepteur eu fugiat exercitation exercitation. Lorem laboris reprehenderit ipsum aliquip ullamco sunt aute culpa occaecat in aliquip incididunt consequat nostrud.",
+        } }
+    },
+    components: { BlockSimpleCard },
+    template: `<block-simple-card v-bind="card" style="max-width: 300px;"/>`,
+})
