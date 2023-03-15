9f0b4357e246de8f994099c22ae229cacd390f63
Ashton Prigge <aprigge@library.ucla.edu>
2023-03-15 11:26:36 -0700

    specs

---
+++ src/stories/BlockCardWithImage.spec.js
@@ -0,0 +1,10 @@
+describe("BlockCardWithImage", () => {
+    it("Default", () => {
+        cy.visit(
+            "/iframe.html?id=block-card-with-image--default&args=&viewMode=story"
+        )
+        cy.get(".block-card-with-image").should("exist")
+
+        cy.percySnapshot("BLOCK / Card With Image: Default")
+    })
+})
---
+++ src/stories/CardMeta.spec.js
@@ -0,0 +1,8 @@
+describe("CardMeta", () => {
+    it("Default", () => {
+        cy.visit("/iframe.html?id=card-meta--default&args=&viewMode=story")
+        cy.get(".card-meta").should("exist")
+
+        cy.percySnapshot("Card Meta: Default")
+    })
+})
