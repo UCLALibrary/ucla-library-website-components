7b0c50e2d99383d1812fdb973974e0b9265731b3
JenDiamond <thejendiamond@gmail.com>
2023-04-12 15:30:11 -0700

    Create PageAnchor.stories.js from Github

---
+++ src/stories/PageAnchor.stories.js
@@ -0,0 +1,31 @@
+import PageAnchor from "../lib-components/PageAnchor.vue"
+
+export default {
+  title: 'PageAnchor',
+  component: PageAnchor,
+  argTypes: {
+    sectionTitles: {
+      control: {
+        type: 'array',
+        separator: ', ',
+      },
+      description: 'An array of section titles to create anchor links for',
+      table: {
+        type: {
+          summary: 'Array<string>',
+        },
+      },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { PageAnchor },
+  template: '<page-anchor v-bind="$props" />',
+});
+
+export const Basic = Template.bind({});
+Basic.args = {
+  sectionTitles: ['Section 1', 'Section 2', 'Section 3'],
+};
