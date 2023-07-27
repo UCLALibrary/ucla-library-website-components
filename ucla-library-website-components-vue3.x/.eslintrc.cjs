7e426d43f026987cae1e226c3547f515b1f0b6aa
Parinita Mulak <pghorpade@library.ucla.edu>
2023-07-27 14:45:30 -0700

    chore: Add eslint (#361)
    * chore: add eslint package
    
    * chore: add eslint with airbnb style guide

---
+++ ucla-library-website-components-vue3.x/.eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module",
+        "project": './tsconfig.json' 
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "vue"
+    ],
+    "rules": {
+    }
+}
