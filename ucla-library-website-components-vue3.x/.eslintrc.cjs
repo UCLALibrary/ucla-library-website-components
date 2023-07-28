56475b445a10cb02174d910ab31c32bc75cd0ca6
pghorpade <pghorpade@library.ucla.edu>
2023-07-28 11:35:43 -0700

    chore: add storybook

--- ucla-library-website-components-vue3.x/.eslintrc.cjs
+++ ucla-library-website-components-vue3.x/.eslintrc.cjs
@@ -1,40 +1,24 @@
 module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": ["airbnb", "airbnb-typescript", "airbnb/hooks", "plugin:cypress/recommended", "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:vue/vue3-essential", "plugin:storybook/recommended"],
+  "overrides": [{
     "env": {
-        "browser": true,
-        "es2021": true
+      "node": true
     },
-    "extends": [
-        "airbnb",
-        "airbnb-typescript",
-        "airbnb/hooks",
-        "plugin:cypress/recommended",
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
+    "files": [".eslintrc.{js,cjs}"],
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module",
-        "project": './tsconfig.json' 
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
-    ],
-    "rules": {
+      "sourceType": "script"
     }
-}
+  }],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "parser": "@typescript-eslint/parser",
+    "sourceType": "module",
+    "project": './tsconfig.json'
+  },
+  "plugins": ["@typescript-eslint", "vue"],
+  "rules": {}
+};
