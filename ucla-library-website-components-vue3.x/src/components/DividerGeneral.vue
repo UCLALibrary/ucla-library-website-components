54bd47541b2335cb50a68fd3a53d8d651aed0cb6
pghorpade <pghorpade@library.ucla.edu>
2023-07-31 11:16:03 -0700

    feat: add divider-general vuecomponent

---
+++ ucla-library-website-components-vue3.x/src/components/DividerGeneral.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="classes" />
+</template>
+
+<script>
+export default {
+    name: "DividerGeneral", // vue component name
+    props: {
+        isBold: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        classes() {
+            return ["divider-general", { "is-bold": this.isBold }]
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.divider-general {
+    max-width: $container-l-main + px;
+    margin: var(--space-2xl) auto;
+    border-bottom: 2px dotted var(--color-secondary-grey-02);
+    height: 1px;
+
+    &.is-bold {
+        border-bottom-color: var(--color-secondary-grey-03);
+    }
+}
+</style>
