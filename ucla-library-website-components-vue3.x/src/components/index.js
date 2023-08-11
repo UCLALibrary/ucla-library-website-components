924a19dc7592e44df04959320bf94a814cd15048
pghorpade <pghorpade@library.ucla.edu>
2023-08-10 17:21:20 -0700

    chore: create build lib configuration to vite

---
+++ ucla-library-website-components-vue3.x/src/components/index.js
@@ -0,0 +1,5 @@
+import "ucla-library-design-tokens/scss/app-global.scss"
+import "@/styles/global.scss"
+
+export { default as DividerGeneral } from "./DividerGeneral.vue"
+export { default as DividerWayFinder } from "./DividerWayFinder.vue"
