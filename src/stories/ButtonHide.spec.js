7738e05422bcb5caf6d994a1a693b9067e974e52
pghorpade <pghorpade@library.ucla.edu>
2023-08-10 19:45:12 -0700

    chore: restructure the files and use vuecli eslint instead

--- src/stories/ButtonHide.spec.js
+++
@@ -1,6 +0,0 @@
-describe("BUTTON / Hide", () => {
-    it("Default", () => {
-        cy.visit("/iframe.html?id=button-hide--default&args=&viewMode=story")
-        cy.get(".button-hide").should("exist")
-    })
-})
