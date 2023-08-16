040e4c2fad69969529dc93a6e240d52ae286fde0
Parinita Mulak <pghorpade@library.ucla.edu>
2023-08-16 16:37:14 -0700

    chore: Add dev server to use component library as a plugin (#374)
    fix: Add dev server to use component library as a plugin

---
+++ dev/serve.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import DividerGeneral from '../src/components/DividerGeneral.vue';
+import DividerWayFinder from '../src/components/DividerWayFinder.vue';
+</script>
+<template>
+    <div id="app" class="app">
+        <h1>
+            This page displays the components in the
+            <a href="https://github.com/UCLALibrary/ucla-library-website-components">
+                ucla-library-website-components npm repo
+            </a>
+        </h1>
+        <hr />
+        <h2>DividerGeneral Component</h2>
+        <divider-general :is-bold="true" />
+        <hr />
+
+        <h2>DividerWayFinder Component</h2>
+        <divider-way-finder color="visit" />
+        <hr />
+    </div>
+</template>
---
+++ vite.config.serve.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "url"
+import { resolve } from "path"
+import svgLoader from "vite-svg-loader"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: "./dev",
+    plugins: [vue(), svgLoader()],
+    css: {
+        preprocessorOptions: {
+            // Additional Sass options go here
+            scss: {
+                additionalData: `
+                  @import "ucla-library-design-tokens/scss/fonts.scss";
+                  @import "ucla-library-design-tokens/scss/app.scss";
+                `,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "~ucla-library-design-tokens": resolve(
+                __dirname,
+                "node_modules/ucla-library-design-tokens"
+            ),
+        },
+    },
+})
