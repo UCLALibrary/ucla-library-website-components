4ca940257fb2f236dc033648515abb02aebbfefc
pghorpade <pghorpade@library.ucla.edu>
2023-08-16 13:55:03 -0700

    fix: Add dev server to use component library as a plugin

---
+++ dev/serve.js
@@ -0,0 +1,16 @@
+import { createApp } from "vue"
+// import store from '@/store';
+import Dev from "./serve.vue"
+import router from "../src/router"
+import UclaLibraryWebsiteComponent from "../src/entry.js"
+
+console.log(Dev)
+const app = createApp(Dev)
+
+// In Vue 3, plugins are used via `app.use()` directly on the app instance
+app.use(UclaLibraryWebsiteComponent)
+
+// app.use(store);
+app.use(router)
+
+app.mount("#app")
