d5b320b1d3aa9f2cd9a9bf6c06633d42d6e6af80
Parinita Mulak <pghorpade@library.ucla.edu>
2023-08-23 11:51:35 -0700

    chore: nuxt recommends pnpm (#376)

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         rollupOptions: {
             external: ["vue"],
             output: {
-                preserveModules: true,
+                // preserveModules: true,
                 exports: "named",
                 globals: {
                     vue: "Vue",
