0aee2ec91cbd3e6d8b55d025b17ba22ddfd2fc27
Andy Wallace <aw@andrewbenedictwallace.com>
2023-09-29 15:57:55 -0400

    feat: "Include past events" single checkbox filter (#401)
    * feat: Remove filter text for "Include past events"
    
    * chore: linter autofixes
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/lib-components/SectionRemoveSearchFilter.vue
+++ src/lib-components/SectionRemoveSearchFilter.vue
@@ -6,14 +6,14 @@ import type { PropType } from 'vue'
 import BlockRemoveSearchFilter from '@/lib-components/BlockRemoveSearchFilter.vue'
 
 interface Item {
-  [key: string]: string[]
+    [key: string]: string[]
 }
 
 const { filters } = defineProps({
-  filters: {
-    type: Object as PropType<Item>,
-    default: () => { },
-  },
+    filters: {
+        type: Object as PropType<Item>,
+        default: () => { },
+    },
 })
 
 const emit = defineEmits(['update:filters', 'remove-selected'])
@@ -21,55 +21,55 @@ const emit = defineEmits(['update:filters', 'remove-selected'])
 const filteredFilters = ref<Item>({})
 
 watch(() => filters, (newFilters) => {
-  Object.entries(newFilters).forEach(([key, value]) => {
-    filteredFilters.value[key] = value
-  })
+    Object.entries(newFilters).forEach(([key, value]) => {
+        filteredFilters.value[key] = value
+    })
 }, { deep: true, immediate: true })
 
 const parsedFilters = computed(() => {
-  const result = Object.entries(filteredFilters.value).flatMap(([name, value]) => {
-    return value.map(item => ({ name, value: item }))
-  })
-  return result
+    const result = Object.entries(filteredFilters.value).flatMap(([name, value]) => {
+        return value.map(item => ({ name, value: item }))
+    })
+    return result
 })
 function getCheckBoxLabel(esFieldName: string) {
-  return esFieldName === 'subjectLibrarian.keyword' ? 'Subject Librarian' : 'Past Events'
+    return esFieldName === 'subjectLibrarian.keyword' ? 'Subject Librarian' : 'Past Events'
 }
 
 function closeBlockFilter(esfieldName: string, label: string | boolean) {
-  filteredFilters.value[esfieldName] = filteredFilters.value[esfieldName].filter(item => item !== label)
-  emit('update:filters', filteredFilters.value)
-  emit('remove-selected')
+    filteredFilters.value[esfieldName] = filteredFilters.value[esfieldName].filter(item => item !== label)
+    emit('update:filters', filteredFilters.value)
+    emit('remove-selected')
 }
 </script>
 
 <template>
-  <div
-    v-show="filteredFilters"
-    class="section-remove-search-filter"
-  >
     <div
-      v-for="(filter) in parsedFilters"
-      :key="`filter-${filter.value}`"
+        v-show="filteredFilters"
+        class="section-remove-search-filter"
     >
-      <BlockRemoveSearchFilter
-        :title="filter.value === 'yes' ? getCheckBoxLabel(filter.name) : filter.value"
-        @remove-block-filter="
-          closeBlockFilter(
-            filter.name,
-            filter.value,
-          )
-        "
-      />
+        <div
+            v-for="(filter) in parsedFilters"
+            :key="`filter-${filter.value}`"
+        >
+            <BlockRemoveSearchFilter
+                :title="filter.value === 'yes' ? getCheckBoxLabel(filter.name) : filter.value"
+                @remove-block-filter="
+                    closeBlockFilter(
+                        filter.name,
+                        filter.value,
+                    )
+                    "
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped lang="scss">
 .section-remove-search-filter {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  flex-wrap: wrap;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
 }
 </style>
