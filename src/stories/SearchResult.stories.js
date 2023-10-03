bead2c3fa7868b17db75dd1c514cc46ffd3bb74d
tinuola <tinuola@users.noreply.github.com>
2023-10-03 15:36:45 -0700

    test: add spec and stories for SearchResult component

---
+++ src/stories/SearchResult.stories.js
@@ -0,0 +1,31 @@
+import SearchResult from "@/lib-components/SearchResult"
+
+export default {
+    title: "SEARCH / Search Result",
+    component: SearchResult,
+}
+
+const mock = {
+    category: "Article",
+    to: "/about/news/article",
+    title: "Lacus Luctus",
+    summary:
+        "Litora torquent per conubia nostra, per inceptos himenaeos.Litora torquent per conubia nostra, per inceptos himenaeos.Litora torquent per conubia nostra, per inceptos himenaeos.Litora torquent per conubia nostra, per inceptos himenaeos.",
+}
+
+export const Default = () => ({
+    data() {
+        return {
+            ...mock,
+        }
+    },
+    components: { SearchResult },
+    template: `
+        <search-result
+            :category="category"
+            :title="title"
+            :summary="summary"
+            :to="to"
+        />
+    `,
+})
