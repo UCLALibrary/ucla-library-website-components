8a93d8d268c80607e9353faeb974450e07a8795c
tinuola <tinuola@users.noreply.github.com>
2023-12-15 12:10:05 -0800

    fix: syntax update

--- src/lib-components/BlockClippedDate.vue
+++ src/lib-components/BlockClippedDate.vue
@@ -18,7 +18,7 @@ import type { ImageItemType, LocationItemType } from '@/types/types'
 const props = defineProps({
   image: {
     type: Object as PropType<ImageItemType>,
-    default: () => ({}),
+    default: () => {},
   },
   to: {
     type: String,
