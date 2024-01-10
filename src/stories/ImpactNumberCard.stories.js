08311b5ab29fc38a5a4746a016b1bbf67f3f88d6
tinuola <tinuola@users.noreply.github.com>
2024-01-10 10:51:18 -0800

    feat: add ImpactNumberCard components

---
+++ src/stories/ImpactNumberCard.stories.js
@@ -0,0 +1,31 @@
+import ImpactNumberCard from '../lib-components/ImpactNumberCard'
+
+export default {
+  title: 'IMPACT REPORT / Impact Number Card',
+  component: ImpactNumberCard,
+}
+
+const mock = {
+  id: '37072',
+  title: 'HathiTrust Partnership',
+  text: 'Pages Viewed Through HathiTrust Digital Library to Support Collections Access',
+  impactNumber: '3,958,321',
+}
+
+export function Default() {
+  return {
+    data() {
+      return {
+        ...mock,
+      }
+    },
+    components: { ImpactNumberCard },
+    template: `
+        <impact-number-card
+            :text="text"
+            :title="title"
+            :impact-number="impactNumber"
+        />
+    `,
+  }
+}
