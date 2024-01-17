e4189ebbb6104620b0572701cc798dd4a51dd075
tinuola <tinuola@users.noreply.github.com>
2024-01-17 11:10:17 -0800

    task: rebase gridGallery component

--- src/lib-components/Flexible/GridGalleryCards.vue
+++ src/lib-components/Flexible/GridGalleryCards.vue
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<!-- eslint-disable no-console -->
->>>>>>> 59ee322 (feat: add flexible-grid-gallery-cards comp, story, spec, types)
 <script setup lang="ts">
 import { computed } from 'vue'
 
@@ -46,23 +42,13 @@ function flattenTimeLineStructure(galleryData: FlexibleGridGalleryItem) {
           ? subitem.image[0]
           : {}
     flattenedValues.push(obj)
-<<<<<<< HEAD
   }
 
-=======
-    console.log(obj)
-  }
-  // console.log(flattenedValues)
->>>>>>> 59ee322 (feat: add flexible-grid-gallery-cards comp, story, spec, types)
   return flattenedValues
 }
 
 const parseGalleryCards = computed(() => {
   const timelineData = flattenTimeLineStructure(block)
-<<<<<<< HEAD
-=======
-  // console.log(timelineData)
->>>>>>> 59ee322 (feat: add flexible-grid-gallery-cards comp, story, spec, types)
   return timelineData
 })
 </script>
