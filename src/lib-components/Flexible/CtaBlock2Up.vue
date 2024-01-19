82a4b0a87970b4bbd0fbe8d6e5c8a0230c55513f
tinuola <tinuola@users.noreply.github.com>
2024-01-18 22:38:38 -0800

    feat: add FlexibleCTA2up comp, story, spec

---
+++ src/lib-components/Flexible/CtaBlock2Up.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import BlockCallToActionTwoUp from '@/lib-components/BlockCallToActionTwoUp.vue'
+
+const { block } = defineProps({
+  block: {
+    type: Object,
+    default: () => {},
+  },
+})
+
+const parsedItems = computed(() => {
+  return block.ctaBlock2Up
+})
+</script>
+
+<template>
+  <section>
+    <BlockCallToActionTwoUp :items="parsedItems" />
+  </section>
+</template>
