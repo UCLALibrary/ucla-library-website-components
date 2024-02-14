47c619081b5f1dfa887da84ef3388def2b7caebf
JenDiamond <thejendiamond@gmail.com>
2024-02-14 12:11:02 -0800

    feat: APPS-2419   Add SectionTeaserCard component to vue3 (#440)
    * Add SectionTeaserCard files, middle of adding the types in type.ts
    
    * lint fix
    
    * working in storybook
    
    * linting (flattening out props in the component)
    
    * linting fixes
    
    * update the Types
    
    * fix linting
    
    * update types
    
    * update types
    
    * comment out .card style in src/style.css
    
    * add comment
    
    * update typescript
    
    * update types
    
    * update typrescript

---
+++ src/lib-components/SectionTeaserCard.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { BlockCardMetaType, EventItemType } from '@/types/types'
+
+import BlockCardWithImage from '@/lib-components/BlockCardWithImage.vue'
+
+const { items } = defineProps({
+  items: {
+    type: Array as PropType<BlockCardMetaType[] & EventItemType[]>,
+    default: () => [],
+  },
+})
+</script>
+
+<template>
+  <ul class="section-teaser-card">
+    <BlockCardWithImage
+      v-for="(item, index) in items"
+      :key="`Card${index}`"
+      :image="item.image"
+      :to="item.to"
+      :category="item.category"
+      :title="item.title"
+      :alternative-full-name="item.alternativeFullName"
+      :language="item.language"
+      :start-date="item.startDate"
+      :end-date="item.endDate"
+      :text="item.text"
+      :image-aspect-ratio="60"
+      :is-vertical="true"
+      :byline-one="item.bylineOne"
+      :byline-two="item.bylineTwo"
+      :section-handle="item.sectionHandle"
+      :ongoing="item.ongoing"
+      class="card"
+    />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.section-teaser-card {
+  max-width: $container-l-main + px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: var(--space-xl) 16px;
+
+  .card {
+    width: calc((100% - 32px) / 3);
+  }
+
+  // Breakpoints
+  @media #{$medium} {
+    align-items: flex-start;
+
+    .card {
+      width: calc((100% - 16px) / 2);
+    }
+  }
+
+  @media #{$small} {
+    align-items: center;
+
+    .card {
+      width: 100%;
+    }
+  }
+}
+</style>
