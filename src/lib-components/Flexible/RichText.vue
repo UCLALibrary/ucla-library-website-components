f4458a413c0be07c2d1ae360bd7cfdffd0870aab
jendiamond <thejendiamond@gmail.com>
2024-02-23 10:30:18 -0800

    set up flexibleRichText files

---
+++ src/lib-components/Flexible/RichText.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { PropType } from 'vue'
+
+import type RichText from "@/types/flexible_types"
+
+// COMPONENTS
+import RichText from '@/lib-components/RichText.vue'
+
+
+const { block } = defineProps({
+  block: {
+    type: Object,
+    default: () => { },
+  },
+})
+
+const parsedContent = computed(() => {
+  const block = []
+  return this.block.richText
+})
+
+// computed: {
+//   parsedContent() {
+//     return this.block.richText
+//   },
+// },
+
+</script>
+
+<template>
+  <div class="rich-text">
+    <h2
+      v-if="block.sectionTitle"
+      class="section-title"
+      v-html="block.sectionTitle"
+    />
+
+    <RichText :rich-text-content="parsedContent" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.rich-text {
+  .section-title {
+    @include step-3;
+    color: var(--color-primary-blue-03);
+    margin-bottom: var(--space-xl);
+  }
+}
+</style>
