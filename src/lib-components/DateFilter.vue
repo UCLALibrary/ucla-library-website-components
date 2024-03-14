5b1c7e3bf3a6155947c8ca020d096bae6200c036
Jess Divers <jessica.divers@library.ucla.edu>
2024-03-14 12:10:12 -0700

    feat: customize datepicker functions

--- src/lib-components/DateFilter.vue
+++ src/lib-components/DateFilter.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { ref, watch, defineAsyncComponent } from 'vue';
 import type { PropType } from 'vue'
 import VueDatePicker from '@vuepic/vue-datepicker';
+import type { DatePickerInstance } from "@vuepic/vue-datepicker"
 import '@vuepic/vue-datepicker/dist/main.css'
+import SvgIconCaretLeft from 'ucla-library-design-tokens/assets/svgs/icon-caret-left.svg'
+import SvgIconCaretRight from 'ucla-library-design-tokens/assets/svgs/icon-caret-right.svg'
 
 // PROPS & DATA
 /* currently App is sending this data as props
@@ -10,6 +13,7 @@ const mockDateFilterData = {
   initialDates: { start: '3/8/2024', end: '3/10/2024' },
   eventDates: ['3/1/2024', '3/2/2024', '3/2/2024', '3/4/2024', '3/6/2024', '3/8/2024', '3/19/2024', '3/19/2024',],
 }
+// QUESTION: does this component need to have the ability to select initial dates?
 */
 const { initialDates, eventDates } = defineProps({
     initialDates: {
@@ -23,10 +27,11 @@ const { initialDates, eventDates } = defineProps({
     },
 })
 
-// TODO TODO
+// TODO TODO do we need to pass initial selection?
 // if we have initial dates in array, set date to initialDates
 // const date = ref(initialDates.length > 0 ? initialDates : [])
 const date = ref()
+const datepicker = ref<DatePickerInstance | null>(null)
 watch(date, async (newDate, oldDate) => {
     if (newDate !== oldDate) {
         console.log('newDate', newDate);
@@ -46,28 +51,148 @@ function calcDateFrequency(dateArray: string[]) {
     return obj;
 }
 const dateFrequency = calcDateFrequency(eventDates);
+
+// METHODS
+const goToToday = () => {
+    // select today's date
+    date.value = [new Date(), null];
+};
+const clearDate = () => {
+    datepicker.value?.updateInternalModelValue(null);
+};
+const threeLetterDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+//Components
+const ButtonLink = defineAsyncComponent(() =>
+    import('@/lib-components/ButtonLink.vue'))
 </script>
 
 <template>
-    <VueDatePicker v-model="date" range class="date-filter">
+    <VueDatePicker v-model="date" ref="datepicker" range :week-start="0" :month-name-format="'long'"
+        :enable-time-picker="false" class="date-filter" placeholder="All upcoming">
+        <template #month-year="{
+        month,
+        year,
+        months,
+        handleMonthYearChange
+    }">
+            <div class="custom-header">
+                <div class="custom-month-year-component">
+                    {{ months[month].text }} {{ year }}
+                </div>
+                <div class="custom-nav-buttons">
+                    <button class="nav-arrow-button" @click="handleMonthYearChange(false)">
+                        <SvgIconCaretLeft />
+                    </button>
+                    <button class="today-button" @click="goToToday">
+                        TODAY
+                    </button>
+                    <button class="nav-arrow-button" @click="handleMonthYearChange(true)">
+                        <SvgIconCaretRight />
+                    </button>
+                </div>
+            </div>
+        </template>
+
+        <template #calendar-header="{ index, day }">
+            <div class="day-header">
+                {{ threeLetterDays[index] }}
+            </div>
+        </template>
+
         <template #day="{ day, date }">
-            {{ day }}
-            <template v-if="dateFrequency.hasOwnProperty(date.toLocaleDateString())" class="event-dots">
-                <span v-for="index in dateFrequency[date.toLocaleDateString()]" :key="index">
-                    X
-                </span>
-            </template>
+            <div class="day-content">
+                {{ day }}
+                <div v-if="dateFrequency.hasOwnProperty(date.toLocaleDateString())" class="event-dots">
+                    <template v-for="index in dateFrequency[date.toLocaleDateString()]" :key="index">
+                        <span class="dot"></span>
+                    </template>
+                </div>
+            </div>
+        </template>
+
+        <template #action-row="{ selectDate }">
+            <div class="action-row">
+                <!-- <button class="select-button" @click="selectDate">Done</button>
+                <button class="close-button" @click="clearDate()">Clear X</button> -->
+                <ButtonLink class="select-button" @click="selectDate" label="Done" />
+                <ButtonLink class="clear-button" @click="clearDate()" label="Clear" icon-name="icon-close"
+                    :is-quaternary="true" />
+            </div>
         </template>
     </VueDatePicker>
 </template>
 
 <style lang="scss" scoped>
 .date-filter {
-    .event-dots {
+    .custom-header {
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
         align-items: center;
-        gap: 2px;
+        width: 100%;
+
+        .custom-nav-buttons {
+            display: flex;
+            gap: 2px;
+            align-items: center;
+
+            .today-button,
+            .nav-arrow-button {
+                background-color: var(--color-primary-blue-01);
+                border: none;
+                border-radius: 0px;
+                padding: 0;
+                cursor: pointer;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 31px;
+                transition: background-color 0.3s ease;
+
+                &:hover {
+                    background-color: var(--color-primary-blue-02);
+                }
+            }
+
+            .today-button {
+                color: var(--color-primary-blue-03);
+                width: 81px;
+            }
+
+            .nav-arrow-button {
+                color: black;
+                width: 25px;
+            }
+
+            :deep(.svg__stroke--primary-blue-03) {
+                stroke: var(--color-black);
+            }
+        }
+    }
+
+    .day-content {
+        display: flex;
+        position: relative;
+        flex-direction: column;
+        width: 100%;
+        margin-top: -5px;
+
+        .event-dots {
+            position: absolute;
+            width: 100%;
+            bottom: -3px;
+            display: flex;
+            flex-direction: row;
+            gap: 3px;
+            justify-content: center;
+
+            .dot {
+                width: 5px;
+                height: 5px;
+                border-radius: 50%;
+                background-color: var(--color-primary-blue-03);
+            }
+        }
     }
 }
 </style>
