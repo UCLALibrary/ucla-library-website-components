b21446c910d0236507fc0e7697ef1c575eed0d47
Jess Divers <jessica.divers@library.ucla.edu>
2024-04-02 15:35:58 -0700

    fix: run build and lint

--- src/lib-components/BlockHours.vue
+++ src/lib-components/BlockHours.vue
@@ -1,58 +1,60 @@
 <script lang="ts" setup>
-import { computed, onMounted } from "vue"
+import { computed, onMounted } from 'vue'
 import ButtonLink from '@/lib-components/ButtonLink.vue'
-//Props
+
+// Props
 const props = defineProps({
-    lid: {
-        type: String,
-        default: "",
-    },
-    isClicc: {
-        type: Boolean,
-        default: false,
-    },
+  lid: {
+    type: String,
+    default: '',
+  },
+  isClicc: {
+    type: Boolean,
+    default: false,
+  },
 })
 // Computed
 const parsedSrc = computed(() => {
-    if (!props.isClicc) {
-        return `https://www.library.ucla.edu/blockHours.html?lid=${props.lid}`
-    } else {
-        return `https://www.library.ucla.edu/blockCliccHours.html?lid=0`
-    }
+  if (!props.isClicc)
+    return `https://www.library.ucla.edu/blockHours.html?lid=${props.lid}`
+  else
+    return 'https://www.library.ucla.edu/blockCliccHours.html?lid=0'
 })
 // Mounted
 onMounted(() => {
-    window.addEventListener(
-        "message",
-        function (e) {
-            var hours_iframe = document.getElementById("the-iframe")
-            console.log("FIRST" + hours_iframe?.height) // TODO REMOVE
-            var eventName = e.data[0]
-            var data = e.data[1]
-            // console.log("DATA: " + hours_iframe)
-            console.log("TEST" + JSON.stringify(hours_iframe))
-            console.log("EVENT" + eventName)
-            switch (eventName) {
-                case "setHeight":
-                    hours_iframe.height = data + 20
-                    console.log("SECOND" + hours_iframe?.height)
-                    break
-            }
-        },
-        false
-    )
+  window.addEventListener(
+    'message',
+    (e) => {
+      const hours_iframe = document.getElementById('the-iframe') as HTMLIFrameElement
+      const eventName = e.data[0]
+      const data = e.data[1]
+      switch (eventName) {
+        case 'setHeight':
+          hours_iframe.height = data + 20
+          break
+      }
+    },
+    false
+  )
 })
 </script>
+
 <template>
-    <div class="block-hours">
-        <h3 class="title">Hours</h3>
-        <div class="content">
-            <iframe id="the-iframe" title="Hours for location" refs="hours_iframe" class="iframe" :src="parsedSrc"
-                frameBorder="0" width="100%" height="100%" />
-            <ButtonLink label="All Library Hours" :is-secondary="true" to="https://calendar.library.ucla.edu/hours"
-                class="btn-lnk" />
-        </div>
+  <div class="block-hours">
+    <h3 class="title">
+      Hours
+    </h3>
+    <div class="content">
+      <iframe
+        id="the-iframe" title="Hours for location" class="iframe" :src="parsedSrc" frameBorder="0"
+        width="100%" height="100%"
+      />
+      <ButtonLink
+        label="All Library Hours" :is-secondary="true" to="https://calendar.library.ucla.edu/hours"
+        class="btn-lnk"
+      />
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
