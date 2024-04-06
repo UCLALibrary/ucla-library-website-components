4f6b97fcbaa6cae1a1fe8d6f0e7e6baa18c91411
JenDiamond <thejendiamond@gmail.com>
2024-04-05 17:00:54 -0700

    feat: APPS-2633_APPS-2636 Convert BlockStaffList & SectionStaffList to vue3 (#490)
    * add files for BlockStaffList & SectionStaffList
    
    * force
    
    * update typescript
    
    * update typescript
    
    * linting
    
    * updates
    
    * update typescript
    
    * linting

---
+++ src/lib-components/SectionStaffList.vue
@@ -0,0 +1,56 @@
+<script
+  setup
+  lang="ts"
+>
+import type { PropType } from 'vue'
+
+// TYPESCRIPT
+import type { BlockStaffListItemType } from '@/types/types'
+
+// COMPONENTS
+import BlockStaffList from '@/lib-components/BlockStaffList.vue'
+
+const { items } = defineProps({
+  items: {
+    type: Array as PropType<BlockStaffListItemType[]>,
+    default: () => [],
+  },
+})
+</script>
+
+<template>
+  <div>
+    <ul class="section-staff-list">
+      <BlockStaffList
+        v-for="item in items"
+        :key="`ucla-staff-${item.to}`"
+        :image="item.image"
+        :to="item.to"
+        :staff-name="item.staffName"
+        :alternative-full-name="item.alternativeName"
+        :language="item.language"
+        :job-title="item.jobTitle"
+        :locations="item.locations"
+        :email="item.email"
+        :phone="item.phone"
+        :departments="item.departments"
+        :consultation="item.consultation"
+        class="block-staff-list-item"
+      />
+    </ul>
+  </div>
+</template>
+
+<style
+  lang="scss"
+  scoped
+>
+.section-staff-list {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-m);
+
+  max-width: $container-l-main + px;
+  margin: 0 auto var(--space-2xl);
+}
+</style>
