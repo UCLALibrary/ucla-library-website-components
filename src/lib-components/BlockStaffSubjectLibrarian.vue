0d6cb4ddf0b17a3ec1e0100b43f00247a5a3f55c
JenDiamond <thejendiamond@gmail.com>
2024-04-10 09:41:21 -0700

    feat: APPS-2583-2582 Fix block StaffSubjectLibrarian & Convert SectionStaffSubjectLibrarian to vue3 (#494)
    * fix BlockSubjectLibrarian add SectionSubjectLibrarian
    
    * fix BlockSubjectLibrarian add SectionSubjectLibrarian
    
    * update typescript
    
    * update typescript
    
    * update typescript
    
    * delete comment
    
    * fix staffName
    
    * linting
    
    * linting

--- src/lib-components/BlockStaffSubjectLibrarian.vue
+++ src/lib-components/BlockStaffSubjectLibrarian.vue
@@ -1,19 +1,26 @@
-<script lang="ts" setup>
-import { defineAsyncComponent } from 'vue'
+<script
+  lang="ts"
+  setup
+>
+import { computed } from 'vue'
+import type { PropType } from 'vue'
 
-// Components
+// LODASH FUNCTIONS
+
+// TYPESCRIPT
+import type { AcademicDepartmentsItemType, AlternativeNameItemType, DepartmentItemType, StaffLocationItemType } from '@/types/types'
+
+// COMPONENTS
 import SmartLink from '@/lib-components/SmartLink.vue'
+import IconWithLink from '@/lib-components/IconWithLink.vue'
 
-defineProps({
+// PROPS & DATA
+const props = defineProps({
   subjectArea: {
     type: String,
     default: '',
   },
   to: {
-    type: String,
-    default: '',
-  },
-  staffName: {
     type: String,
     default: '',
   },
@@ -25,17 +32,25 @@ defineProps({
     type: String,
     default: '',
   },
-  academicDepartments: {
-    type: Array,
+  jobTitle: {
+    type: String,
+    default: '',
+  },
+  departments: {
+    type: Array as PropType<DepartmentItemType[]>,
     default: () => [],
   },
-  alternativeFullName: {
-    type: String,
-    default: '',
+  academicDepartments: {
+    type: Array as PropType<AcademicDepartmentsItemType[]>,
+    default: () => [],
   },
-  language: {
-    type: String,
-    default: '',
+  locations: {
+    type: Array as PropType<StaffLocationItemType[]>,
+    default: () => [],
+  },
+  alternativeName: {
+    type: Array as PropType<AlternativeNameItemType[]>,
+    default: () => [],
   },
   uri: {
     type: String,
@@ -53,47 +68,126 @@ defineProps({
     type: String,
     default: '',
   },
+  language: {
+    type: String,
+    default: '',
+  }
 })
 
-const IconWithLink = defineAsyncComponent(
-  () => import('@/lib-components/IconWithLink.vue')
-)
+const lastDepartment = computed(() => {
+  const dept = props.departments
+  return dept[dept.length - 1].title
+})
+
+const parsedAlternativeFullName = computed(() => {
+  return props.alternativeName[0].fullName
+})
+
+const parsedLanguage = computed(() => {
+  return props.alternativeName[0].languageAltName
+})
+
+const parsedStaffName = computed(() => {
+  return `${props.nameFirst} ${props.nameLast}`
+})
 </script>
 
 <template>
-  <tr class="block-staff-subject-librarian">
-    <!-- SUBJECT AREA -->
-    <td class="academic-department">
-      {{ subjectArea }}
-    </td>
+  <div>
+    <tr class="block-staff-subject-librarian">
+      <!-- SUBJECT AREA -->
+      <td class="academic-department">
+        {{ subjectArea }}
+      </td>
 
-    <!-- NAME -->
-    <td class="librarian-block">
-      <SmartLink :to="to" class="staff-name">
-        {{ staffName }}
-        <span v-if="alternativeFullName" :lang="language">
-          {{ alternativeFullName }}</span>
-      </SmartLink>
-    </td>
+      <!-- NAME -->
+      <td class="librarian-block">
 
-    <!-- CONTACT INFO -->
-    <td class="contact-info">
-      <div class="email">
-        <IconWithLink :text="email" icon-name="svg-icon-email" :to="`mailto:${email}`" />
-      </div>
+        <SmartLink
+          :to="to"
+          class="staff-name"
+          v-if="props.alternativeName.length === 0 || props.alternativeName === null"
+        >
+          {{ parsedStaffName }}
+        </SmartLink>
 
-      <div v-if="phone" class="phone">
-        <IconWithLink :text="phone" icon-name="svg-icon-phone" :to="`tel:${phone}`" />
-      </div>
+        <SmartLink
+          :to="to"
+          class="staff-name"
+          v-else
+        >
+          {{ parsedStaffName }}
+          <span
+            :lang="parsedLanguage"
+            v-if="props.alternativeName && props.alternativeName != null"
+          >
+            {{ parsedAlternativeFullName }}</span>
+        </SmartLink>
+        <div
+          class="job-title"
+          v-html="jobTitle"
+        />
 
-      <div v-if="consultation" class="consultation">
-        <IconWithLink text="Book a consultation" icon-name="svg-icon-consultation" :to="consultation" />
-      </div>
-    </td>
-  </tr>
+        <ul
+          v-if="departments.length"
+          class="departments"
+        >
+          <li class="department">
+            {{ lastDepartment }}
+          </li>
+        </ul>
+
+        <div v-if="locations.length">
+          <IconWithLink
+            v-for="  location   in   locations  "
+            :key="`location-${location.id}`"
+            :text="location.title"
+            icon-name="svg-icon-location"
+            :to="`/${location.to}`"
+          />
+        </div>
+      </td>
+
+      <!-- CONTACT INFO -->
+      <td class="contact-info">
+        <div class="email">
+          <IconWithLink
+            :text="email"
+            icon-name="svg-icon-email"
+            :to="`mailto:${email}`"
+          />
+        </div>
+
+        <div
+          v-if="phone"
+          class="phone"
+        >
+          <IconWithLink
+            :text="phone"
+            icon-name="svg-icon-phone"
+            :to="`tel:${phone}`"
+          />
+        </div>
+
+        <div
+          v-if="consultation"
+          class="consultation"
+        >
+          <IconWithLink
+            text="Book a consultation"
+            icon-name="svg-icon-consultation"
+            :to="consultation"
+          />
+        </div>
+      </td>
+    </tr>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style
+  lang="scss"
+  scoped
+>
 .block-staff-subject-librarian {
   display: flex;
   flex-direction: row;
