33f2f77456bf63d9fa6f33b46202a55eae1e889b
pghorpade <pghorpade@library.ucla.edu>
2024-04-19 15:09:03 -0700

    fix: Add sample search generic component to under masthead

--- src/lib-components/ExampleMeapNavMastHead.vue
+++ src/lib-components/ExampleMeapNavMastHead.vue
@@ -1,5 +1,6 @@
 <script>
-// import MastheadSecondary from "@/lib-components/MastheadSecondary"
+import MastheadSecondary from "@/lib-components/MastheadSecondary"
+import SearchGeneric from "@/lib-components/SearchGeneric"
 import { mapState } from 'pinia'
 import HeaderSmart from '@/lib-components/HeaderSmart'
 
@@ -9,14 +10,87 @@ import { useGlobalStore } from '@/stores/GlobalStore'
 export default {
   name: 'ExampleMeapNavMastHead',
   components: {
-    // MastheadSecondary,
+    MastheadSecondary,
     HeaderSmart,
+    SearchGeneric,
   },
   props: {
     title: {
       type: String,
       default: '',
     },
+    isSearch: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      items: [
+        {
+          name: 'Neque porro quisquam',
+        },
+        {
+          name: 'Qui dolorem ipsum quia dolor',
+        },
+        {
+          name: 'Consectetur, adipisci velit.',
+        },
+        {
+          name: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        },
+        {
+          name: 'Excepteur sint occaecat cupidatat non proident',
+        },
+        {
+          name: 'Vel illum qui dolorem eum fugiat',
+        },
+        {
+          name: 'Quis nostrum exercitationem ullam',
+        },
+        {
+          name: 'Quis autem vel eum iure reprehenderit',
+        },
+        {
+          name: 'Excepteur sint occaecat cupidatat non proident1',
+        },
+        {
+          name: 'Vel illum qui dolorem eum fugiat1',
+        },
+        {
+          name: 'Quis nostrum exercitationem ullam1',
+        },
+        {
+          name: 'Quis autem vel eum iure reprehenderit1',
+        },
+      ],
+
+      searchFilters: [
+        {
+          label: 'Location',
+          esFieldName: 'location',
+          inputType: 'radio',
+          items: this.items
+        },
+        {
+          label: 'Department',
+          esFieldName: 'departments.title.keyword',
+          inputType: 'checkbox',
+          items: this.items
+        },
+        {
+          label: 'Subject Librarian',
+          esFieldName: 'subjectLibrarian.keyword',
+          inputType: 'single-checkbox',
+        },
+      ],
+
+      searchGenericQuery: {
+        queryText: '',
+        queryFilters: {},
+      }
+
+    }
   },
   computed: {
     ...mapState(useGlobalStore, ['header', 'winWidth']),
@@ -33,6 +107,11 @@ export default {
       return this.isMobile ? 'header-main-responsive' : 'header-main'
     },
   },
+  methods: {
+    getSearchData() {
+      console.log("search is ready")
+    }
+  },
 }
 </script>
 
@@ -41,20 +120,29 @@ export default {
     <HeaderSmart title="Modern Endangered Archives Program" />
 
     <main id="main">
-      <!-- masthead-secondary
-                title="Modern Endangered Archives Program"
-                text="Our grants enable digitization and access to at-risk cultural heritage collections from around the world. Explore our projects and learn more about available grant opportunities."
-                theme="meap"
-            / -->
+      <masthead-secondary
+        title="Modern Endangered Archives Program"
+        text="Our grants enable digitization and access to at-risk cultural heritage collections from around the world. Explore our projects and learn more about available grant opportunities."
+        theme="meap"
+      />
+      <search-generic
+        v-if="isSearch"
+        search-type="about"
+        :filters="searchFilters"
+        class="generic-search"
+        :search-generic-query="searchGenericQuery"
+        :placeholder="Test"
+        @search-ready="getSearchData"
+      />
     </main>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @media #{$medium} {
-    .brand-bar {
-        position: absolute;
-        width: 100%;
-    }
+  .brand-bar {
+    position: absolute;
+    width: 100%;
+  }
 }
 </style>
