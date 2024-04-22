38e05cffbcb38c6aed94790848eb70148331afe3
JenDiamond <thejendiamond@gmail.com>
2024-04-22 15:55:24 -0700

    feat: APPS-2632 convert block & section PostSmall to vue3 (#498)
    * PostSmall files
    
    * update BlockPostSmall Stories
    
    * update scetionPostSmall stories
    
    * linting
    
    * linting
    
    * change image to media in Responsive image
    
    * fix typo
    
    * fix typo
    
    * update keys
    
    * update SectionPostSmall.vue
    
    * section stories
    
    * linting
    
    * update sectionpostsmall
    
    * update sectionpostsmall
    
    * fixed error
    
    * add types
    
    * linting
    
    * sectionPostStories update
    
    * update section
    
    * linting
    
    * categoryName
    
    * update sectionPostSmall to
    
    * button more
    
    * button more
    
    * stories
    
    * fix unwanted change
    
    * update story

---
+++ src/lib-components/SectionPostSmall.vue
@@ -0,0 +1,89 @@
+<script
+  setup
+  lang="ts"
+>
+import type { PropType } from 'vue'
+import type { MediaItemType } from '@/types/types'
+
+// COMPONENTS
+import BlockPostSmall from '@/lib-components/BlockPostSmall.vue'
+import ButtonMore from '@/lib-components/ButtonMore.vue'
+
+// TYPES
+interface PostSmallItemType {
+  image: MediaItemType
+  categoryName: string
+  author: string
+  title: string
+  to: string
+}
+
+const { items } = defineProps({
+  items: {
+    type: Array as PropType<PostSmallItemType[]>,
+    default: () => [],
+  },
+  to: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <section class="section-post-small">
+    <div class="grid">
+      <BlockPostSmall
+        v-for="item in items"
+        :key="`block-post-${item.title}`"
+        :category-name="item.categoryName"
+        :author="item.author"
+        :title="item.title"
+        :image="item.image"
+        :to="item.to"
+        class="block"
+      />
+    </div>
+
+    <smart-link
+      v-if="to"
+      class="more"
+      :to="to"
+    >
+      <ButtonMore />
+    </smart-link>
+  </section>
+</template>
+
+<style
+  lang="scss"
+  scoped
+>
+.section-post-small {
+  background-color: var(--color-white);
+  padding: 0 var(--unit-gutter);
+
+  .grid {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+
+    max-width: var(--unit-content-width);
+    margin: 0 auto;
+  }
+
+  .block {
+    margin-bottom: 10px;
+  }
+
+  .more {
+    display: block;
+    padding: 30px 40px;
+    margin: 0 auto;
+    width: max-content;
+  }
+}
+</style>
---
+++ src/stories/BlockPostSmall.stories.js
@@ -0,0 +1,140 @@
+// Import mock api data
+import * as API from '@/stories/mock-api.json'
+import BlockPostSmall from '@/lib-components/BlockPostSmall'
+
+// Storybook default settings
+export default {
+  title: 'BLOCK / Post Small',
+  component: BlockPostSmall,
+}
+
+const mockDefault = {
+  image: API.image,
+  to: '/foo/bar',
+  categoryName: 'squirrels',
+  author: 'Fluffy Ranger',
+  title: 'What to Feed Squirrels',
+}
+
+const mockVisit = {
+  image: API.image,
+  to: '/visit/foo/bar/',
+  categoryName: 'wild pets',
+  author: 'Dexter Diamond',
+  title: 'Introducing Mammals to Young Naturalists',
+}
+
+const mockAbout = {
+  image: API.image,
+  to: '/about/foo/bar',
+  categoryName: 'red squirrels',
+  author: 'Penelope Nutstop',
+  title: 'Twenty-two Things About Flying Squirrels',
+}
+
+const mockHelp = {
+  image: API.image,
+  to: '/help/foo/bar/',
+  categoryName: 'gray squirrels',
+  author: 'Smokey Smith',
+  title: 'Squirrels: Diet, Habits & Other Facts',
+}
+
+const mockLongText = {
+  image: API.image,
+  to: '/visit/foo/bar/',
+  categoryName: 'wild squirrels',
+  author: 'Fluffy Ranger',
+  title: 'Six Stupendous Reasons to Appreciate the Heck Out of these surprisingly brainy, aerodynamic, nut-crazed squirrels',
+}
+
+// Variations of stories below
+export function Default() {
+  return {
+    data() {
+      return {
+        item: {
+          ...mockDefault,
+        },
+      }
+    },
+    components: { BlockPostSmall },
+    template: `
+      <block-post-small
+        v-bind="item"
+      />
+  `,
+  }
+}
+
+export function Visit() {
+  return {
+    data() {
+      return {
+        item: {
+          ...mockVisit,
+        },
+      }
+    },
+    components: { BlockPostSmall },
+    template: `
+      <block-post-small
+        v-bind="item"
+      />
+  `,
+  }
+}
+
+export function About() {
+  return {
+    data() {
+      return {
+        item: {
+          ...mockAbout,
+        },
+      }
+    },
+    components: { BlockPostSmall },
+    template: `
+      <block-post-small
+        v-bind="item"
+      />
+  `,
+  }
+}
+
+export function Help() {
+  return {
+    data() {
+      return {
+        item: {
+          ...mockHelp,
+        },
+      }
+    },
+    components: { BlockPostSmall },
+    template: `
+      <block-post-small
+        v-bind="item"
+      />
+  `,
+  }
+}
+
+export function LongText() {
+  return {
+    data() {
+      return {
+        item: {
+          ...mockLongText,
+        },
+      }
+    },
+    components: { BlockPostSmall },
+    template: `
+      <block-post-small
+        v-bind="item"
+      />
+  `,
+  }
+}
