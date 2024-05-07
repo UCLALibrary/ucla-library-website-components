0291e00f36770e5bbbebd5666b20280e8eee2054
Jess Divers <jessica.divers@library.ucla.edu>
2024-05-07 16:18:36 -0700

    fix: query param in searchhome

--- src/stories/SearchHome.spec.js
+++ src/stories/SearchHome.spec.js
@@ -7,4 +7,11 @@ describe('Search / Search Home', () => {
 
     cy.percySnapshot('Search / Home: Default')
   })
+  it('uses q for query parameter', () => {
+    cy.visit('/iframe.html?id=search-search-home--default&args=&viewMode=story')
+    // click the library search tab, then submit empty search
+    cy.contains('button', 'Site Search').click()
+    cy.get('button[class="button-submit"]').click()
+    cy.url().should('include', '?q=')
+  })
 })
