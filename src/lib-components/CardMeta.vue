2a664e063462b5d148e1312d63ed47efcdbf40d8
jendiamond <thejendiamond@gmail.com>
2024-06-25 16:17:32 -0700

    add stories

--- src/lib-components/CardMeta.vue
+++ src/lib-components/CardMeta.vue
@@ -3,6 +3,7 @@
   lang="ts"
 >
 import { computed } from 'vue'
+import { useTheme } from '@/composables/useTheme'
 import { useRoute } from 'vue-router'
 import type { PropType } from 'vue'
 
@@ -10,6 +11,7 @@ import type { PropType } from 'vue'
 import SmartLink from '@/lib-components/SmartLink.vue'
 import IconWithLink from '@/lib-components/IconWithLink.vue'
 import RichText from '@/lib-components/RichText.vue'
+import BlockTag from '@/lib-components/BlockTag.vue'
 
 // UTILITY FUNCTIONS
 import formatTimes from '@/utils/formatEventTimes'
@@ -73,13 +75,24 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  screeningTags: {
+  tagLabels: {
     type: Array as PropType<ScreeningTagItemType[]>,
     default: () => [],
   },
+  introduction: {
+    type: String,
+    default: '',
+  },
+  isCentered: {
+    type: Boolean,
+    default: false,
+  }
 })
 
 const route = useRoute()
+
+// THEME
+const theme = useTheme()
 
 const isImpactReport = computed(() => {
   return !!(route !== undefined && route.path.includes('impact'))
@@ -115,99 +128,120 @@ const parsedLocations = computed(() => {
     }
   })
 })
+
+const parsedIntroduction = computed(() => {
+  if (props.introduction)
+    return `Presented by ${props.introduction}`
+})
 </script>
 
 <template>
   <div class="card-meta">
     <div
-      v-if="category"
-      class="category"
-      v-html="category"
-    />
-    <SmartLink
-      v-if="to"
-      :link-target="parsedTarget"
-      :to="to"
-      class="title"
-    >
-      {{ title }}
-      <span
-        v-if="alternativeFullName"
-        :lang="language"
-        class="translation"
-        v-html="alternativeFullName"
-      />
-    </SmartLink>
-
-    <h3
-      v-else
-      class="title-no-link"
-      v-html="title"
-    />
-
-    <div
-      v-if="bylineOne || bylineTwo"
-      class="byline-group"
+      v-if="isCentered"
+      class="centered"
     >
       <div
-        v-if="bylineOne"
-        class="schedule-item"
-        v-html="bylineOne"
+        v-if="category"
+        class="category"
+        v-html="category"
       />
-      <div
-        v-if="bylineTwo"
-        class="schedule-item"
-        v-html="bylineTwo"
-      />
-    </div>
+      <SmartLink
+        v-if="to"
+        :link-target="parsedTarget"
+        :to="to"
+        class="title"
+      >
+        {{ title }}
+        <span
+          v-if="alternativeFullName"
+          :lang="language"
+          class="translation"
+          v-html="alternativeFullName"
+        />
+      </SmartLink>
 
-    <div
-      v-if="startDate || ongoing"
-      class="date-time"
-    >
-      <div v-if="ongoing">
-        Ongoing
+      <h3
+        v-else
+        class="title-no-link"
+        v-html="title"
+      />
+
+      <div
+        v-if="bylineOne || bylineTwo"
+        class="byline-group"
+      >
+        <div
+          v-if="bylineOne"
+          class="schedule-item"
+          v-html="bylineOne"
+        />
+        <div
+          v-if="bylineTwo"
+          class="schedule-item"
+          v-html="bylineTwo"
+        />
       </div>
-      <time
-        v-if="startDate"
-        class="schedule-item"
-        v-html="parsedDate"
+
+      <div
+        v-if="startDate || ongoing"
+        class="date-time"
+      >
+        <div v-if="ongoing">
+          Ongoing
+        </div>
+        <time
+          v-if="startDate"
+          class="schedule-item"
+          v-html="parsedDate"
+        />
+        <time
+          v-if="startDate"
+          class="schedule-item"
+          v-html="parsedTime"
+        />
+      </div>
+
+      <div
+        v-if="locations.length"
+        class="location-group"
+      >
+        <IconWithLink
+          v-for="(location, index) in parsedLocations"
+          :key="`location-card-meta${index}`"
+          :text="location.title"
+          :icon-name="location.svg"
+          :to="location.to"
+        />
+      </div>
+
+      <div
+        v-if="tagLabels.length"
+        class="block-tags"
+      >
+        <BlockTag
+          v-for="tag in tagLabels"
+          :key="`tag-${tag.title}`"
+          :title="title"
+          class="tag-label"
+        />
+      </div>
+
+      <div
+        v-if="introduction"
+        class="introduction"
+        v-html="parsedIntroduction"
       />
-      <time
-        v-if="startDate"
-        class="schedule-item"
-        v-html="parsedTime"
+
+      <!--  -->
+      <slot></slot>
+
+      <RichText
+        v-if="text"
+        class="text"
+        :rich-text-content="text"
       />
     </div>
-
-    <div
-      v-if="locations.length"
-      class="location-group"
-    >
-      <IconWithLink
-        v-for="(location, index) in parsedLocations"
-        :key="`location-card-meta${index}`"
-        :text="location.title"
-        :icon-name="location.svg"
-        :to="location.to"
-      />
-    </div>
-
-    <div class="block-tag">
-      <BlockTag
-        v-for="item in items"
-        :key="`block-post-${item.title}`"
-        :label="label"
-        :iconName="iconName"
-        class="block-tag"
-      />
-    </div>
-
-    <RichText
-      v-if="text"
-      class="text"
-      :rich-text-content="text"
-    />
   </div>
 </template>
 
@@ -215,100 +249,5 @@ const parsedLocations = computed(() => {
   lang="scss"
   scoped
 >
-.card-meta {
-  .meta {
-    z-index: 10;
-    width: 100%;
-  }
-
-  .category {
-    @include overline;
-    color: var(--color-primary-blue-05);
-    margin-top: var(--space-xs);
-    margin-bottom: var(--space-s);
-  }
-
-  .title {
-    @include card-clickable-area;
-    display: block;
-
-    .translation {
-      display: block;
-    }
-  }
-
-  .title,
-  .title-no-link {
-    @include step-1;
-    color: var(--color-primary-blue-03);
-    margin: var(--space-s) 0;
-    line-height: $line-height--1;
-  }
-
-  .date-time {
-    @include step-0;
-    color: var(--color-secondary-grey-05);
-    margin: $component-02 + px 0 var(--space-s);
-    display: flex;
-    flex-direction: column;
-
-    .svg-online {
-      margin-bottom: -5px;
-      margin-left: 10px;
-      padding-left: 10px;
-      border-left: 1px solid var(--color-secondary-grey-02);
-    }
-  }
-
-  .byline-group {
-    display: flex;
-    flex-direction: column;
-    @include step-0;
-    color: var(--color-secondary-grey-04);
-    margin: var(--space-s) 0;
-  }
-
-  .text {
-    @include step-0;
-    color: var(--color-black);
-    @include truncate(4);
-    max-width: none;
-    padding-right: 0;
-    margin-top: var(--space-s);
-
-    :deep(strong) {
-      font-weight: 500;
-    }
-  }
-
-  .location-group {
-    color: var(--color-primary-blue-03);
-    font-family: var(--font-secondary);
-    line-height: 1;
-    margin-bottom: var(--space-s);
-    display: flex;
-    flex-direction: column;
-  }
-
-  .icon-with-link {
-    position: relative;
-    z-index: 20;
-  }
-
-  // Breakpoints
-  @media #{$medium} {
-    .text {
-      margin-top: 0;
-    }
-  }
-
-  // Hovers
-  @media #{$has-hover} {
-    &:hover {
-      .title {
-        @include link-hover;
-      }
-    }
-  }
-}
+@import "@/styles/themes.scss";
 </style>
