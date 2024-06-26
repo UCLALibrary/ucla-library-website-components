9c87ef2a392674d26c1f71b23987a8e28c0fafe2
tinuola <tinuola@users.noreply.github.com>
2024-06-25 17:39:15 -0700

    style: ftva button-link theme

--- src/lib-components/ButtonLink.vue
+++ src/lib-components/ButtonLink.vue
@@ -1,83 +1,83 @@
 <script setup>
-import { computed, defineAsyncComponent } from "vue"
-import { useTheme } from "@/composables/useTheme"
+import { computed, defineAsyncComponent } from 'vue'
+import { useTheme } from '@/composables/useTheme'
 
 // Helper functions
-import isInternalLink from "@/utils/isInternalLink"
-import SmartLink from "@/lib-components/SmartLink.vue"
+import isInternalLink from '@/utils/isInternalLink'
+import SmartLink from '@/lib-components/SmartLink.vue'
+
+const props = defineProps({
+  to: {
+    type: String,
+    default: '',
+  },
+  /**
+   * Determines what text the button should have.
+   */
+  label: {
+    type: String,
+    default: '',
+  },
+  /**
+   * Determines what icon should be used in button.
+   * Do not include this prop if it is an internal link.
+   */
+  iconName: {
+    type: String,
+    default: '',
+  },
+  isSecondary: {
+    type: Boolean,
+    default: false,
+  },
+  isTertiary: {
+    type: Boolean,
+    default: false,
+  },
+  isQuaternary: {
+    type: Boolean,
+    default: false,
+  },
+  isDownload: {
+    type: Boolean,
+    default: false,
+  },
+  linkTarget: {
+    type: String,
+    default: '',
+  },
+})
 
 const SvgExternalLink = defineAsyncComponent(() =>
-    import("ucla-library-design-tokens/assets/svgs/icon-external-link.svg")
+  import('ucla-library-design-tokens/assets/svgs/icon-external-link.svg')
 )
 
 const SvgArrowRight = defineAsyncComponent(() =>
-    import("ucla-library-design-tokens/assets/svgs/icon-arrow-right.svg")
+  import('ucla-library-design-tokens/assets/svgs/icon-arrow-right.svg')
 )
 
 const SvgArrowDownload = defineAsyncComponent(() =>
-    import("ucla-library-design-tokens/assets/svgs/icon-download.svg")
+  import('ucla-library-design-tokens/assets/svgs/icon-download.svg')
 )
 
 const IconClose = defineAsyncComponent(() =>
-    import("ucla-library-design-tokens/assets/svgs/icon-close.svg")
+  import('ucla-library-design-tokens/assets/svgs/icon-close.svg')
 )
-
-const props = defineProps({
-    to: {
-        type: String,
-        default: "",
-    },
-    /**
-     * Determines what text the button should have.
-     */
-    label: {
-        type: String,
-        default: "",
-    },
-    /**
-     * Determines what icon should be used in button.
-     * Do not include this prop if it is an internal link.
-     */
-    iconName: {
-        type: String,
-        default: "",
-    },
-    isSecondary: {
-        type: Boolean,
-        default: false,
-    },
-    isTertiary: {
-        type: Boolean,
-        default: false,
-    },
-    isQuaternary: {
-        type: Boolean,
-        default: false,
-    },
-    isDownload: {
-        type: Boolean,
-        default: false,
-    },
-    linkTarget: {
-        type: String,
-        default: "",
-    },
-})
 
 const theme = useTheme()
 
 const parsedClasses = computed(() => {
-    return [
-        "button-link",
-        theme?.value || "",
-        props.isSecondary
-            ? "is-secondary"
-            : props.isTertiary
-            ? "is-tertiary"
-            : props.isQuaternary
-            ? "is-quaternary"
-            : "is-primary",
-    ]
+  return [
+    'button-link',
+    theme?.value || '',
+    props.isSecondary
+      ? 'is-secondary'
+      : props.isTertiary
+        ? 'is-tertiary'
+        : props.isQuaternary
+          ? 'is-quaternary'
+          : '',
+  ]
 })
 
 // if -> the iconName is svg-download then the download icon will display
@@ -85,51 +85,51 @@ const parsedClasses = computed(() => {
 // else svg-arrow-diagonal will display
 
 const parsedIconName = computed(() => {
-    switch (true) {
-        case props.isDownload:
-            return SvgArrowDownload
-        case isInternalLink(props.to):
-            return SvgArrowRight
-        case props.linkTarget === "_blank" ||
-            props.iconName === "svg-external-link":
-            return SvgExternalLink
-        case props.iconName === "icon-close":
-            return IconClose
-        // case props.iconName:
-        //     return props.iconName
-        case props.iconName === "none":
-            return ""
-        default:
-            return SvgExternalLink
-    }
+  switch (true) {
+    case props.isDownload:
+      return SvgArrowDownload
+    case isInternalLink(props.to):
+      return SvgArrowRight
+    case props.linkTarget === '_blank'
+            || props.iconName === 'svg-external-link':
+      return SvgExternalLink
+    case props.iconName === 'icon-close':
+      return IconClose
+      // case props.iconName:
+      //     return props.iconName
+    case props.iconName === 'none':
+      return ''
+    default:
+      return SvgExternalLink
+  }
 })
 </script>
 
 <template>
-    <SmartLink
-        :to="to"
-        :class="parsedClasses"
-        :is-download="isDownload"
-        :link-target="linkTarget"
-    >
-        <span class="label">{{ label }}</span>
+  <SmartLink
+    :to="to"
+    :class="parsedClasses"
+    :is-download="isDownload"
+    :link-target="linkTarget"
+  >
+    <span class="label">{{ label }}</span>
 
-        <component
-            :is="parsedIconName"
-            v-if="parsedIconName !== ''"
-            class="arrow"
-            aria-hidden="true"
-        />
-        <div class="hover">
-            <span class="label">{{ label }}</span>
-            <component
-                :is="parsedIconName"
-                v-if="parsedIconName !== ''"
-                class="arrow"
-                aria-hidden="true"
-            />
-        </div>
-    </SmartLink>
+    <component
+      :is="parsedIconName"
+      v-if="parsedIconName !== ''"
+      class="arrow"
+      aria-hidden="true"
+    />
+    <div class="hover">
+      <span class="label">{{ label }}</span>
+      <component
+        :is="parsedIconName"
+        v-if="parsedIconName !== ''"
+        class="arrow"
+        aria-hidden="true"
+      />
+    </div>
+  </SmartLink>
 </template>
 
 <style lang="scss" scoped>
--- src/stories/ButtonLink.stories.js
+++ src/stories/ButtonLink.stories.js
@@ -1,126 +1,163 @@
-import ButtonLink from "@/lib-components/ButtonLink"
+import { computed } from 'vue'
+import ButtonLink from '@/lib-components/ButtonLink'
 
 // Storybook default settings
 export default {
-    title: "BUTTON / Link",
-    component: ButtonLink,
+  title: 'BUTTON / Link',
+  component: ButtonLink,
 
-    argTypes: {
-        label: { type: "string" },
-        iconName: {
-            type: "select",
-            options: [
-                "svg-arrow-right",
-                "svg-arrow-download",
-                "svg-external-link",
-                "icon-close",
-            ],
-        },
-        isSecondary: { type: "boolean" },
-        isTertiary: { type: "boolean" },
-        isQuaternary: { type: "boolean" },
-        to: { type: "string" },
+  argTypes: {
+    label: { type: 'string' },
+    iconName: {
+      type: 'select',
+      options: [
+        'svg-arrow-right',
+        'svg-arrow-download',
+        'svg-external-link',
+        'icon-close',
+      ],
     },
+    isSecondary: { type: 'boolean' },
+    isTertiary: { type: 'boolean' },
+    isQuaternary: { type: 'boolean' },
+    to: { type: 'string' },
+  },
 }
 
 function Template(args) {
-    return {
-        components: { ButtonLink },
-        setup() {
-            return { args }
-        },
-        template: '<button-link v-bind="args" />',
-    }
+  return {
+    components: { ButtonLink },
+    setup() {
+      return { args }
+    },
+    template: '<button-link v-bind="args" />',
+  }
 }
 
 export const WithControls = {
-    args: {
-        label: "Ibus orci Luctus no template",
-    },
+  args: {
+    label: 'Ibus orci Luctus no template',
+  },
 }
 
 export const WithControlsIconClose = Template.bind({})
 WithControlsIconClose.args = {
-    label: "Ibus orci Luctus",
-    iconName: "icon-close",
+  label: 'Ibus orci Luctus',
+  iconName: 'icon-close',
 }
 
 // Variations of stories below
 export function Default() {
-    return {
-        components: { ButtonLink },
-        template: '<button-link label="Egest perl Conub" to="/help/more" />',
-    }
+  return {
+    components: { ButtonLink },
+    template: '<button-link label="Egest perl Conub" to="/help/more" />',
+  }
 }
 
 export function SecondaryInternal() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Etiam et Ultrices" :isSecondary="true" to=\'/help/more\' />',
-    }
+  }
 }
 
 export function SecondaryLibraryInternal() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Etiam et Ultrices" :isSecondary="true" to=\'https://calendar.library.ucla.edu/\' />',
-    }
+  }
 }
 
 export function SecondaryExternal() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Etiam et Ultrices" :isSecondary="true" to=\'https://www.google.com/\' />',
-    }
+  }
 }
 
 export function LongLabel() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Proin sodales dolor sit amet." iconName="svg-arrow-right" to=\'/help/more\'/>',
-    }
+  }
 }
 
 export function HasGlyph() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Aug id Dignissim" :isSecondary="true"  :is-download="true" to=\'/help/more\'/>',
-    }
+  }
 }
 
 export function HasExternalGlyph() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Aug id Dignissim" :isSecondary="true" iconName="svg-external-link" to=\'www.google.com\'/>',
-    }
+  }
 }
 
 export function HasNoGlyph() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    template:
             '<button-link label="Aug id Dignissim" :isSecondary="true" icon-name="none" to=\'www.google.com\'/>',
-    }
+  }
 }
 
 export function FTVA() {
-    return {
-        components: { ButtonLink },
-        template:
+  return {
+    components: { ButtonLink },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    template:
             '<button-link label="FTVA Primary" icon-name="none" to=\'www.google.com\'/>',
-    }
+  }
+}
+
+export function FTVAWithGlyph() {
+  return {
+    components: { ButtonLink },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    template:
+            '<button-link label="FTVA Primary" icon-name="svg-external-link" to=\'www.google.com\'/>',
+  }
 }
 
 export function FTVASecondary() {
-    return {
-        components: { ButtonLink },
-        template:
-            '<button-link label="FTVA Secondary" icon-name="none" to=\'www.google.com\'/>',
-    }
+  return {
+    components: { ButtonLink },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    template:
+            '<button-link label="FTVA Secondary" :isSecondary="true" icon-name="none" to=\'www.google.com\'/>',
+  }
+}
+
+export function FTVASecondaryWithGlyph() {
+  return {
+    components: { ButtonLink },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    template:
+            '<button-link label="FTVA Secondary" :isSecondary="true" icon-name="svg-external-link" to=\'www.google.com\'/>',
+  }
 }
