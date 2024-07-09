16f4d4a0f1a4d7445e8faec7977a8fe76e4eb043
Jess Divers <jessica.divers@library.ucla.edu>
2024-07-09 16:13:13 -0700

    feat: add responsive-iframe

--- src/stories/ResponsiveVideo.stories.js
+++ src/stories/ResponsiveVideo.stories.js
@@ -1,3 +1,4 @@
+import { computed } from 'vue'
 import ResponsiveVideo from '@/lib-components/ResponsiveVideo'
 
 // Import mock api data
@@ -29,5 +30,29 @@ export function VideoWithControls() {
     },
     components: { ResponsiveVideo },
     template: '<responsive-video :media="video" :controls="true"/>',
+  }
+}
+
+// Example using a slot to put a responsive iframe inside responsive video
+const mockFTVAData = {
+  trailer: '<figure><iframe width="560" height="315" src="https://www.youtube.com/embed/uYr_SvIKKuI?si=ihenbmyE91KqyXK5" title="YouTube video player" frameborder="0"></iframe></figure>'
+}
+const parsedTrailer = computed(() => {
+  return mockFTVAData.trailer.split('src=\"')[1].split('\"')[0]
+})
+export function FTVAVideoEmbed() {
+  return {
+    setup() {
+      return {
+        parsedTrailer
+      }
+    },
+    data() {
+      return {
+        video: API.video,
+      }
+    },
+    components: { ResponsiveVideo },
+    template: '<responsive-video :aspect-ratio="56.9" :controls="true"><template v-slot><iframe :src="parsedTrailer" title="YouTube video player" class="responsive-iframe" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen loading="lazy"></iframe></template></responsive-video>',
   }
 }
