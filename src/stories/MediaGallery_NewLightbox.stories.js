fe3363b9209b3f6f71255a819e9367818d5ae551
Jess <jessica.divers@gmail.com>
2024-07-11 15:53:10 -0700

    feat: APPS-2790 Create Component FTVA Carousel -> NewLightbox  (#550)
    * feat: add ftva theme to newlightbox
    
    * feat: finish styling carousel, slots
    
    * fix: updates styles to make icon 8 by 8
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>

--- src/stories/MediaGallery_NewLightbox.stories.js
+++ src/stories/MediaGallery_NewLightbox.stories.js
@@ -1,5 +1,6 @@
+import { computed } from 'vue'
 import FlexibleMediaGalleryNewLightbox from '@/lib-components/Flexible/MediaGallery/NewLightbox.vue'
-
+import BlockTag from '@/lib-components/BlockTag.vue'
 import { Gallery as MEDIA_GALLERY_MOCK } from '@/stories/mock/Media'
 
 // Storybook default settings
@@ -29,5 +30,75 @@ export function SingleItem() {
     },
     components: { FlexibleMediaGalleryNewLightbox },
     template: '<flexible-media-gallery-new-lightbox :items="items" />',
+  }
+}
+
+// FTVA style data mock for carousel
+// as it comes from craft CMS
+const mockFTVAGalleryRawData = [
+  {
+    image: [MEDIA_GALLERY_MOCK.mediaGallery[0].item],
+    creditText: 'Credit text'
+  },
+  {
+    image: [MEDIA_GALLERY_MOCK.mediaGallery[1].item],
+    creditText: 'Credit text 2'
+  }
+]
+
+// FTVA implementation of this component requires a different data structure than the default,
+// parent page should implement a computed function like this to transform the data
+const mockFTVAGalleryComputedData = computed(() => {
+  // map image to item, map creditText to credit
+  return mockFTVAGalleryRawData.map((rawItem) => {
+    return {
+      item: rawItem.image[0],
+      credit: rawItem.creditText,
+      captionText: 'Caption text', // TODO get homepage carousel data sample to make more accurate
+      captionTitle: 'Caption title' // TODO get homepage carousel data sample to make more accurate
+    }
+  })
+})
+
+export function FTVA_EventDetailCarousel() {
+  return {
+    data() {
+      return {
+        items: mockFTVAGalleryComputedData,
+      }
+    },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    components: { FlexibleMediaGalleryNewLightbox },
+    template: '<flexible-media-gallery-new-lightbox :items="items" />',
+  }
+}
+
+// TODO finish example showing homepage implementation with slots
+// mockdata for blocktags in parent
+const mockTags = ['tag1', 'tag2', 'tag3']
+// TODO: for part 2 of carousel homepage styling
+export function FTVA_Homepage() {
+  return {
+    setup() {
+      return {
+        mockTags
+      }
+    },
+    data() {
+      return {
+        items: mockFTVAGalleryComputedData
+      }
+    },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    components: { FlexibleMediaGalleryNewLightbox, BlockTag },
+    template: '<flexible-media-gallery-new-lightbox class="homepage" :items="items"><template v-slot="slotProps"><BlockTag :label="mockTags[slotProps.selectionIndex]" /></template></ flexible-media-gallery-new-lightbox>',
   }
 }
