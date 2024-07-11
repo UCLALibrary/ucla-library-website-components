001aada6aca8bf7de890cce7e8c172421d882acd
tinuola <tinuola@users.noreply.github.com>
2024-07-10 19:11:25 -0700

    fix: linting

--- src/stories/NavBreadcrumb.stories.js
+++ src/stories/NavBreadcrumb.stories.js
@@ -1,96 +1,96 @@
-import { computed, onBeforeUnmount, onMounted } from "vue"
-import router from "@/router"
-import NavBreadcrumb from "@/lib-components/NavBreadcrumb"
-import { useGlobalStore } from "@/stores/GlobalStore"
+import { computed, onBeforeUnmount, onMounted } from 'vue'
+import router from '@/router'
+import NavBreadcrumb from '@/lib-components/NavBreadcrumb'
+import { useGlobalStore } from '@/stores/GlobalStore'
 
 // Storybook default settings
 export default {
-    title: "NAV / Breadcrumb",
-    component: NavBreadcrumb,
-    decorators: [
-        () => ({
-            router,
-            template: "<story />",
-        }),
-    ],
+  title: 'NAV / Breadcrumb',
+  component: NavBreadcrumb,
+  decorators: [
+    () => ({
+      router,
+      template: '<story />',
+    }),
+  ],
 }
 
 function Template(args) {
-    router.push(args.to)
-    return {
-        setup() {
-            onMounted(() => {
-                const globalStore = useGlobalStore()
+  router.push(args.to)
+  return {
+    setup() {
+      onMounted(() => {
+        const globalStore = useGlobalStore()
 
-                const updateWinWidth = () => {
-                    globalStore.winWidth = window.innerWidth
-                }
+        const updateWinWidth = () => {
+          globalStore.winWidth = window.innerWidth
+        }
 
-                // Set initial winWidth
-                updateWinWidth()
+        // Set initial winWidth
+        updateWinWidth()
 
-                window.addEventListener("resize", updateWinWidth)
+        window.addEventListener('resize', updateWinWidth)
 
-                // Clean up
-                onBeforeUnmount(() => {
-                    window.removeEventListener("resize", updateWinWidth)
-                })
-            })
+        // Clean up
+        onBeforeUnmount(() => {
+          window.removeEventListener('resize', updateWinWidth)
+        })
+      })
 
-            return { args }
-        },
-        components: { NavBreadcrumb },
-        template: '<nav-breadcrumb v-bind="args" />',
-    }
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb v-bind="args" />',
+  }
 }
 
 export const Default = Template.bind({})
 Default.args = {
-    to: "/arts-library/arts-library-collections-access-and-services-during-seismic-construction-closure",
+  to: '/arts-library/arts-library-collections-access-and-services-during-seismic-construction-closure',
 }
 
 export const MultipleNesting = Template.bind({})
 MultipleNesting.args = {
-    to: "/explore-collections/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles",
+  to: '/explore-collections/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles',
 }
 
 export const MultipleNestingCollapsed = Template.bind({})
 MultipleNestingCollapsed.args = {
-    to: "/explore-collections/watch-and-listen-online/ktla-collection/national-and-local-politics/ktla-news-demo-article",
+  to: '/explore-collections/watch-and-listen-online/ktla-collection/national-and-local-politics/ktla-news-demo-article',
 }
 
 function TemplateFTVA(args) {
-    router.push(args.to)
-    return {
-        provide() {
-            return {
-                theme: computed(() => "ftva"),
-            }
-        },
-        setup() {
-            onMounted(() => {
-                const globalStore = useGlobalStore()
+  router.push(args.to)
+  return {
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    setup() {
+      onMounted(() => {
+        const globalStore = useGlobalStore()
 
-                const updateWinWidth = () => {
-                    globalStore.winWidth = window.innerWidth
-                }
+        const updateWinWidth = () => {
+          globalStore.winWidth = window.innerWidth
+        }
 
-                updateWinWidth()
+        updateWinWidth()
 
-                window.addEventListener("resize", updateWinWidth)
+        window.addEventListener('resize', updateWinWidth)
 
-                onBeforeUnmount(() => {
-                    window.removeEventListener("resize", updateWinWidth)
-                })
-            })
-            return { args }
-        },
-        components: { NavBreadcrumb },
-        template: '<nav-breadcrumb v-bind="args" />',
-    }
+        onBeforeUnmount(() => {
+          window.removeEventListener('resize', updateWinWidth)
+        })
+      })
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb v-bind="args" />',
+  }
 }
 
 export const FTVA = TemplateFTVA.bind({})
 FTVA.args = {
-    to: "/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles",
+  to: '/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles',
 }
