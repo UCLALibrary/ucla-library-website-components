8c2b9d32d07d0c5a16bec4b8c3b7f57676435524
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2024-07-16 12:42:02 -0700

    feat: APPS-2700 Create Component FTVA Breadcrumb Variant (#548)
    * feat: implement logic for multi-level breadcrumb nesting"
    
    * task: reorganize, rename scss files
    
    * fix: linting
    
    * feat: implement logic to expand truncated links
    
    * update: add story for nesting behavior
    
    * task: update lock file
    
    * fix: styling adjustments
    
    * revert code to initial link expansion logic
    
    * wip: breadcrumb responsive logic
    
    * update the design tokens
    
    * fix: styling, mobile range
    
    * fix: breadcrumb caret order
    
    * style: change caret dir on mobile
    
    * fix: update logic for rendering carets
    
    * fix: linting
    
    * refactor, add useRoute
    
    * fix: linting
    
    * fix: remove console statement
    
    * feat: Revert specific commits to undo recent changes (#557)
    
    Revert specific commits to undo recent changes
    
    * chore(release): set `package.json` to 2.39.0-alpha.105 [skip ci]
    
    * stop releases while merging ([698c834](https://github.com/UCLALibrary/ucla-library-website-components/commit/698c8348c599a64c88b427bcb2cd858a81613fd9))
    
    * Revert specific commits to undo recent changes ([#557](https://github.com/UCLALibrary/ucla-library-website-components/issues/557)) ([ab94529](https://github.com/UCLALibrary/ucla-library-website-components/commit/ab9452976f917ec97ac3b0645973b028d5e3d879))
    
    * add nav-search import to base scss files
    
    * fix: add back props, template logic for hardcoded breadcrumbs
    
    * chore: refactor navbreadcrumb template for Tinu review (#559)
    
    chore: refactor navbreadcrumb template
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>
    
    * task: alphabetize scss imports
    
    * task: correct merge overwrite of yml files
    
    * Update publish-to-npm.yml indentation
    
    * Update CHANGELOG.md with code from main
    
    * Update pnpm-lock.yaml with code from main
    
    ---------
    
    Co-authored-by: tinuola <tinuola@users.noreply.github.com>
    Co-authored-by: jendiamond <thejendiamond@gmail.com>
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: Jess <jessica.divers@gmail.com>
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>

--- src/stories/NavBreadcrumb.stories.js
+++ src/stories/NavBreadcrumb.stories.js
@@ -1,15 +1,98 @@
+import { computed, onBeforeUnmount, onMounted } from 'vue'
+import router from '@/router'
 import NavBreadcrumb from '@/lib-components/NavBreadcrumb'
+import { useGlobalStore } from '@/stores/GlobalStore'
 
 // Storybook default settings
 export default {
   title: 'NAV / Breadcrumb',
   component: NavBreadcrumb,
+  decorators: [
+    () => ({
+      router,
+      template: '<story />',
+    }),
+  ],
 }
 
-// Variations of stories below
-export function Default() {
+function Template(args) {
+  router.push(args.to)
   return {
+    setup() {
+      onMounted(() => {
+        const globalStore = useGlobalStore()
+
+        const updateWinWidth = () => {
+          globalStore.winWidth = window.innerWidth
+        }
+
+        // Set initial winWidth
+        updateWinWidth()
+
+        window.addEventListener('resize', updateWinWidth)
+
+        // Clean up
+        onBeforeUnmount(() => {
+          window.removeEventListener('resize', updateWinWidth)
+        })
+      })
+
+      return { args }
+    },
     components: { NavBreadcrumb },
-    template: '<nav-breadcrumb title="jane-doe" to="/about/news" parent-title="parent"/>',
+    template: '<nav-breadcrumb v-bind="args" />',
   }
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  to: '/about/news',
+  title: 'jane doe',
+  parentTitle: 'parent',
+}
+
+export const MultipleNesting = Template.bind({})
+MultipleNesting.args = {
+  to: '/explore-collections/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles',
+}
+
+export const MultipleNestingCollapsed = Template.bind({})
+MultipleNestingCollapsed.args = {
+  to: '/explore-collections/watch-and-listen-online/ktla-collection/national-and-local-politics/ktla-news-demo-article',
+}
+
+function TemplateFTVA(args) {
+  router.push(args.to)
+  return {
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    setup() {
+      onMounted(() => {
+        const globalStore = useGlobalStore()
+
+        const updateWinWidth = () => {
+          globalStore.winWidth = window.innerWidth
+        }
+
+        updateWinWidth()
+
+        window.addEventListener('resize', updateWinWidth)
+
+        onBeforeUnmount(() => {
+          window.removeEventListener('resize', updateWinWidth)
+        })
+      })
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb v-bind="args" />',
+  }
+}
+
+export const FTVA = TemplateFTVA.bind({})
+FTVA.args = {
+  to: '/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles',
 }
