1909b51d8b421c889e829661b6e37a3939fdc26f
tinuola <tinuola@users.noreply.github.com>
2024-07-16 13:04:57 -0700

    task: rebasing from main

---
+++ src/lib-components/ButtonDropdown.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed } from "vue"
+import { useRoute } from "vue-router"
+import { useTheme } from "@/composables/useTheme"
+
+// Components
+
+const { eventDetail, socialList, title, hasIcon } = defineProps({
+    eventDetail: {
+        type: Object,
+        default: () => {},
+    },
+    socialList: {
+        type: Array, // social objects?
+        default: () => [],
+    },
+    title: {
+        type: String,
+        default: () => "",
+    },
+    hasIcon: {
+        type: Boolean,
+        default: () => "",
+    },
+})
+
+const route = useRoute()
+
+// Computed
+</script>
+
+<template>
+    <h2>Dropdown</h2>
+    {{ title }}
+</template>
+
+<style lang="scss" scoped>
+@import "@/styles/themes.scss";
+</style>
