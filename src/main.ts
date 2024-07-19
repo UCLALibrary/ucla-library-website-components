f0408d00f3424b6207851819f3921eccdcef01c2
Jess Divers <jessica.divers@library.ucla.edu>
2024-07-19 13:59:06 -0700

    fix: remove extra import

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router'
 import './style.css'
 import 'ucla-library-design-tokens/scss/app-global.scss'
 import '@/styles/global.scss'
-import '@/styles/themes.scss'
 
 import App from './App.vue'
 
