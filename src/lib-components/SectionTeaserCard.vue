910d035d5a0a415531710bda317f2487ccb6e91f
JenDiamond <thejendiamond@gmail.com>
2024-08-06 12:02:06 -0700

    fix: APPS-2827 Component SectionTeaserCard (#577)
    * progress
    
    * date at bottom
    
    * add stles to get the text at the bottom
    
    * add outline style
    
    * linting
    
    * delete extraneous file
    
    * get border-radius woking on images
    
    * linting
    
    * reset story mock
    
    * add blur border
    
    * mis-spelling fix
    
    * hover & scroll
    
    * add scroll bar color
    
    * update suggested changes

--- src/lib-components/SectionTeaserCard.vue
+++ src/lib-components/SectionTeaserCard.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 import { computed } from 'vue'
 import type { PropType } from 'vue'
 import type { BlockCardMetaType, EventItemType } from '@/types/types'
@@ -16,32 +19,47 @@ const { items } = defineProps({
     required: false
   },
 })
+
 const theme = useTheme()
+
 const classes = computed(() => {
   return ['section-teaser-card', theme?.value || '']
-})
-const titleClasses = computed(() => {
-  return ['section-teaser-card-title', theme?.value || '']
 })
 </script>
 
 <template>
-  <div v-if="sectionTitle" :class="titleClasses">
-    {{ sectionTitle }}
-  </div>
-  <ul :class="classes" :data-header="sectionTitle ? sectionTitle : null">
+  <ul
+    :class="classes"
+    :data-header="sectionTitle ? sectionTitle : null"
+  >
     <BlockCardWithImage
-      v-for="(item, index) in items" :key="`Card${index}`" :image="item.image" :to="item.to"
-      :category="item.category" :title="item.title" :alternative-full-name="item.alternativeFullName"
-      :language="item.language" :start-date="item.startDate" :end-date="item.endDate" :text="item.text"
-      :image-aspect-ratio="60" :is-vertical="true" :byline-one="item.bylineOne" :byline-two="item.bylineTwo"
-      :section-handle="item.sectionHandle" :ongoing="item.ongoing" class="card"
+      v-for="(item, index) in items"
+      :key="`Card${index}`"
+      :image="item.image"
+      :to="item.to"
+      :category="item.category"
+      :title="item.title"
+      :alternative-full-name="item.alternativeFullName"
+      :language="item.language"
+      :start-date="item.startDate"
+      :end-date="item.endDate"
+      :text="item.text"
+      :image-aspect-ratio="60"
+      :is-vertical="true"
+      :byline-one="item.bylineOne"
+      :byline-two="item.bylineTwo"
+      :section-handle="item.sectionHandle"
+      :ongoing="item.ongoing"
+      class="card"
     />
   </ul>
 </template>
 
-<style lang="scss" scoped>
-  @import "ucla-library-design-tokens/scss/_tokens-ftva";
-  @import "@/styles/default/_section-teaser-card.scss";
-  @import "@/styles/ftva/_section-teaser-card.scss";
+<style
+  lang="scss"
+  scoped
+>
+@import "ucla-library-design-tokens/scss/_tokens-ftva";
+@import "@/styles/default/_section-teaser-card.scss";
+@import "@/styles/ftva/_section-teaser-card.scss";
 </style>
