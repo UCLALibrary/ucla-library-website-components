2d1ba975fe0412ab4515cba0ef9b643616d3127f
Jess <jessica.divers@gmail.com>
2024-08-29 16:48:02 -0700

    feat: APPS-2698 create ftva header (#591)
    * chore: refactor add theme to sitebrand bar
    
    * chore: refactor
    
    * fix: css vars for storybook
    
    * feat: ftva version of sitebrandbar
    
    * chore: lint, refactor headersticky to add theme
    
    * fix: headersticky style, story
    
    * feat: ftva theme headersticky pt 1
    
    * feat: search button
    
    * feat: in progress work header
    
    * feat: search menu
    
    * fix: run linter
    
    * feat: mobile behavior pt 1
    
    * feat: mobile functions and styles
    
    * feat: logo on mobile ftva
    
    * lint: run linter
    
    * fix: design specs, new icons
    
    * chore: tweak styles
    
    * chore: remove unused navprimary story
    
    * chore: documentation
    
    * chore: organize mobile methods and remove extra comments
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>

--- src/stories/SiteBrandBar.stories.js
+++ src/stories/SiteBrandBar.stories.js
@@ -1,3 +1,4 @@
+import { computed } from 'vue'
 import SiteBrandBar from '@/lib-components/SiteBrandBar'
 
 export default {
@@ -8,6 +9,18 @@ export default {
 export function Default() {
   return {
     template: '<site-brand-bar/>',
+    components: { SiteBrandBar },
+  }
+}
+
+export function FTVAVersion() {
+  return {
+    template: '<site-brand-bar/>',
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
     components: { SiteBrandBar },
   }
 }
