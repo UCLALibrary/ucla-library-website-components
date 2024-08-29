3dfb10ea403b2ed1572617ddbc7a62c76a248405
tinuola <tinuola@users.noreply.github.com>
2024-08-28 17:32:48 -0700

    task: setup component files

---
+++ src/lib-components/TabItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/styles/default/_tab-item.scss";
+@import "@/styles/ftva/_tab-item.scss";
+</style>
---
+++ src/lib-components/TabList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/styles/default/_tab-list.scss";
+@import "@/styles/ftva/_tab-list.scss";
+</style>
