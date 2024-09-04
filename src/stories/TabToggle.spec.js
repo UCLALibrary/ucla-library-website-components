cfcde4d2b412a5f704c53bb072469f0cef55d22c
tinuola <tinuola@users.noreply.github.com>
2024-09-04 13:38:06 -0700

    fix spec file

--- src/stories/TabToggle.spec.js
+++ src/stories/TabToggle.spec.js
@@ -1,8 +1,8 @@
 describe('Tab Toggle', () => {
   it('Default', () => {
-    cy.visit('/iframe.html?id=tab-toggle--default&args=&viewMode=story')
+    cy.visit('/iframe.html?id=tab-toggle--default&viewMode=story')
 
-    cy.get('.tab-toggle').should('exist')
+    cy.get('.tab-list').should('exist')
 
     cy.percySnapshot('Tab Toggle: Default')
   })
