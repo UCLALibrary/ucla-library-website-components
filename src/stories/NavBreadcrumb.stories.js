f52f7553d19358a42db72b55bb8fe295d672f5fd
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2024-09-24 09:51:16 -0700

    fix: APPS-2947 Update NavBreadcrumb logic to display route title or prop title (#616)
    * wip: refactor breadcrumb logic
    
    * wip: update breadcrumb stories
    
    * task: clean up code
    
    * fix: add extra logic for parent breadcrumb icon on mobile
    
    * update code documentation
    
    * update ref, function naming
    
    * fix: update ref name in template
    
    * fix: remove collapsed class when group is expanded
    
    ---------
    
    Co-authored-by: tinuola <tinuola@users.noreply.github.com>
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>

--- src/stories/NavBreadcrumb.stories.js
+++ src/stories/NavBreadcrumb.stories.js
@@ -6,16 +6,50 @@ import NavBreadcrumb from '@/lib-components/NavBreadcrumb'
 export default {
   title: 'NAV / Breadcrumb',
   component: NavBreadcrumb,
-  decorators: [
-    () => ({
-      router,
-      template: '<story />',
-    }),
-  ],
 }
 
 function Template(args) {
-  router.push(args.to)
+  return {
+    setup() {
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb to="/about/news" title="Jane Doe" parentTitle="Parent"/>',
+  }
+}
+
+// Legacy breadcrumbs (Values entered at page-level)
+export const DefaultSingleLevelWithPageProps = Template.bind({})
+
+// Breadcrumbs generated from route with option to use title from route or set at page-level
+function Template2(args) {
+  router.push('/parent-1/parent-2/final-breadcrumb/')
+  return {
+    setup() {
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb />',
+  }
+}
+
+export const MultipleLevelsRouteWithSlashEnding = Template2.bind({})
+
+function Template3(args) {
+  router.push('/parent-1/parent-2/final-breadcrumb')
+  return {
+    setup() {
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb />',
+  }
+}
+
+export const MultipleLevelsRouteNoSlashEnding = Template3.bind({})
+
+function Template4(args) {
+  router.push('/events/upcoming-events/la-région-centrale-10-20-23-screening-03-08-24')
   return {
     setup() {
       return { args }
@@ -25,28 +59,33 @@ function Template(args) {
   }
 }
 
-// Legacy breadcrumb (Values entered at page-level)
-export const Default = Template.bind({})
-Default.args = {
-  to: '/about/news',
-  title: 'Jane Doe',
-  parentTitle: 'Parent',
+export const MultipleLevelsWithRouteTitle = Template4.bind({})
+
+export const MultipleLevelsWithPropTitle = Template4.bind({})
+MultipleLevelsWithPropTitle.args = {
+  title: 'Breadcrumb Title Passed by Prop',
 }
 
-// All breadcrumbs generated from route
-export const MultipleNesting = Template.bind({})
-MultipleNesting.args = {
-  to: '/events/upcoming-events/la-région-centrale-10-20-23-screening-03-08-24',
+function Template5(args) {
+  router.push('/explore-collections/watch-and-listen-online/ktla-collection/national-and-local-politics/ktla-news-demo-article')
+  return {
+    setup() {
+      return { args }
+    },
+    components: { NavBreadcrumb },
+    template: '<nav-breadcrumb v-bind="args" />',
+  }
 }
 
-// All breadcrumbs generated from route
-export const MultipleNestingCollapsed = Template.bind({})
-MultipleNestingCollapsed.args = {
-  to: '/explore-collections/watch-and-listen-online/ktla-collection/national-and-local-politics/ktla-news-demo-article',
+export const CollapsedLevelsWithRouteTitle = Template5.bind({})
+
+export const CollapsedLevelsWithPropTitle = Template5.bind({})
+CollapsedLevelsWithPropTitle.args = {
+  title: 'Breadcrumb Title Passed by Prop',
 }
 
-function TemplateFTVA(args) {
-  router.push(args.to)
+function Template6(args) {
+  router.push('/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles')
   return {
     provide() {
       return {
@@ -61,10 +100,9 @@ function TemplateFTVA(args) {
   }
 }
 
-// FTVA Event: Using title from Craft data
-// to generate final breadcrumb
-export const FTVA = TemplateFTVA.bind({})
-FTVA.args = {
-  to: '/watch-and-listen-online/senator-john-f.-kennedy-gives-press-conference-in-los-angeles',
-  title: 'Senator John F. Kennedy: "Press Conference" in Los Angeles?'
+export const FTVATheme = Template6.bind({})
+
+export const FTVAThemeWithPropTitle = Template6.bind({})
+FTVAThemeWithPropTitle.args = {
+  title: 'Breadcrumb Title Passed by Prop',
 }
