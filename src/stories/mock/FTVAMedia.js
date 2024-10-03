94455c98ab1d37b403eacc41b8a1508db7f96874
tinuola <tinuola@users.noreply.github.com>
2024-10-03 15:39:53 -0700

    update mock file with verified data

--- src/stories/mock/FTVAMedia.js
+++ src/stories/mock/FTVAMedia.js
@@ -1,113 +1,75 @@
-const ImageFile1 = [
-  {
-    id: '46054',
-    src: 'https://static.library.ucla.edu/craftassetstest/images/pine-needles_2022-10-18-005212_jcjf.jpg',
-    srcset:
-      'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 2560w',
-    height: 1918,
-    width: 2560,
-    title: 'Pine needles',
-    focalPoint: [0.5, 0.5],
-    kind: 'image',
-    type: 'image/jpeg',
-    alt: 'image alt text',
-  },
-]
-
-const ImageFile2 = [{
-  id: '3156835',
-  src: 'https://static.library.ucla.edu/craftassetstest/images/_fullscreen/TomReed_MalcolmX.webp',
-  height: 1813,
-  width: 2560,
-  srcset: 'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 2560w',
-  alt: 'Tom Reed hosting an episode exploring the teachings of Malcolm X',
-  focalPoint: [
-    0.5,
-    0.5
-  ],
-  kind: 'image',
-  type: 'image/jpeg',
-}]
-
-const ImageFile3 = [{
-  dataId: '3201291',
-  src: 'https://static.library.ucla.edu/craftassetstest/images/_fullscreen/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg',
-  srcset: 'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 2560w',
-  height: 1075,
-  width: 2560,
-  title: 'Michael Snow Walking Woman city resize crop 1000 420 90 c1',
-  focalPoint: [
-    0.5,
-    0.5
-  ],
-  kind: 'image',
-  type: 'image/jpeg',
-  alt: null
-}]
-
-const ImageFile4 = [{
-  dataId: '3264676',
-  src: 'https://static.library.ucla.edu/craftassetstest/FTVA/_fullscreen/T-Coronae-Borealis-nova.jpg',
-  srcset: 'https://static.library.ucla.edu/craftassetstest/FTVA/_375xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 375w, https://static.library.ucla.edu/craftassetstest/FTVA/_960xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 960w, https://static.library.ucla.edu/craftassetstest/FTVA/_1280xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 1280w, https://static.library.ucla.edu/craftassetstest/FTVA/_1920xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 1920w, https://static.library.ucla.edu/craftassetstest/FTVA/_2560xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 2560w',
-  height: 1075,
-  width: 2560,
-  title: 'Michael Snow Walking Woman city resize crop 1000 420 90 c1',
-  focalPoint: [
-    0.5,
-    0.5
-  ],
-  kind: 'image',
-  type: 'image/jpeg',
-  alt: null
-}]
-
 export const Gallery = {
-  id: '46450',
   typeHandle: 'mediaGallery',
-  sectionTitle: 'Pine Needles are Awesome',
-  richTextSimplified: null,
-  mediaGalleryStyle: 'fullWidth',
+  dataId: '3266536',
+  sectionTitle: 'Once every 80 years',
+  sectionSummary: '<p><em>If predictions are correct, a key outburst star could put on a show in early 2024.</em></p>',
   mediaGallery: [
+    {
+      dataId: '3266537',
+      captionTitle: 'Recurrent Nova',
+      captionText: 'If predictions are correct, a key outburst star could put on a show in early 2024.',
+      sortOrder: 1,
+      item: [
+        {
+          dataId: '3266312',
+          src: 'https://static.library.ucla.edu/craftassetstest/images/_fullscreen/Recurrent-nova_0-1.jpg',
+          srcset: 'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/Recurrent-nova_0-1.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/Recurrent-nova_0-1.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/Recurrent-nova_0-1.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/Recurrent-nova_0-1.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/Recurrent-nova_0-1.jpg 2560w',
+          height: 1946,
+          width: 2560,
+          title: 'Recurrent nova 0 1',
+          focalPoint: [
+            0.5,
+            0.5
+          ],
+          kind: 'image',
+          type: 'image/jpeg',
+          alt: null
+        }
+      ]
+    },
     {
       id: '46451',
       captionTitle: 'Cat Ipsum: Just saw other new cats inside the house snacking and playing nobody asked before using my litter box so mad meow kittycat',
-      captionText: 'Chase dog then run away sleep on keyboard kitty run to human with blood on mouth from frenzied attack on poor innocent mouse, don\'t i look cute?',
-      sortOrder: 1,
-      item: ImageFile1,
-      coverImage: ImageFile1,
-      credit: 'photo by someone',
-      linkUrl: null,
-      linkText: null,
-    },
-    {
-      id: '46452',
-      captionTitle: 'Tom Reed’s “For Members Only”: Black Perspectives on Local L.A. TV',
-      captionText: 'I\'m baby meh bicycle rights activated charcoal cronut you probably haven\'t heard of them distillery flannel cliche woke. Raclette sustainable vape salvia direct trade hashtag street art.',
-      credit: null,
+      captionText: 'Cat ipsum dolor sit amet, the cat was chasing the mouse. What the heck just happened, something feels fishy going to catch the red dot today going to catch the red dot today yet disappear for four days and return home with an expensive injury.',
       sortOrder: 2,
-      item: ImageFile2,
-      coverImage: ImageFile2,
-      embedCode: null,
-      linkUrl: null,
-      linkText: null,
+      item: [
+        {
+          id: '46054',
+          src: 'https://static.library.ucla.edu/craftassetstest/images/pine-needles_2022-10-18-005212_jcjf.jpg',
+          srcset:
+      'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 2560w',
+          height: 1918,
+          width: 2560,
+          title: 'Pine needles',
+          focalPoint: [0.5, 0.5],
+          kind: 'image',
+          type: 'image/jpeg',
+          alt: 'image alt text',
+        }
+      ],
     },
     {
-      id: '46453',
-      captionTitle: 'Walking Woman',
-      captionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      dataId: '3266538',
+      captionTitle: null,
+      captionText: 'This finder chart covers about as much sky as the field of view in a typical pair of 7-power binoculars',
       sortOrder: 3,
-      item: ImageFile3,
-      coverImage: ImageFile3,
-      embedCode: null,
+      item: [
+        {
+          dataId: '3266330',
+          src: 'https://static.library.ucla.edu/craftassetstest/images/_fullscreen/T-CrB-finder-ST-larger.jpg',
+          srcset: 'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/T-CrB-finder-ST-larger.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/T-CrB-finder-ST-larger.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/T-CrB-finder-ST-larger.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/T-CrB-finder-ST-larger.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/T-CrB-finder-ST-larger.jpg 2560w',
+          height: 2301,
+          width: 2560,
+          title: 'T Cr B finder ST larger',
+          focalPoint: [
+            0.5,
+            0.5
+          ],
+          kind: 'image',
+          type: 'image/jpeg',
+          alt: null
+        }
+      ]
     },
-    {
-      id: '46576',
-      captionTitle: 'Coronae Borealis Nova',
-      captionText: 'Doggo ipsum very good spot waggy wags thicc heckin angery woofer, ur givin me a spook borking doggo. Wow very biscit very jealous pupper heckin angery woofer sub woofer pupper, snoot noodle horse fat boi.',
-      sortOrder: 4,
-      item: ImageFile4,
-      coverImage: ImageFile4,
-      embedCode: null,
-    },
-  ],
+  ]
 }
