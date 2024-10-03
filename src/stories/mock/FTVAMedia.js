d3bbbdfbe0f4c1f980929a97a0ab63f7a74a1d48
tinuola <tinuola@users.noreply.github.com>
2024-10-02 18:52:16 -0700

    task: add mock file for FTVA gallery

---
+++ src/stories/mock/FTVAMedia.js
@@ -0,0 +1,113 @@
+const ImageFile1 = [
+  {
+    id: '46054',
+    src: 'https://static.library.ucla.edu/craftassetstest/images/pine-needles_2022-10-18-005212_jcjf.jpg',
+    srcset:
+      'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/pine-needles_2022-10-18-005212_jcjf.jpg 2560w',
+    height: 1918,
+    width: 2560,
+    title: 'Pine needles',
+    focalPoint: [0.5, 0.5],
+    kind: 'image',
+    type: 'image/jpeg',
+    alt: 'image alt text',
+  },
+]
+
+const ImageFile2 = [{
+  id: '3156835',
+  src: 'https://static.library.ucla.edu/craftassetstest/images/_fullscreen/TomReed_MalcolmX.webp',
+  height: 1813,
+  width: 2560,
+  srcset: 'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/TomReed_MalcolmX.webp 2560w',
+  alt: 'Tom Reed hosting an episode exploring the teachings of Malcolm X',
+  focalPoint: [
+    0.5,
+    0.5
+  ],
+  kind: 'image',
+  type: 'image/jpeg',
+}]
+
+const ImageFile3 = [{
+  dataId: '3201291',
+  src: 'https://static.library.ucla.edu/craftassetstest/images/_fullscreen/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg',
+  srcset: 'https://static.library.ucla.edu/craftassetstest/images/_375xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 375w, https://static.library.ucla.edu/craftassetstest/images/_960xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 960w, https://static.library.ucla.edu/craftassetstest/images/_1280xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 1280w, https://static.library.ucla.edu/craftassetstest/images/_1920xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 1920w, https://static.library.ucla.edu/craftassetstest/images/_2560xAUTO_crop_center-center_none/Michael-Snow-Walking-Woman-city-resize-crop_1000_420_90_c1.jpg 2560w',
+  height: 1075,
+  width: 2560,
+  title: 'Michael Snow Walking Woman city resize crop 1000 420 90 c1',
+  focalPoint: [
+    0.5,
+    0.5
+  ],
+  kind: 'image',
+  type: 'image/jpeg',
+  alt: null
+}]
+
+const ImageFile4 = [{
+  dataId: '3264676',
+  src: 'https://static.library.ucla.edu/craftassetstest/FTVA/_fullscreen/T-Coronae-Borealis-nova.jpg',
+  srcset: 'https://static.library.ucla.edu/craftassetstest/FTVA/_375xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 375w, https://static.library.ucla.edu/craftassetstest/FTVA/_960xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 960w, https://static.library.ucla.edu/craftassetstest/FTVA/_1280xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 1280w, https://static.library.ucla.edu/craftassetstest/FTVA/_1920xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 1920w, https://static.library.ucla.edu/craftassetstest/FTVA/_2560xAUTO_crop_center-center_none/T-Coronae-Borealis-nova.jpg 2560w',
+  height: 1075,
+  width: 2560,
+  title: 'Michael Snow Walking Woman city resize crop 1000 420 90 c1',
+  focalPoint: [
+    0.5,
+    0.5
+  ],
+  kind: 'image',
+  type: 'image/jpeg',
+  alt: null
+}]
+
+export const Gallery = {
+  id: '46450',
+  typeHandle: 'mediaGallery',
+  sectionTitle: 'Pine Needles are Awesome',
+  richTextSimplified: null,
+  mediaGalleryStyle: 'fullWidth',
+  mediaGallery: [
+    {
+      id: '46451',
+      captionTitle: 'Cat Ipsum: Just saw other new cats inside the house snacking and playing nobody asked before using my litter box so mad meow kittycat',
+      captionText: 'Chase dog then run away sleep on keyboard kitty run to human with blood on mouth from frenzied attack on poor innocent mouse, don\'t i look cute?',
+      sortOrder: 1,
+      item: ImageFile1,
+      coverImage: ImageFile1,
+      credit: 'photo by someone',
+      linkUrl: null,
+      linkText: null,
+    },
+    {
+      id: '46452',
+      captionTitle: 'Tom Reed’s “For Members Only”: Black Perspectives on Local L.A. TV',
+      captionText: 'I\'m baby meh bicycle rights activated charcoal cronut you probably haven\'t heard of them distillery flannel cliche woke. Raclette sustainable vape salvia direct trade hashtag street art.',
+      credit: null,
+      sortOrder: 2,
+      item: ImageFile2,
+      coverImage: ImageFile2,
+      embedCode: null,
+      linkUrl: null,
+      linkText: null,
+    },
+    {
+      id: '46453',
+      captionTitle: 'Walking Woman',
+      captionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      sortOrder: 3,
+      item: ImageFile3,
+      coverImage: ImageFile3,
+      embedCode: null,
+    },
+    {
+      id: '46576',
+      captionTitle: 'Coronae Borealis Nova',
+      captionText: 'Doggo ipsum very good spot waggy wags thicc heckin angery woofer, ur givin me a spook borking doggo. Wow very biscit very jealous pupper heckin angery woofer sub woofer pupper, snoot noodle horse fat boi.',
+      sortOrder: 4,
+      item: ImageFile4,
+      coverImage: ImageFile4,
+      embedCode: null,
+    },
+  ],
+}
