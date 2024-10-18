a39ff7e48ceede36b1c4661a4577c40e8e221cd8
Jess Divers <jessica.divers@library.ucla.edu>
2024-10-17 17:09:34 -0700

    fix: time on ucla homepage

--- src/lib-components/CardMeta.vue
+++ src/lib-components/CardMeta.vue
@@ -134,21 +134,18 @@ const parsedDateCreated = computed(() => {
 
 const parsedTime = computed(() => {
   // necessary check for library-website-nuxt & meap
-  if (props.startDate && props.sectionHandle === 'event') {
-    console.log('1')
+  if (props.startDate && props.sectionHandle === 'event')
     return formatTimes(props.startDate, props.endDate)
-  }
+
   // legacy behavior returns nothing when sectionHandle is not 'event',
-  // so check theme is set AND we are not showing ftvaEventSeries data to avoid returning nothing
-  else if (props.startDate && (props.sectionHandle !== 'ftvaEventSeries' && theme?.value !== undefined)) {
-    console.log('2')
+  // so check theme is set to ftva AND we are not showing ftvaEventSeries data to avoid returning nothing
+  else if (props.startDate && (props.sectionHandle !== 'ftvaEventSeries' && theme?.value === 'ftva'))
     return props.endDate ? formatTimes(props.startDate, props.endDate) : formatTimes(props.startDate, props.startDate)
-  }
+
   // in all other cases incl. if it is ftvaEventSeries, return nothing
-  else if (props.startDate && (props.sectionHandle === 'ftvaEventSeries')) {
-    console.log('3')
+  else if (props.startDate && (props.sectionHandle === 'ftvaEventSeries'))
     return format(new Date(props.startDate), 'h:mm aaa')
-  }
+
   return ''
 })
 
