260ecbbe2c7eda24c0387b3b49b2fa584b74a6bf
tinuola <tinuola@users.noreply.github.com>
2024-11-15 15:16:20 -0800

    wip: add minimal calendar

---
+++ src/lib-components/BaseCalendar.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+  data: () => ({
+    type: 'month',
+    weekday: [0, 1, 2, 3, 4, 5, 6],
+    value: [new Date()],
+    events: [],
+  }),
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<template>
+  <h2>Tinu's Calendar</h2>
+  <div>
+    <v-sheet>
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :events="events"
+        :view-mode="type"
+        :weekdays="weekday"
+      ></v-calendar>
+    </v-sheet>
+  </div>
+</template>
