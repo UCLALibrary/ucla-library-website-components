be2eb71bb6f63bfae7672811d751c467b91764d3
tinuola <tinuola@users.noreply.github.com>
2024-11-27 11:14:39 -0800

    wip: update styling

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,10 @@ import './style.css'
 import 'ucla-library-design-tokens/scss/app-global.scss'
 import '@/styles/global.scss'
 
-<<<<<<< HEAD
 import { vuetify } from './plugins/vuetify'
-=======
-import { createVuetify } from 'vuetify'
-import { VCalendar } from 'vuetify/labs/VCalendar'
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
->>>>>>> b8f008d8 (wip: calendar navigation)
 
 import router from './router'
 
 import App from './App.vue'
 
-<<<<<<< HEAD
-=======
-const vuetify = createVuetify({
-  components: {
-    VCalendar,
-  }
-})
-
->>>>>>> b8f008d8 (wip: calendar navigation)
 createApp(App).use(vuetify).use(router).use(createPinia()).mount('#app')
