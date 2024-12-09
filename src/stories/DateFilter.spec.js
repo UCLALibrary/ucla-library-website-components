952e975d78bd161d918ff5703d45f407456412fb
Jess Divers <jessica.divers@library.ucla.edu>
2024-12-09 12:45:44 -0700

    chore: add a test for datefilter emit behavior

--- src/stories/DateFilter.spec.js
+++ src/stories/DateFilter.spec.js
@@ -1,8 +1,30 @@
 describe('DateFilter', () => {
-  it('Default', () => {
-    cy.visit('/iframe.html?id=datefilter--default')
-    cy.get('.date-filter').should('exist')
+  describe('Default', () => {
+    beforeEach(() => {
+      cy.visit('/iframe.html?id=datefilter--default')
+    })
 
-    cy.percySnapshot('Date Filter: Default')
+    it('renders the component', () => {
+      cy.get('.date-filter').should('exist')
+      cy.percySnapshot('Date Filter: Default')
+    })
+
+    it('emits new selected date on done click and not before', () => {
+      // check contents of data-test:selected-date before
+      cy.get('[data-test=selected-date]').should('contain', '')
+
+      // open date picker & select date
+      cy.get('input.dp__input').click()
+      cy.get('.dp__calendar_item').eq(5).click()
+      cy.get('.dp__calendar_item').eq(7).click()
+
+      // check that contents havent changed yet
+      cy.get('[data-test=selected-date]').should('contain', '')
+      // click done button
+      cy.get('.select-button').click()
+
+      // check contents of data-test:selected-date after
+      cy.get('[data-test=selected-date]').contains('{ "startDate":')
+    })
   })
 })
