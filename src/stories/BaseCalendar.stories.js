dc24f852c908c7c68a3bf8e43e73fb0457b07283
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2024-12-10 15:52:48 -0800

    feat: APPS 3079 Calendar Component EventCard Popup (#661)
    * task: setup vuetify config
    
    * task: install vite vuetify plugin
    
    * task: add vuetify lab import to vite config
    
    * task: remove unneeded vuetify plugin folder/file
    
    * task: adjusting/refining vuetify config
    
    * wip: display test events
    
    * wip: calendar navigation
    
    * fix: linting
    
    * task: uninstall vite-plugin-vuetify
    
    * wip: add v-menu config, basic implementation
    
    * wip: implement basic calendar item popup
    
    * clean up code, reposition header today btn
    
    * wip: update styling
    
    * Update prop to set calendar's starting month
    
    * wip: add slot component to calendar
    
    * wip
    
    * Add inner components to calendar, update styles
    
    * update card-meta slot to remove extra margin if slot is unused
    
    * update calendar component slot story
    
    * Update calendar stories, add same-day events
    
    * wip: remove SmartLink component
    
    * rm tagLabels from cardWithImage, set calendar height
    
    * refactor: set base calendar and slot options
    
    * fix: linting
    
    * update documentation
    
    * update template logic, css to reflect highlighted tag labels
    
    * update mock calendar data with isHighlighted field
    
    * add logic to set visual state for selected event item
    
    * refactor deselect event listener
    
    * update calendar mock data
    
    * add documentation to component story
    
    * refactor, cleanup
    
    * refactor logic to handle event selection state styling
    
    * copy yaml file from main branch after pnpm reinstall
    
    * remove event card vertical overflow
    
    * wip: typescript
    
    * add typescript
    
    ---------
    
    Co-authored-by: tinuola <tinuola@users.noreply.github.com>

--- src/stories/BaseCalendar.stories.js
+++ src/stories/BaseCalendar.stories.js
@@ -1,14 +1,46 @@
 import { computed } from 'vue'
-
 import BaseCalendar from '../lib-components/BaseCalendar.vue'
 import { mockCalendarEvents } from './mock/CalendarEvents'
+
+/**
+ * Calendar component that extends Vuetify's `v-calendar` component and uses the `v-menu` component to display event item popup/dialog.
+ *
+ * Props:
+ *
+ * 1. defaultEventCalendar: Boolean value
+ * - When `true` (default), renders an event calendar with preset inner components:
+ *     - BlockCardWithImage, BlockEventDetail, and BlockTag
+ *     - See __Default Event__ stories
+ * - When `false`, BaseCalendar can either be:
+ *     - a simplified calendar that uses Vuetify's `v-list` component; see __Default Vuetify__ story
+ *     - a calendar with custom components if the slot `calendarSlotComponent` is used
+ *
+ *
+ * 2. events: Array of event objects
+ *
+ *
+ * 3. firstEventMonth: Array with a default `new Date()` object as its only item
+ * - When used, sets the first month that the calendar displays on page load
+ * - Example syntax: `[new Date('September 01, 2024 00:00:00')]`
+ * - See __Set Start Month__ story
+ */
 
 export default {
   title: 'Base Calendar',
   component: BaseCalendar,
 }
 
-export function Default() {
+export function DefaultVuetify() {
+  return {
+    data() {
+      return { ...mockCalendarEvents }
+    },
+    components: { BaseCalendar },
+    template: '<div style="display: flex;justify-content: center;"><base-calendar :events="events" :defaultEventCalendar="false" /></div>'
+  }
+}
+
+export function DefaultEvent() {
   return {
     data() {
       return { ...mockCalendarEvents }
@@ -18,7 +50,7 @@ export function Default() {
   }
 }
 
-export function DefaultFTVA() {
+export function DefaultFTVAEvent() {
   return {
     data() {
       return { ...mockCalendarEvents }
@@ -30,5 +62,51 @@ export function DefaultFTVA() {
     },
     components: { BaseCalendar },
     template: '<div style="display: flex;justify-content: center;"><base-calendar :events="events" /></div>'
+  }
+}
+
+export function SetStartMonth() {
+  return {
+    data() {
+      return {
+        ...mockCalendarEvents,
+        mockCalendarStart: [new Date('September 01, 2024 00:00:00')],
+      }
+    },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    components: { BaseCalendar },
+    template: `<div 
+        style="display: flex;justify-content: center;">
+        <base-calendar
+          :events="events"
+          :firstEventMonth="mockCalendarStart" />
+      </div>`
+  }
+}
+
+export function SameDayEvents() {
+  return {
+    data() {
+      return {
+        ...mockCalendarEvents,
+        mockCalendarStart: [new Date('October 01, 2024 00:00:00')],
+      }
+    },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    components: { BaseCalendar },
+    template: `<div 
+        style="display: flex;justify-content: center;">
+        <base-calendar
+          :events="events"
+          :firstEventMonth="mockCalendarStart" />
+      </div>`
   }
 }
