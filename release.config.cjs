fa1a2e38a43d60ea5e01780c99408e6267d57052
Jess <jessica.divers@gmail.com>
2025-02-07 11:14:36 -0700

    feat: update release config (#683)
    * feat: update release config
    
    * fix: labels
    
    * fix: remove extra whitespace
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>

--- release.config.cjs
+++ release.config.cjs
@@ -2,6 +2,12 @@ module.exports = {
   branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
+    {
+      releaseRules: [
+        { type: 'refactor', release: 'patch' },
+        { type: 'chore', release: 'patch' },
+      ],
+    },
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
