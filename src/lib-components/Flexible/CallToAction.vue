b4153c09524d9db1b6fbbdb1bf66dc205acdabe4
jendiamond <thejendiamond@gmail.com>
2025-02-26 16:47:22 -0800

    Flexible/CallToAction.vue

--- src/lib-components/Flexible/CallToAction.vue
+++ src/lib-components/Flexible/CallToAction.vue
@@ -6,6 +6,8 @@ import type { PropType } from 'vue'
 import type { FlexibleCallToAction } from '@/types/flexible_types'
 import BlockCallToAction from '@/lib-components/BlockCallToAction.vue'
 
+import { useTheme } from '@/composables/useTheme'
+
 const { block } = defineProps({
   block: {
     type: Object as PropType<FlexibleCallToAction>,
@@ -16,20 +18,21 @@ const { block } = defineProps({
 const parsedItems = computed(() => {
   return block.callToAction[0]
 })
+
+const theme = useTheme()
+const classes = computed(() => {
+  return ['flexible-block-call-to-action', theme?.value || '']
+})
 </script>
 
 <template>
   <BlockCallToAction
-    class="cta-block-content-width"
+    :class="classes"
     :to="parsedItems.buttonUrl"
     :name="parsedItems.buttonText"
     :title="parsedItems.titleCta"
     :text="parsedItems.summary"
     :svg-name="parsedItems.icon"
-    :is-dark="parsedItems.backgroundColor === 'true'"
+    :is-dark="((parsedItems.backgroundColor === 'true') || (parsedItems.isDark === 'true'))"
   />
 </template>
-
-<style lang="scss" scoped>
-@import "@/styles/ftva/_flexible-call-to-action.scss";
-</style>
