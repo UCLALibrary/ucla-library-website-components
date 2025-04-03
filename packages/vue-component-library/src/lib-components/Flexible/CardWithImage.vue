fa3fbc0a7ee431b750e726377437df0deff1b14e
jendiamond <thejendiamond@gmail.com>
2025-04-03 16:03:29 -0700

    ftvaEvent date working and links

--- packages/vue-component-library/src/lib-components/Flexible/CardWithImage.vue
+++ packages/vue-component-library/src/lib-components/Flexible/CardWithImage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import { computed } from 'vue'
+import format from 'date-fns/format'
 
 // THEME
 import { useTheme } from '@/composables/useTheme'
@@ -41,142 +42,186 @@ const parsedList = computed(() => {
   return items
 })
 
+
+// format(new Date(startDate), 'MMMM d, Y')
+
+
+// const parsedDate = computed(() => {
+//   let dateParsed = ''
+//   let formattedDate = ''
+//   if (
+//     block.cardWithImage
+//     && block.cardWithImage[0].contentLink
+//     && block.cardWithImage[0].contentLink[0].startDateWithTime
+//   )
+//     if (obj.contentType.includes('ftvaEvent')) {
+//       return dateParsed = format(new Date("startDateWithTime"), 'MMMM d, Y')
+//     }
+//   if (obj.ftvaEventSeries) { dateParsed = "ongoing" || "startDateWithTime" }
+//   if (obj.ftvaArticle) { dateParsed = "postDate" }
+//   if (obj.ftvaGeneralContentPage) { dateParsed = null }
+
+//   return dateParsed
+// })
+
+// const parsedLink = computed(() => {
+//   let to = ''
+//   if (obj.contentType === "ftvaGeneralContentPage") {
+//     to = obj.slug
+//   } else {
+//     to = obj.uri
+//   }
+//   return to
+// })
+
 const parsedItems = computed(() => {
   // Maps values based on content type and external or internal content
   // filter out null objects
   if (theme.value === 'ftva') {
     return parsedList.value
       .map((obj) => {
-        console.log("FTVA OBJ", obj.imageCarousel)
+        console.log("FTVA OBJ", obj)
         return {
           ...obj,
+          to: (obj.sectionHandle === "ftvaGeneralContentPage" && obj.slug) || obj.uri,
           parsedImage: (obj.imageCarousel && obj.imageCarousel[0] && obj.imageCarousel[0].image[0]) || obj.ftvaImage,
-          title: obj.title || obj.eventTitle || obj.titleGeneral
+          title: obj.eventTitle || obj.title || obj.titleGeneral,
+          //startDate: (obj.contentType === "ftvaArticle" || obj.contentType === "ftvaEvent") ? obj.startDateWithTime : null,
+          postDate: (obj.contentType === "ftvaArticle") ? "obj.postDate" : null,
+          //dateFormat: obj.contentType === "ftvaArticle" || obj.contentType === "ftvaEvent" ? obj.startDate : null
+          // articleByline2: Long Date with no Time
+          // articleByline2: obj.contentType === "ftvaArticle" ? obj.startDateWithTime : null,
+          byline2:
+            obj.contentType === "ftvaEvent"
+              ? format(new Date(obj.startDateWithTime), 'MMMM d, Y')
+              : '',
         }
 
       })
-  }
-  return parsedList.value
-    .filter(e => e !== null)
-    .map((obj) => {
-      // Article
-      if (
-        obj.typeHandle !== 'externalContent'
-        && obj.contentType.includes('article')
-      ) {
-        return {
-          ...obj,
-          to: stripMeapFromURI(obj.to),
-          parsedImage: _get(
-            obj,
-            'heroImage[0].image[0]',
-            undefined
-          ),
-          parsedLocation: _get(
-            obj,
-            'associatedLocations',
-            []
-          ),
-          parsedCategory: _get(
-            obj,
-            'articleCategory[0].title',
-            ''
-          ),
-          byline1: _get(obj, 'articleByline1[0].title', ''),
-          byline2:
-            obj.articleByline2 !== null
-              ? formatDates(
-                obj.articleByline2,
-                obj.articleByline2
-              )
-              : '',
+  } else {
+    return parsedList.value
+      .filter(e => e !== null)
+      .map((obj) => {
+        // Article
+        if (
+          obj.typeHandle !== 'externalContent'
+          && obj.contentType.includes('article')
+        ) {
+          return {
+            ...obj,
+            to: stripMeapFromURI(obj.to),
+            parsedImage: _get(
+              obj,
+              'heroImage[0].image[0]',
+              undefined
+            ),
+            parsedLocation: _get(
+              obj,
+              'associatedLocations',
+              []
+            ),
+            parsedCategory: _get(
+              obj,
+              'articleCategory[0].title',
+              ''
+            ),
+            byline1: _get(obj, 'articleByline1[0].title', ''),
+            byline2:
+              obj.articleByline2 !== null
+                ? formatDates(
+                  obj.articleByline2,
+                  obj.articleByline2
+                )
+                : '',
+          }
         }
-      }
 
-      // Project
-      else if (
-        obj.typeHandle !== 'externalContent'
-        && obj.contentType.includes('meapProject')
-      ) {
-        return {
-          ...obj,
-          to: stripMeapFromURI(obj.to),
-          parsedImage: _get(
-            obj,
-            'heroImage[0].image[0]',
-            undefined
-          ),
-          parsedLocation: _get(obj, 'projectLocations', []),
-          parsedCategory: _get(obj, 'projectCategory', ''),
-          byline1: _get(obj, 'projectByline1[0].title', ''),
+        // Project
+        else if (
+          obj.typeHandle !== 'externalContent'
+          && obj.contentType.includes('meapProject')
+        ) {
+          return {
+            ...obj,
+            to: stripMeapFromURI(obj.to),
+            parsedImage: _get(
+              obj,
+              'heroImage[0].image[0]',
+              undefined
+            ),
+            parsedLocation: _get(obj, 'projectLocations', []),
+            parsedCategory: _get(obj, 'projectCategory', ''),
+            byline1: _get(obj, 'projectByline1[0].title', ''),
+          }
         }
-      }
-      else if (
-        obj.typeHandle !== 'externalContent'
-        && obj.contentType === 'event'
-      ) {
-        return {
-          ...obj,
-          to: stripMeapFromURI(obj.to),
-          parsedImage: _get(
-            obj,
-            'heroImage[0].image[0]',
-            undefined
-          ),
-          parsedLocation: _get(
-            obj,
-            'associatedLocations',
-            []
-          ),
-          parsedCategory: _get(obj, 'eventType.title', ''),
-          startDate: _get(obj, 'startDateWithTime', ''),
-          endDate: _get(obj, 'endDateWithTime', ''),
-          text: _get(obj, 'eventDescription', ''),
+        else if (
+          obj.typeHandle !== 'externalContent'
+          && obj.contentType === 'event'
+        ) {
+          return {
+            ...obj,
+            to: stripMeapFromURI(obj.to),
+            parsedImage: _get(
+              obj,
+              'heroImage[0].image[0]',
+              undefined
+            ),
+            parsedLocation: _get(
+              obj,
+              'associatedLocations',
+              []
+            ),
+            parsedCategory: _get(obj, 'eventType.title', ''),
+            startDate: _get(obj, 'startDateWithTime', ''),
+            endDate: _get(obj, 'endDateWithTime', ''),
+            text: _get(obj, 'eventDescription', ''),
+          }
         }
-      }
-      else if (
-        obj.typeHandle !== 'externalContent'
-        && (obj.contentType === 'exhibition'
-          || 'workshopOrEventSeries')
-      ) {
-        return {
-          ...obj,
-          to: stripMeapFromURI(obj.to),
-          parsedImage: _get(
-            obj,
-            'heroImage[0].image[0]',
-            undefined
-          ),
-          parsedLocation: _get(
-            obj,
-            'associatedLocations',
-            []
-          ),
-          startDate: _get(obj, 'startDate', ''),
-          endDate: _get(obj, 'endDate', ''),
+        else if (
+          obj.typeHandle !== 'externalContent'
+          && (obj.contentType === 'exhibition'
+            || 'workshopOrEventSeries')
+        ) {
+          return {
+            ...obj,
+            to: stripMeapFromURI(obj.to),
+            parsedImage: _get(
+              obj,
+              'heroImage[0].image[0]',
+              undefined
+            ),
+            parsedLocation: _get(
+              obj,
+              'associatedLocations',
+              []
+            ),
+            startDate: _get(obj, 'startDate', ''),
+            endDate: _get(obj, 'endDate', ''),
+          }
         }
-      }
-      else if (obj.typeHandle === 'externalContent') {
-        return {
-          ...obj,
-          parsedImage: _get(obj, 'image[0]', undefined),
-          // parsedLocation:  obj.location !== null ? [obj.location] : [],
-          parsedCategory: _get(obj, 'category', ''),
+        else if (obj.typeHandle === 'externalContent') {
+          return {
+            ...obj,
+            parsedImage: _get(obj, 'image[0]', undefined),
+            // parsedLocation:  obj.location !== null ? [obj.location] : [],
+            parsedCategory: _get(obj, 'category', ''),
+          }
         }
-      }
-      else {
-        return {
-          ...obj,
-          parsedImage: _get(
-            obj,
-            'heroImage[0].image[0]',
-            undefined
-          ),
-          to: stripMeapFromURI(obj.to),
+        else {
+          return {
+            ...obj,
+            parsedImage: _get(
+              obj,
+              'heroImage[0].image[0]',
+              undefined
+            ),
+            to: stripMeapFromURI(obj.to),
+          }
         }
-      }
-    })
+      })
+  }
 })
+
 // THEME
 const theme = useTheme()
 const classes = computed(() => {
@@ -189,6 +234,7 @@ const classes = computed(() => {
     v-if="block.cardWithImage"
     :class="classes"
   >
+    <h3>{{ parsedList }}</h3>
     <div class="section-header">
       <h2
         v-if="block.sectionTitle"
@@ -222,6 +268,7 @@ const classes = computed(() => {
         :end-date="item.endDate"
         :section-handle="item.contentType"
         :ongoing="item.ongoing"
+        :date-format="parsedDateFormat"
         class="block"
       />
     </ul>
