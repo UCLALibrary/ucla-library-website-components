302c0666495723732818b219de5ea81485df2a68
Luka Vuk <luka.vuk0909@gmail.com>
2025-06-26 20:42:23 +0200

    format a component with prettier

--- packages/vue-component-library/src/lib-components/ButtonSubmit.vue
+++ packages/vue-component-library/src/lib-components/ButtonSubmit.vue
@@ -1,70 +1,61 @@
 <script setup>
-import { computed, useSlots } from 'vue'
+// Imports
+import { computed, useSlots } from "vue"
+import IconSearch from "ucla-library-design-tokens/assets/svgs/icon-search.svg"
 
-// Components
-import IconSearch from 'ucla-library-design-tokens/assets/svgs/icon-search.svg'
-
-const emit = defineEmits(['click'])
+const emit = defineEmits(["click"])
 const slots = useSlots()
-
-// Computeds
-const hasSlot = computed(() => {
-  return !!slots.default
-})
 
 // Methods
 function handleClick() {
-  emit('click')
+    emit("click")
 }
 </script>
 
 <template>
-  <button
-    class="button-submit"
-    type="submit"
-    @click="handleClick"
-  >
-    <slot v-if="hasSlot" />
-    <IconSearch v-else />
-  </button>
+    <button class="button-submit" type="submit" @click="handleClick">
+        <slot>
+            <IconSearch />
+        </slot>
+    </button>
 </template>
 
 <style lang="scss" scoped>
 .button-submit {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-  height: 74px;
-  width: 74px;
-  padding: 0;
-  background-color:  var(--color-primary-blue-01);
-
-  // Target SVG elements
-  :deep(svg path){
-    fill: var(--color-primary-blue-04);
-    transition: fill 0.3s ease-in-out;
-  }
-
-  // Hovers
-  @media #{$has-hover} {
-    &:hover {
-      :deep(svg path) {
-        fill: var(--color-primary-blue-02);
-      }
-    }
-  }
-  // Breakpoints
-  @media #{$medium} {
-    height: 53px;
-  }
-  @media #{$small} {
-    height: 60px;
+    height: 74px;
+    width: 74px;
+    padding: 0;
+    background-color: var(--color-primary-blue-01);
 
     // Target SVG elements
-    :deep(svg path){
-      fill: var(--color-primary-blue-03);
+    :deep(svg path) {
+        fill: var(--color-primary-blue-04);
+        transition: fill 0.3s ease-in-out;
     }
-  }
+
+    // Hovers
+    @media #{$has-hover} {
+        &:hover {
+            :deep(svg path) {
+                fill: var(--color-primary-blue-02);
+            }
+        }
+    }
+    // Breakpoints
+    @media #{$medium} {
+        height: 53px;
+    }
+    @media #{$small} {
+        height: 60px;
+
+        // Target SVG elements
+        :deep(svg path) {
+            fill: var(--color-primary-blue-03);
+        }
+    }
 }
 </style>
