061fa24b3cb811222fa402e99cfe4a489e377a53
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-06-27 11:37:25 +0200

    Typing the props correctly, adding defaults

--- packages/vue-component-library/src/lib-components/SearchResultsCount.vue
+++ packages/vue-component-library/src/lib-components/SearchResultsCount.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref, computed } from "vue"
 
-const props = defineProps<{
-    count: number
-    label?: string
-    prefix?: string
-    animate?: boolean
-}>()
+const props = withDefaults(
+    defineProps<{
+        count: number
+        label?: string
+        prefix?: string
+        animate?: boolean
+    }>(),
+    {
+        animate: false,
+    }
+)
 
 const parsedResults = computed(() => {
     if (props.prefix && props.label) {
