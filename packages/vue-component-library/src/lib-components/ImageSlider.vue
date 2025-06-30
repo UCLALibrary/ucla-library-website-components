b56324bc1d6f805e1bb27c5207337907e61b93d4
Jess Divers <jessica.divers@library.ucla.edu>
2025-06-30 13:37:58 -0700

    chroe: mock data for stories

--- packages/vue-component-library/src/lib-components/ImageSlider.vue
+++ packages/vue-component-library/src/lib-components/ImageSlider.vue
@@ -1,148 +1,143 @@
 <script lang="ts" setup>
-import ResponsiveImage from './ResponsiveImage.vue';
-import { ref, watch } from 'vue';
+import { ref } from 'vue'
+import ResponsiveImage from './ResponsiveImage.vue'
 
 const { beforeImage, afterImage } = defineProps({
-    beforeImage: {
-        type: Object,
-        required: true
-    },
-    afterImage: {
-        type: Object,
-        required: true
-    }
+  beforeImage: {
+    type: Object,
+    required: true
+  },
+  afterImage: {
+    type: Object,
+    required: true
+  }
 })
 
-
-const sliderContainer = ref<HTMLElement | null>(null);
-const slider = ref<HTMLInputElement | null>(null);
+const sliderContainer = ref<HTMLElement | null>(null)
+const slider = ref<HTMLInputElement | null>(null)
 
 function handleSliderInput(event: Event) {
-    const target = event.target as HTMLInputElement;
-    console.log('Slider input event:', target.value);
-    if (sliderContainer.value) {
-        sliderContainer.value.style.setProperty('--position', `${target.value}%`);
-    }
+  const target = event.target as HTMLInputElement
+  console.log('Slider input event:', target.value)
+  if (sliderContainer.value)
+    sliderContainer.value.style.setProperty('--position', `${target.value}%`)
 }
-// watch(() => slider.value?._value, (newValue) => {
-//     console.log('Slider value changed:', newValue);
-//     if (sliderContainer.value) {
-//         sliderContainer.value.style.setProperty('--position', `${newValue}%`);
-//     }
-// });
-
 </script>
+
 <template>
-    <div ref="sliderContainer" class="image-slider">
-        <div class="image-container">
-            <ResponsiveImage :media="beforeImage" class="slider-image before-image" />
-            <ResponsiveImage :media="afterImage" class="slider-image after-image" />
-            <!-- <div class="image-labels">
+  <div ref="sliderContainer" class="image-slider">
+    <div class="image-container">
+      <ResponsiveImage :media="beforeImage" class="slider-image before-image" />
+      <ResponsiveImage :media="afterImage" class="slider-image after-image" />
+      <!-- <div class="image-labels">
                 <span class="before-label"><slot name="beforeLabel">Before</slot></span>
                 <span class="after-label"><slot name="afterLabel">After</slot></span>
             </div> -->
-        </div>
-        <input
-            ref="slider"
-            type="range"
-            min="0"
-            max="100"
-            value="50"
-            aria-label="Percentage of before photo shown"
-            class="slider"
-            @input="(e) => handleSliderInput(e)"
-        />
-        <div class="slider-line" aria-hidden="true"></div>
-        <div class="slider-button" aria-hidden="true">
-            <span class="button-text">Slide to Compare</span>
-        </div>
     </div>
+    <input
+      ref="slider"
+      type="range"
+      min="0"
+      max="100"
+      value="50"
+      aria-label="Percentage of before photo shown"
+      class="slider"
+      @input="(e) => handleSliderInput(e)"
+    >
+    <div class="slider-line" aria-hidden="true" />
+    <div class="slider-button" aria-hidden="true">
+      <span class="button-text">Slide to Compare</span>
+    </div>
+  </div>
 </template>
+
 <style lang="scss" scoped>
 /* Add your styles here */
-// *,
-// *::after,
-// *::before {
-//     margin: 0;
-//     padding: 0;
-//     box-sizing: border-box;
-// }
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
-// .image-slider {
-//     display: grid;
-//     // width: 100%;
-//     width: 100vw;
-//     place-content: center;
-//     position: relative;
-//     overflow: hidden;
-//     border-radius: 1rem;
-//     --position: 50%;
-// }
-// .image-container {
-//     width: 100%;
-//     max-width: 1160px;
-//     max-height: 90vh;
-//     aspect-ratio: 1/1;
-//     :deep(img) {
-//         display: block;
-//         max-width: 100%;
-//     }
-// }
+.image-slider {
+    display: grid;
+    width: 100%;
+    max-width: 1160px;
+    // width: 100vw;
+    place-content: center;
+    position: relative;
+    overflow: hidden;
+    border-radius: 1rem;
+    --position: 50%;
+}
+.image-container {
+    position: relative;
+    width: 100%;
+    max-height: 90vh;
+    aspect-ratio: 1/1;
+    :deep(img) {
+        display: block;
+        max-width: 100%;
+    }
+    background: red;
+}
 
-// .slider-image {
-//     width: 100%;
-//     height: 100%;
-//     object-fit: cover;
-//     object-position: left;
-// }
+.slider-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: left;
+}
 
-// .image-before {
-//     position: absolute;
-//     inset: 0;
-//     width: var(--position);
-//     filter: grayscale(100%)
-// }
+.before-image {
+    position: absolute;
+    inset: 0;
+    width: var(--position);
+    filter: grayscale(100%)
+}
 
-// .slider {
-//     position: absolute;
-//     inset: 0;
-//     cursor: pointer;
-//     opacity: 0;
-//     /* for Firefox */
-//     width: 100%;
-//     height: 100%;
-// }
+.slider {
+    position: absolute;
+    inset: 0;
+    cursor: pointer;
+    opacity: 0;
+    /* for Firefox */
+    width: 100%;
+    height: 100%;
+}
 
-// .slider:focus-visible~.slider-button {
-//     outline: 5px solid black;
-//     outline-offset: 3px;
-// }
+.slider:focus-visible~.slider-button {
+    outline: 5px solid black;
+    outline-offset: 3px;
+}
 
-// .slider-line {
-//     position: absolute;
-//     inset: 0;
-//     width: .2rem;
-//     height: 100%;
-//     background-color: #fff;
-//     /* z-index: 10; */
-//     left: var(--position);
-//     transform: translateX(-50%);
-//     pointer-events: none;
-// }
+.slider-line {
+    position: absolute;
+    inset: 0;
+    width: .2rem;
+    height: 100%;
+    background-color: #fff;
+    /* z-index: 10; */
+    left: var(--position);
+    transform: translateX(-50%);
+    pointer-events: none;
+}
 
-// .slider-button {
-//     position: absolute;
-//     background-color: #fff;
-//     color: black;
-//     padding: .5rem;
-//     border-radius: 100vw;
-//     display: grid;
-//     place-items: center;
-//     top: 50%;
-//     left: var(--position);
-//     transform: translate(-50%, -50%);
-//     pointer-events: none;
-//     /* z-index: 100; */
-//     box-shadow: 1px 1px 1px hsl(0, 50%, 2%, .5);
-// }
+.slider-button {
+    position: absolute;
+    background-color: #fff;
+    color: black;
+    padding: .5rem;
+    border-radius: 100vw;
+    display: grid;
+    place-items: center;
+    top: 50%;
+    left: var(--position);
+    transform: translate(-50%, -50%);
+    pointer-events: none;
+    /* z-index: 100; */
+    box-shadow: 1px 1px 1px hsl(0, 50%, 2%, .5);
+}
 </style>
--- packages/vue-component-library/src/stories/ImageSlider.stories.js
+++ packages/vue-component-library/src/stories/ImageSlider.stories.js
@@ -1,18 +1,47 @@
 // import { computed } from 'vue'
 import ImageSlider from '../lib-components/ImageSlider'
-import * as API from '@/stories/mock-api.json'
 
 export default {
   title: 'Image Slider',
   component: ImageSlider,
 }
 
+const mockImage = [
+  {
+    id: '3512399',
+    src: 'https://static.library.ucla.edu/craftassetstest/FTVA/_fullscreen/Ericka-Beckman-Hiatus-1999.-Image-courtesy-the-artists.jpg',
+    height: 1748,
+    width: 2560,
+    srcset: 'https://static.library.ucla.edu/craftassetstest/FTVA/_375xAUTO_crop_center-center_none/Ericka-Beckman-Hiatus-1999.-Image-courtesy-the-artists.jpg 375w, https://static.library.ucla.edu/craftassetstest/FTVA/_960xAUTO_crop_center-center_none/Ericka-Beckman-Hiatus-1999.-Image-courtesy-the-artists.jpg 960w, https://static.library.ucla.edu/craftassetstest/FTVA/_1280xAUTO_crop_center-center_none/Ericka-Beckman-Hiatus-1999.-Image-courtesy-the-artists.jpg 1280w, https://static.library.ucla.edu/craftassetstest/FTVA/_1920xAUTO_crop_center-center_none/Ericka-Beckman-Hiatus-1999.-Image-courtesy-the-artists.jpg 1920w, https://static.library.ucla.edu/craftassetstest/FTVA/_2560xAUTO_crop_center-center_none/Ericka-Beckman-Hiatus-1999.-Image-courtesy-the-artists.jpg 2560w',
+    alt: null,
+    focalPoint: [
+      0.5,
+      0.5
+    ]
+  }
+]
+
+const mockBeforeImage = [
+  {
+    id: '3280520',
+    src: 'https://static.library.ucla.edu/craftassetstest/FTVA/_fullscreen/hot_air_balloon.jpg',
+    height: 1748,
+    width: 2560,
+    srcset: 'https://static.library.ucla.edu/craftassetstest/FTVA/_375xAUTO_crop_center-center_none/hot_air_balloon.jpg 375w, https://static.library.ucla.edu/craftassetstest/FTVA/_960xAUTO_crop_center-center_none/hot_air_balloon.jpg 960w, https://static.library.ucla.edu/craftassetstest/FTVA/_1280xAUTO_crop_center-center_none/hot_air_balloon.jpg 1280w, https://static.library.ucla.edu/craftassetstest/FTVA/_1920xAUTO_crop_center-center_none/hot_air_balloon.jpg 1920w, https://static.library.ucla.edu/craftassetstest/FTVA/_2560xAUTO_crop_center-center_none/hot_air_balloon.jpg 2560w',
+    alt: 'Hot air balloon',
+    focalPoint: [
+      0.5,
+      0.5
+    ]
+  }
+]
+
 // Mock data for the ImageSlider carousel, which is a carousel of ImageSlider components
 const mockBeforeAfterImageCarousel = [{
-    id: '37072',
-    caption: 'Before and After Image Slider caption #1',
-    beforeImage: API.image,
-    afterImage: API.image
+  id: '37072',
+  caption: 'Before and After Image Slider caption #1',
+  beforeImage: mockBeforeImage[0],
+  afterImage: mockImage[0]
 }]
 
 export function Default() {
