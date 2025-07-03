4aed0da4d88df1f18e23c4cb8790a43782c96e8d
angelahuqing <angelahuqing@gmail.com>
2025-07-03 13:27:30 -0700

    fix: APPS-3342 Update cardThreeColumn to include category/series title above the main title

--- packages/vue-component-library/src/stories/BlockCardThreeColumn.stories.js
+++ packages/vue-component-library/src/stories/BlockCardThreeColumn.stories.js
@@ -16,6 +16,7 @@ const mockDefault = {
   to: 'series/todd-solondz-series',
   startDate: '2025-12-13T06:30:00+00:00',
   image: API.image,
+  category: "ULLAMCO",
   title: 'TEST - Todd Solondz Series',
   endDate: '2025-12-13T05:45:00+00:00',
   tagLabels: [
@@ -33,6 +34,7 @@ const mockSingleDigitDate = {
   to: 'series/todd-solondz-series',
   startDate: '2026-02-03T06:30:00+00:00',
   image: API.image,
+  category: "ULLAMCO",
   title: 'TEST - Todd Solondz Series',
   endDate: '2026-04-08T05:45:00+00:00',
   tagLabels: [
@@ -136,6 +138,30 @@ export function NoImage(args) {
         :image=null
         :image-aspect-ratio="100"
         title="What I Really Do Is Magic: Edith Head and Hollywood Costume Design"
+        :tagLabels="tagLabels"
+      />`,
+  }
+}
+
+export function Category(args) {
+  return {
+    data() {
+      return { ...mockDefault }
+    },
+    provide() {
+      return {
+        theme: computed(() => 'ftva'),
+      }
+    },
+    components: { BlockCardThreeColumn },
+    template: `
+      <block-card-three-column
+        :to="to"
+        :start-date="startDate"
+        :image="image"
+        :image-aspect-ratio="100"
+        :category="category"
+        :title="title"
         :tagLabels="tagLabels"
       />`,
   }
