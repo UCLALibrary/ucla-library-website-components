8665246e4648eb92863adcd085f08c49a86680c7
Luka Vuk <luka.vuk0909@gmail.com>
2025-07-07 20:51:32 +0200

    add button tag stories

---
+++ packages/vue-component-library/src/stories/ButtonTag.stories.js
@@ -0,0 +1,200 @@
+import { computed } from "vue"
+import ButtonTag from "@/lib-components/ButtonTag"
+import {
+    ButtonTagIcons,
+    ButtonTagVariants,
+} from "@/types/components/buttonTag.types"
+
+/**
+ * A component to display a non-interactive tag with a label and an optional icon. <br>
+ * <sub>(For an interactive tag that can be clicked to remove it from a list, try BlockRemoveSearchFilter). </sub>
+ *
+ * Props:
+ * - <b>label</b>: A string representing the text to display in the tag
+ * - <b>iconName</b>: An optional icon to display in the tag, imported from `@/types/components/buttonTag.types`
+ * <sub>Valid names: `ButtonTagIcons.SvgIconGuest`, `ButtonTagIcons.SvgIconFilm`, `ButtonTagIcons.SvgIconTV`, `ButtonTagIcons.SvgIconFilmreel`, `ButtonTagIcons.SvgIconDigital`, `ButtonTagIcons.SvgIconOnline`, `ButtonTagIcons.SvgIconFamilyFriendly`</sub>
+ * - <b>variant</b>: The visual style of the tag, imported from `@/types/components/buttonTag.types`
+ * <sub>Valid variants: `ButtonTagVariants.Primary`, `ButtonTagVariants.Secondary`</sub>
+ *
+ */
+
+export default {
+    title: "Funkhaus / Tag",
+    component: ButtonTag,
+}
+
+export function Default() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "default label",
+                },
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function DefaultHighlighted() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "highlighted label",
+                    isHighlighted: true,
+                },
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function DefaultWithMultipleLabels() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: ["Genre", "Black and White Photographs"],
+                    iconName: ButtonTagIcons.SvgIconFilmreel,
+                },
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function DefaultWithOnClick() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "Clear Filter",
+                    isHighlighted: true,
+                    onClick: () => {
+                        alert("Filter cleared!")
+                    },
+                },
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function PrimaryFTVA() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "ftva primary label",
+                    variant: ButtonTagVariants.Primary,
+                },
+            }
+        },
+        provide() {
+            return {
+                theme: computed(() => "ftva"),
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function SecondaryFTVA() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "ftva secondary label",
+                    iconName: ButtonTagIcons.SvgIconGuest,
+                    variant: ButtonTagVariants.Secondary,
+                },
+            }
+        },
+        provide() {
+            return {
+                theme: computed(() => "ftva"),
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function SecondaryFTVANoIcon() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "ftva secondary no icon label",
+                    variant: ButtonTagVariants.Secondary,
+                },
+            }
+        },
+        provide() {
+            return {
+                theme: computed(() => "ftva"),
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
+
+export function SecondaryFTVAHighlighted() {
+    return {
+        data() {
+            return {
+                props: {
+                    label: "ftva secondary highlighted label",
+                    variant: ButtonTagVariants.Secondary,
+                    isHighlighted: true,
+                },
+            }
+        },
+        provide() {
+            return {
+                theme: computed(() => "ftva"),
+            }
+        },
+        components: { ButtonTag },
+        template: `
+    <button-tag
+        v-bind="props"
+    />
+  `,
+    }
+}
