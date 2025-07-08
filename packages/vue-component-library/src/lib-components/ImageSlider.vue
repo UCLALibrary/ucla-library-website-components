2cac84f000a8b1952ec8bdd9011f1dcf3899fbc6
Jess <jessica.divers@gmail.com>
2025-07-08 11:24:17 -0700

    feat: APPS-3376 create image slider component (#751)
    * feat: add image slider skeleton
    
    * feat: add handlerSliderInput function
    
    * chroe: mock data for stories
    
    * fix: slider solution for non-square sliders
    
    * chore: add export, run linter
    
    * feat: hookup new slider logic, better images, styles
    
    * feat: add labels and caption
    
    * fix: run linter
    
    * fix: cleanup comments
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>

---
+++ packages/vue-component-library/src/lib-components/ImageSlider.vue
@@ -0,0 +1,182 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import SvgIconCaretLeft from 'ucla-library-design-tokens/assets/svgs/icon-caret-left.svg'
+import SvgIconCaretRight from 'ucla-library-design-tokens/assets/svgs/icon-caret-right.svg'
+
+const { beforeImage, afterImage } = defineProps({
+  beforeImage: {
+    type: Object,
+    required: true
+  },
+  afterImage: {
+    type: Object,
+    required: true
+  }
+})
+
+const sliderContainer = ref<HTMLElement | null>(null)
+const slider = ref<HTMLInputElement | null>(null)
+const afterImageElement = ref<HTMLImageElement | null>(null)
+
+function handleSliderInput(event: Event) {
+  const target = event.target as HTMLInputElement
+  if (sliderContainer.value && slider.value && afterImageElement.value)
+    sliderContainer.value.style.setProperty('--position', `${target.value}%`)
+}
+</script>
+
+<template>
+  <div ref="sliderContainer" class="image-slider">
+    <div class="image-container">
+      <img class="before-image slider-image" :src="beforeImage.src" alt="color photo">
+      <img ref="afterImageElement" class="after-image slider-image" :src="afterImage.src" alt="black and white">
+      <div class="image-labels">
+        <span class="before-label slider-label">
+          <slot name="beforeLabel">Before</slot>
+        </span>
+        <span class="after-label slider-label">
+          <slot name="afterLabel">After</slot>
+        </span>
+      </div>
+    </div>
+    <input
+      ref="slider" type="range" min="0" max="100" value="50" aria-label="Percentage of before photo shown"
+      class="slider" @input="(e) => handleSliderInput(e)"
+    >
+    <div class="slider-line" aria-hidden="true" />
+    <div class="slider-button" aria-hidden="true">
+      <span class="button-text">
+        <SvgIconCaretLeft class="button-icon" />
+        <SvgIconCaretRight class="button-icon" />
+      </span>
+    </div>
+  </div>
+  <div class="caption">
+    <slot name="captionText" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+/* Add your styles here */
+*,
+*::after,
+*::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.image-slider {
+    position: relative;
+    display: grid;
+    width: 100%;
+    height: 400px;
+    max-width: 1160px;
+    place-content: center;
+    position: relative;
+    overflow: hidden;
+    --position: 50%; // Initial position of the slider
+    .image-container {
+        width: 100%;
+        max-width: 1160px;
+        max-height: 90vh;
+        overflow: hidden;
+        aspect-ratio: 2/1;
+
+        :deep(img) {
+          display: block;
+          max-width: 100%;
+
+        }
+      }
+}
+
+.image-labels {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  z-index: 9;
+  padding: 1rem;
+  span.slider-label {
+    text-transform: uppercase;
+    color: black;
+    background-color: white;
+    padding: .25rem .5rem;
+  }
+}
+
+.slider-image {
+  position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    resize: horizontal;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+}
+
+.after-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  clip-path: polygon(0 0, var(--position) 0, var(--position) 100%, 0 100%); // Adjust the width of the visible area
+}
+
+.caption {
+  @include ftva-caption;
+  padding-top: 15px;
+  width: 100%;
+  text-align: center;
+  color: #737373;
+}
+
+.slider {
+    position: absolute;
+    inset: 0;
+    cursor: pointer;
+    opacity: 0;
+    z-index: 10; // keep this element on top of the images
+    /* for Firefox */
+    width: 100%;
+    height: 100%;
+}
+
+.slider:focus-visible~.slider-button {
+    outline: 5px solid black;
+    outline-offset: 3px;
+}
+
+.slider-line {
+    position: absolute;
+    inset: 0;
+    width: .2rem;
+    height: 100%;
+    background-color: #fff;
+    z-index: 10;
+    left: var(--position);
+    transform: translateX(-50%);
+    pointer-events: none;
+}
+
+.slider-button {
+    position: absolute;
+    background-color: #fff;
+    color: black;
+    border-radius: 100vw;
+    min-width: 75px; // set minimum width to prevent icons wrapping near slider edges
+    padding: .25rem 0 0 0;
+    display: grid;
+    place-items: center;
+    top: 50%;
+    left: var(--position);
+    transform: translate(-50%, -50%);
+    pointer-events: none;
+    z-index: 100;
+}
+</style>
---
+++ packages/vue-component-library/src/stories/ImageSlider.stories.js
@@ -0,0 +1,88 @@
+import ImageSlider from '../lib-components/ImageSlider'
+
+export default {
+  title: 'Image Slider',
+  component: ImageSlider,
+}
+
+const mockBeforeImage = [
+  {
+    id: '3280520',
+    src: 'https://static.library.ucla.edu/craftassetstest/FTVA/_fullscreen/hot_air_balloon.jpg',
+    height: 1748,
+    width: 2560,
+    srcset: 'https://static.library.ucla.edu/craftassetstest/FTVA/_375xAUTO_crop_center-center_none/hot_air_balloon.jpg 375w, https://static.library.ucla.edu/craftassetstest/FTVA/_960xAUTO_crop_center-center_none/hot_air_balloon.jpg 960w, https://static.library.ucla.edu/craftassetstest/FTVA/_1280xAUTO_crop_center-center_none/hot_air_balloon.jpg 1280w, https://static.library.ucla.edu/craftassetstest/FTVA/_1920xAUTO_crop_center-center_none/hot_air_balloon.jpg 1920w, https://static.library.ucla.edu/craftassetstest/FTVA/_2560xAUTO_crop_center-center_none/hot_air_balloon.jpg 2560w',
+    alt: 'Hot air balloon',
+    focalPoint: [
+      0.5,
+      0.5
+    ]
+  }
+]
+
+// Mock data for the ImageSlider carousel, which is a carousel of ImageSlider components
+const mockBeforeAfterImageCarousel = [{
+  id: '37072',
+  caption: 'Before and After Image Slider caption #1',
+  beforeImage: mockBeforeImage[0],
+  afterImage: mockBeforeImage[0]
+}]
+
+export function Default() {
+  return {
+    data() {
+      return {
+        ...mockBeforeAfterImageCarousel[0],
+      }
+    },
+    components: { ImageSlider },
+    template: `
+    <component is="style" type="text/css">
+.before-image {
+    filter: grayscale(100%)
+}
+    </component>
+        <div style="padding: 1rem; max-width: 1160px;">
+        <image-slider
+            :before-image="beforeImage"
+            :after-image="afterImage"
+        >
+            <template #captionText>
+               {{ caption }}
+            </template>
+        </image-slider>
+        </div>
+    `,
+  }
+}
+
+export function WithCustomLabels() {
+  return {
+    data() {
+      return {
+        ...mockBeforeAfterImageCarousel[0],
+      }
+    },
+    components: { ImageSlider },
+    template: `
+        <component is="style" type="text/css">
+.before-image {
+    filter: grayscale(100%)
+}
+    </component>
+        <div style="padding: 1rem; max-width: 1160px;">
+        <image-slider
+            :before-image="beforeImage"
+            :after-image="afterImage"
+        >
+            <template #beforeLabel>
+               Color
+            </template>
+            <template #afterLabel>
+              Black and White
+            </template>
+        </image-slider>
+        </div>
+    `,
+  }
+}
