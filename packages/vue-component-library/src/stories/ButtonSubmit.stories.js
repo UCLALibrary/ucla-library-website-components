1a7297f13651fc818485e68e6150e460f991ed02
Luka Vuk <luka.vuk0909@gmail.com>
2025-07-23 19:40:45 +0200

    add dlc theme

--- packages/vue-component-library/src/stories/ButtonSubmit.stories.js
+++ packages/vue-component-library/src/stories/ButtonSubmit.stories.js
@@ -1,41 +1,52 @@
-import ButtonSubmit from '@/lib-components/ButtonSubmit'
-import SvgIconLocker from 'ucla-library-design-tokens/assets/svgs/icon-locker.svg'
+import ButtonSubmit from "@/lib-components/ButtonSubmit"
+import SvgIconLocker from "ucla-library-design-tokens/assets/svgs/icon-locker.svg"
+import { computed } from "vue"
 
 export default {
-  title: 'Funkhaus / ButtonSubmit',
-  component: ButtonSubmit,
+    title: "Funkhaus / ButtonSubmit",
+    component: ButtonSubmit,
 }
 
 const methods = {
-  onSubmit() {
-    alert('Button clicked!')
-  },
+    onSubmit() {
+        alert("Button clicked!")
+    },
 }
 
 // Variations of stories below
 export function Default() {
-  return {
-    components: { ButtonSubmit },
-    methods: methods,
-    template: `<button-submit @click="onSubmit" />`,
-  }
+    return {
+        components: { ButtonSubmit },
+        provide() {
+            return {
+                theme: computed(() => "dlc"),
+            }
+        },
+        methods: methods,
+        template: `<button-submit @click="onSubmit" />`,
+    }
 }
 
 export function WithCustomIcon() {
-  return {
-    components: { ButtonSubmit },
-    data() {
-      return {
-        customIcon: SvgIconLocker,
-      }
-    },
-    methods: methods,
-    template: `
+    return {
+        components: { ButtonSubmit },
+        provide() {
+            return {
+                theme: computed(() => "dlc"),
+            }
+        },
+        data() {
+            return {
+                customIcon: SvgIconLocker,
+            }
+        },
+        methods: methods,
+        template: `
       <button-submit @click="onSubmit">
         <template #default>
           <component :is="customIcon" />
         </template>
       </button-submit>
     `,
-  }
+    }
 }
