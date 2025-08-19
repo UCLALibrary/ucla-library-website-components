b98196f60c95baa227b9be0277446bf97485975f
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-08-19 14:56:31 +0200

    Add's props for testing the component visually, as the storybook breaks on interacting with router/route.

--- packages/vue-component-library/src/lib-components/ButtonPageView.vue
+++ packages/vue-component-library/src/lib-components/ButtonPageView.vue
@@ -4,9 +4,7 @@ import SvgList from 'ucla-library-design-tokens/assets/svgs/icon-list-view.svg'
 import SvgGallery from 'ucla-library-design-tokens/assets/svgs/icon-gallery.svg'
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-
 import { useTheme } from '@/composables/useTheme'
-
 // CHILD COMPONENTS
 import SmartLink from '@/lib-components/SmartLink.vue'
 
@@ -17,6 +15,14 @@ const theme = useTheme()
 const route = useRoute()
 const router = useRouter()
 
+// Storybook mock prop
+const props = defineProps({
+  storybookMock: { type: Boolean, default: false },
+  isListView: { type: Boolean, default: undefined },
+  isGalleryView: { type: Boolean, default: undefined },
+})
+const emit = defineEmits(['view-change'])
+
 // Computed
 const classes = computed(() => ['button-page-view', theme?.value || ''])
 
@@ -26,38 +32,39 @@ const currentView = computed(() => {
   return viewParam === 'gallery' ? 'gallery' : 'list'
 })
 
-const isListView = computed(() => currentView.value === 'list')
-const isGalleryView = computed(() => currentView.value === 'gallery')
+// Use props if storybookMock, else use router logic
+const isListView = computed(() =>
+  props.storybookMock && props.isListView !== undefined
+    ? props.isListView
+    : currentView.value === 'list'
+)
+const isGalleryView = computed(() =>
+  props.storybookMock && props.isGalleryView !== undefined
+    ? props.isGalleryView
+    : currentView.value === 'gallery'
+)
 
-// Function to update the view in the URL
+// Function to update the view in the URL or emit event for Storybook
 function updateView(view: 'list' | 'gallery') {
-  router.push({
-    query: { ...route.query, view }
-  })
+  if (props.storybookMock) {
+    emit('view-change', view)
+  } else {
+    router.push({
+      query: { ...route.query, view }
+    })
+  }
 }
 </script>
 
 <template>
   <div :class="classes">
-    <SmartLink
-      class="button-wrapper"
-      :class="{ active: isListView }"
-      type="button"
-      :aria-pressed="isListView"
-      aria-label="List view"
-      @click="updateView('list')"
-    >
+    <SmartLink class="button-wrapper" :class="{ active: isListView }" type="button" :aria-pressed="isListView"
+      aria-label="List view" @click="updateView('list')">
       <SvgList class="icon list" />
     </SmartLink>
 
-    <SmartLink
-      class="button-wrapper"
-      :class="{ active: isGalleryView }"
-      type="button"
-      :aria-pressed="isGalleryView"
-      aria-label="Card view"
-      @click="updateView('gallery')"
-    >
+    <SmartLink class="button-wrapper" :class="{ active: isGalleryView }" type="button" :aria-pressed="isGalleryView"
+      aria-label="Card view" @click="updateView('gallery')">
       <SvgGallery class="icon gallery" />
     </SmartLink>
   </div>
