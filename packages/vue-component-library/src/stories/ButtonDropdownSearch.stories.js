e74381f159dd9e072f991a372431380e632490be
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-08-20 20:09:13 +0200

    feat: create ButtonDropdownSearch

---
+++ packages/vue-component-library/src/stories/ButtonDropdownSearch.stories.js
@@ -0,0 +1,59 @@
+import ButtonDropdownSearch from "@/lib-components/ButtonDropdownSearch.vue"
+import { ref, computed } from "vue"
+
+export default {
+    title: "Funkhaus / ButtonDropdownSearch",
+    component: ButtonDropdownSearch,
+    argTypes: {
+        modelValue: { control: "text" },
+        options: { control: "array" },
+    },
+}
+
+const Template = (args) => ({
+    components: { ButtonDropdownSearch },
+    setup() {
+        const selected = ref(args.modelValue)
+        return { args, selected }
+    },
+    provide() {
+        return {
+            theme: computed(() => "dlc"),
+        }
+    },
+    template: `
+    <ButtonDropdownSearch
+      v-bind="args"
+      v-model="selected"
+    />
+    <div style="margin-top: 1em;">Selected: {{ selected }}</div>
+  `,
+})
+
+export const Default = Template.bind({})
+Default.args = {
+    options: ["All Fields", "Title", "Subject"],
+    modelValue: "All Fields",
+}
+
+export const ManyOptions = Template.bind({})
+ManyOptions.args = {
+    options: [
+        "All Fields",
+        "Title",
+        "Subject",
+        "Author",
+        "ISBN",
+        "Publisher",
+        "Year",
+        "Language",
+        "Format",
+    ],
+    modelValue: "All Fields",
+}
+
+export const SubjectDefault = Template.bind({})
+SubjectDefault.args = {
+    options: ["All Fields", "Title", "Subject"],
+    modelValue: "Subject",
+}
