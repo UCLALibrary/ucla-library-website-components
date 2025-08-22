e74e9604190157ef22f708f3e012726d83441c33
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-08-22 18:28:50 +0200

    Updates component to use the dlc theme

--- packages/vue-component-library/src/lib-components/SearchResultsCount.vue
+++ packages/vue-component-library/src/lib-components/SearchResultsCount.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref, computed } from "vue"
+import { useTheme } from "@/composables/useTheme"
+
+const theme = useTheme()
+
 type SearchResultsCountProps = {
     count: number
     label: string
@@ -22,6 +26,10 @@ const parsedResults = computed(() => {
         return props.label
     }
     return ""
+})
+
+const classes = computed(() => {
+    return ["search-results-count", theme?.value || ""]
 })
 
 const animatedCount = ref(0)
@@ -69,7 +77,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="search-results-count">
+    <div :class="classes">
         <span class="parsed-results">
             {{ parsedResults }}
         </span>
