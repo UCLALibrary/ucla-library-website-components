27bab09bf338bb289bc99dd076553b95b65c05f7
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-08-25 15:45:17 +0200

    Adds a prop for a longer field.

--- packages/vue-component-library/src/stories/ButtonDropdownSearch.stories.js
+++ packages/vue-component-library/src/stories/ButtonDropdownSearch.stories.js
@@ -1,61 +1,68 @@
-import { computed, ref } from 'vue'
-import ButtonDropdownSearch from '@/lib-components/ButtonDropdownSearch.vue'
+import { computed, ref } from "vue"
+import ButtonDropdownSearch from "@/lib-components/ButtonDropdownSearch.vue"
 
 export default {
-  title: 'Funkhaus / ButtonDropdownSearch',
-  component: ButtonDropdownSearch,
-  argTypes: {
-    modelValue: { control: 'text' },
-    options: { control: 'array' },
-  },
+    title: "Funkhaus / ButtonDropdownSearch",
+    component: ButtonDropdownSearch,
+    argTypes: {
+        modelValue: { control: "text" },
+        options: { control: "array" },
+    },
 }
 
 function Template(args) {
-  return {
-    components: { ButtonDropdownSearch },
-    setup() {
-      const selected = ref(args.modelValue)
-      return { args, selected }
-    },
-    provide() {
-      return {
-        theme: computed(() => 'dlc'),
-      }
-    },
-    template: `
+    return {
+        components: { ButtonDropdownSearch },
+        provide() {
+            return {
+                theme: computed(() => "dlc"),
+            }
+        },
+        setup() {
+            const selected = ref(args.modelValue)
+            return { args, selected }
+        },
+        template: `
     <ButtonDropdownSearch
       v-bind="args"
       v-model="selected"
     />
     <div style="margin-top: 1em;">Selected: {{ selected }}</div>
   `,
-  }
+    }
 }
 
 export const Default = Template.bind({})
 Default.args = {
-  options: ['All Fields', 'Title', 'Subject'],
-  modelValue: 'All Fields',
+    options: ["All Fields", "Title", "Subject"],
+    modelValue: "All Fields",
 }
 
 export const ManyOptions = Template.bind({})
 ManyOptions.args = {
-  options: [
-    'All Fields',
-    'Title',
-    'Subject',
-    'Author',
-    'ISBN',
-    'Publisher',
-    'Year',
-    'Language',
-    'Format',
-  ],
-  modelValue: 'All Fields',
+    options: [
+        "All Fields",
+        "Title",
+        "Subject",
+        "Author",
+        "ISBN",
+        "Publisher",
+        "Year",
+        "Language",
+        "Format",
+    ],
+    modelValue: "All Fields",
 }
 
 export const SubjectDefault = Template.bind({})
 SubjectDefault.args = {
-  options: ['All Fields', 'Title', 'Subject'],
-  modelValue: 'Subject',
+    options: ["All Fields", "Title", "Subject"],
+    modelValue: "Subject",
+}
+
+export const IsLong = Template.bind({})
+IsLong.args = {
+    options: ["All Fields", "Title", "Subject"],
+    modelValue: "All Fields",
+    isLong: true,
 }
