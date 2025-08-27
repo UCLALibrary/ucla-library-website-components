7b51bd9b46420bad98ab534de6b86f09d6c646e0
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-08-27 11:20:54 +0200

    run fix lint

--- packages/vue-component-library/src/lib-components/BentoPod.vue
+++ packages/vue-component-library/src/lib-components/BentoPod.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
 // Props
-type BentoPodProps = {
-    title: string
-    description: string
-    buttonLabel?: string
-    buttonLink?: string
-    items: Array<Record<string, any>>
-    labelOpen?: string
-    labelClose?: string
+// Imports
+import { computed, nextTick, onMounted, onUnmounted, ref } from 'vue'
+import SvgExternalLink from 'ucla-library-design-tokens/assets/svgs/icon-external-link.svg'
+import SvgArrowDown from 'ucla-library-design-tokens/assets/svgs/icon-caret-down.svg'
+import DividerGeneral from './DividerGeneral.vue'
+import SmartLink from '@/lib-components/SmartLink.vue'
+import { useTheme } from '@/composables/useTheme'
+
+interface BentoPodProps {
+  title: string
+  description: string
+  buttonLabel?: string
+  buttonLink?: string
+  items: Array<Record<string, any>>
+  labelOpen?: string
+  labelClose?: string
 }
 const props = withDefaults(defineProps<BentoPodProps>(), {
-    title: "",
-    description: "",
-    buttonLabel: "",
-    buttonLink: "",
-    items: () => [],
-    labelOpen: "Less",
-    labelClose: "More",
+  title: '',
+  description: '',
+  buttonLabel: '',
+  buttonLink: '',
+  items: () => [],
+  labelOpen: 'Less',
+  labelClose: 'More',
 })
-
-// Imports
-import { ref, computed, onMounted, onUnmounted, nextTick } from "vue"
-import SmartLink from "@/lib-components/SmartLink.vue"
-import DividerGeneral from "./DividerGeneral.vue"
-import SvgExternalLink from "ucla-library-design-tokens/assets/svgs/icon-external-link.svg"
-import SvgArrowDown from "ucla-library-design-tokens/assets/svgs/icon-caret-down.svg"
-import { useTheme } from "@/composables/useTheme"
 
 const theme = useTheme()
 
@@ -38,15 +38,15 @@ const debounceTimeout = ref<number | undefined>(undefined)
 
 // Computed
 const classes = computed(() => [
-    "bento-pod",
-    theme?.value || "",
-    {
-        "is-expanded": isExpanded.value,
-    },
+  'bento-pod',
+  theme?.value || '',
+  {
+    'is-expanded': isExpanded.value,
+  },
 ])
 
 const dynamicLabel = computed(() =>
-    isExpanded.value ? props.labelOpen : props.labelClose
+  isExpanded.value ? props.labelOpen : props.labelClose
 )
 
 const firstItems = computed(() => props.items.slice(0, 3))
@@ -55,147 +55,148 @@ const extraItems = computed(() => props.items.slice(3))
 const showButton = computed(() => extraItems.value.length > 0)
 
 const wrapperStyles = computed(() => ({
-    height: isExpanded.value ? `${expandedHeight.value}px` : "0px",
-    overflow: "hidden",
-    transition: "height 0.3s",
+  height: isExpanded.value ? `${expandedHeight.value}px` : '0px',
+  overflow: 'hidden',
+  transition: 'height 0.3s',
 }))
 
 // Methods
 function measureHeights() {
-    nextTick(() => {
-        // Only measure the expandable section
-        let expanded = 0
-        for (let i = 0; i < itemRefs.value.length; i++) {
-            const el = itemRefs.value[i]
-            if (el) expanded += el.getBoundingClientRect().height
-        }
-        expandedHeight.value = expanded
-    })
+  nextTick(() => {
+    // Only measure the expandable section
+    let expanded = 0
+    for (let i = 0; i < itemRefs.value.length; i++) {
+      const el = itemRefs.value[i]
+      if (el)
+        expanded += el.getBoundingClientRect().height
+    }
+    expandedHeight.value = expanded
+  })
 }
 
 function debounceMeasureHeights() {
-    clearTimeout(debounceTimeout.value)
-    debounceTimeout.value = window.setTimeout(() => {
-        measureHeights()
-    }, 100)
+  clearTimeout(debounceTimeout.value)
+  debounceTimeout.value = window.setTimeout(() => {
+    measureHeights()
+  }, 100)
 }
 
 function toggle() {
-    isExpanded.value = !isExpanded.value
-    debounceMeasureHeights()
+  isExpanded.value = !isExpanded.value
+  debounceMeasureHeights()
 }
 
 function getItemRef(idx: number) {
-    return (el: HTMLElement | null) => setItemRef(idx, el)
+  return (el: HTMLElement | null) => setItemRef(idx, el)
 }
 function setItemRef(idx: number, el: HTMLElement | null) {
-    itemRefs.value[idx] = el
+  itemRefs.value[idx] = el
 }
 
 // Lifecycle Hooks
 onMounted(() => {
-    debounceMeasureHeights()
-    window.addEventListener("resize", debounceMeasureHeights)
+  debounceMeasureHeights()
+  window.addEventListener('resize', debounceMeasureHeights)
 })
 onUnmounted(() => {
-    window.removeEventListener("resize", debounceMeasureHeights)
+  window.removeEventListener('resize', debounceMeasureHeights)
 })
 </script>
 
 <template>
-    <div :class="classes">
-        <h4 class="title" v-html="title" v-if="title" />
-        <div class="description" v-html="description" v-if="description" />
+  <div :class="classes">
+    <h4 v-if="title" class="title" v-html="title" />
+    <div v-if="description" class="description" v-html="description" />
 
-        <smart-link
-            class="pod-button"
-            :to="buttonLink"
-            v-if="buttonLabel && buttonLink"
+    <SmartLink
+      v-if="buttonLabel && buttonLink"
+      class="pod-button"
+      :to="buttonLink"
+    >
+      <span class="button-label underline-hover" v-html="buttonLabel" />
+      <SvgExternalLink aria-hidden="true" />
+    </SmartLink>
+
+    <div>
+      <!-- Always visible first 3 items -->
+      <div class="items">
+        <div
+          v-for="(item, index) in firstItems"
+          :key="item.title ? item.title + index : index"
+          class="item"
         >
-            <span class="button-label underline-hover" v-html="buttonLabel" />
-            <SvgExternalLink aria-hidden="true" />
-        </smart-link>
-
-        <div>
-            <!-- Always visible first 3 items -->
-            <div class="items">
-                <div
-                    class="item"
-                    v-for="(item, index) in firstItems"
-                    :key="item.title ? item.title + index : index"
-                >
-                    <smart-link v-if="item.to" :to="item.to" class="item-link">
-                        <h5
-                            v-if="item.title"
-                            class="item-title"
-                            v-html="item.title"
-                        />
-                    </smart-link>
-                    <h5
-                        v-else-if="item.title"
-                        class="item-title"
-                        v-html="item.title"
-                    />
-                    <div class="item-details">
-                        <span
-                            v-for="(value, key) in item.meta"
-                            :key="key"
-                            :class="'item-' + key"
-                        >
-                            <strong>{{ key }}:</strong>
-                            <span class="detail" v-html="value" />
-                        </span>
-                    </div>
-                    <DividerGeneral class="divider" />
-                </div>
-            </div>
-            <!-- Expandable extra items -->
-            <div class="items" :style="wrapperStyles" ref="itemsWrapper">
-                <div
-                    class="item extra"
-                    v-for="(item, index) in extraItems"
-                    :key="item.title ? item.title + (index + 3) : index + 3"
-                    :ref="(el) => getItemRef(index)(el as HTMLElement | null)"
-                >
-                    <smart-link v-if="item.to" :to="item.to" class="item-link">
-                        <h5
-                            v-if="item.title"
-                            class="item-title"
-                            v-html="item.title"
-                        />
-                    </smart-link>
-                    <h5
-                        v-else-if="item.title"
-                        class="item-title"
-                        v-html="item.title"
-                    />
-                    <div class="item-details">
-                        <span
-                            v-for="(value, key) in item.meta"
-                            :key="key"
-                            :class="'item-' + key"
-                        >
-                            <strong>{{ key }}:</strong>
-                            <span class="detail" v-html="value" />
-                        </span>
-                    </div>
-                    <DividerGeneral class="divider" />
-                </div>
-            </div>
+          <SmartLink v-if="item.to" :to="item.to" class="item-link">
+            <h5
+              v-if="item.title"
+              class="item-title"
+              v-html="item.title"
+            />
+          </SmartLink>
+          <h5
+            v-else-if="item.title"
+            class="item-title"
+            v-html="item.title"
+          />
+          <div class="item-details">
+            <span
+              v-for="(value, key) in item.meta"
+              :key="key"
+              :class="`item-${key}`"
+            >
+              <strong>{{ key }}:</strong>
+              <span class="detail" v-html="value" />
+            </span>
+          </div>
+          <DividerGeneral class="divider" />
         </div>
-
-        <button
-            v-if="showButton"
-            class="btn"
-            :aria-expanded="isExpanded"
-            @click="toggle"
+      </div>
+      <!-- Expandable extra items -->
+      <div ref="itemsWrapper" class="items" :style="wrapperStyles">
+        <div
+          v-for="(item, index) in extraItems"
+          :key="item.title ? item.title + (index + 3) : index + 3"
+          :ref="(el) => getItemRef(index)(el as HTMLElement | null)"
+          class="item extra"
         >
-            <transition name="fade-label" mode="out-in">
-                <span :key="dynamicLabel" class="label" v-html="dynamicLabel" />
-            </transition>
-            <SvgArrowDown aria-hidden="true" class="caret-icon" />
-        </button>
+          <SmartLink v-if="item.to" :to="item.to" class="item-link">
+            <h5
+              v-if="item.title"
+              class="item-title"
+              v-html="item.title"
+            />
+          </SmartLink>
+          <h5
+            v-else-if="item.title"
+            class="item-title"
+            v-html="item.title"
+          />
+          <div class="item-details">
+            <span
+              v-for="(value, key) in item.meta"
+              :key="key"
+              :class="`item-${key}`"
+            >
+              <strong>{{ key }}:</strong>
+              <span class="detail" v-html="value" />
+            </span>
+          </div>
+          <DividerGeneral class="divider" />
+        </div>
+      </div>
     </div>
+
+    <button
+      v-if="showButton"
+      class="btn"
+      :aria-expanded="isExpanded"
+      @click="toggle"
+    >
+      <transition name="fade-label" mode="out-in">
+        <span :key="dynamicLabel" class="label" v-html="dynamicLabel" />
+      </transition>
+      <SvgArrowDown aria-hidden="true" class="caret-icon" />
+    </button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
--- packages/vue-component-library/src/stories/BentoPod.stories.js
+++ packages/vue-component-library/src/stories/BentoPod.stories.js
@@ -1,246 +1,248 @@
-import BentoPod from "@/lib-components/BentoPod.vue"
-import { computed } from "vue"
+import { computed } from 'vue'
+import BentoPod from '@/lib-components/BentoPod.vue'
 
 export default {
-    title: "Funkhaus / BentoPod",
-    component: BentoPod,
-    argTypes: {
-        title: { control: "text" },
-        description: { control: "text" },
-        buttonLabel: { control: "text" },
-        buttonLink: { control: "text" },
-        labelOpen: { control: "text" },
-        labelClose: { control: "text" },
-        items: { control: "object" },
-    },
+  title: 'Funkhaus / BentoPod',
+  component: BentoPod,
+  argTypes: {
+    title: { control: 'text' },
+    description: { control: 'text' },
+    buttonLabel: { control: 'text' },
+    buttonLink: { control: 'text' },
+    labelOpen: { control: 'text' },
+    labelClose: { control: 'text' },
+    items: { control: 'object' },
+  },
 }
 
-const Template = (args) => ({
+function Template(args) {
+  return {
     components: { BentoPod },
     provide() {
-        return {
-            theme: computed(() => "dlc"),
-        }
+      return {
+        theme: computed(() => 'dlc'),
+      }
     },
     setup() {
-        return { args }
+      return { args }
     },
     template: '<BentoPod v-bind="args" />',
-})
+  }
+}
 
 export const Default = Template.bind({})
 Default.args = {
-    title: "Bento Pod Title",
-    description: "This is a description for the Bento Pod.",
-    buttonLabel: "Go to Link",
-    buttonLink: "/somewhere",
-    labelOpen: "Show Less",
-    labelClose: "Show More",
-    items: [
-        {
-            title: "Item 1",
-            to: "/item-1",
-            meta: {
-                type: "Type A",
-                date: "2025-07-08",
-                program: "Program X",
-            },
-        },
-        {
-            title: "Item 2",
-            to: "/item-2",
-            meta: {
-                type: "Type B",
-                date: "2025-07-09",
-                program: "Program Y",
-            },
-        },
-        {
-            title: "Item 3",
-            to: "/item-3",
-            meta: {
-                type: "Type C",
-                date: "2025-07-10",
-                program: "Program Z",
-            },
-        },
-        {
-            title: "Item 4",
-            to: "/item-4",
-            meta: {
-                type: "Type D",
-                date: "2025-07-11",
-                program: "Program W",
-            },
-        },
-        {
-            title: "Item 5",
-            to: "/item-5",
-            meta: {
-                type: "Type E",
-                date: "2025-07-12",
-                program: "Program V",
-            },
-        },
-    ],
+  title: 'Bento Pod Title',
+  description: 'This is a description for the Bento Pod.',
+  buttonLabel: 'Go to Link',
+  buttonLink: '/somewhere',
+  labelOpen: 'Show Less',
+  labelClose: 'Show More',
+  items: [
+    {
+      title: 'Item 1',
+      to: '/item-1',
+      meta: {
+        type: 'Type A',
+        date: '2025-07-08',
+        program: 'Program X',
+      },
+    },
+    {
+      title: 'Item 2',
+      to: '/item-2',
+      meta: {
+        type: 'Type B',
+        date: '2025-07-09',
+        program: 'Program Y',
+      },
+    },
+    {
+      title: 'Item 3',
+      to: '/item-3',
+      meta: {
+        type: 'Type C',
+        date: '2025-07-10',
+        program: 'Program Z',
+      },
+    },
+    {
+      title: 'Item 4',
+      to: '/item-4',
+      meta: {
+        type: 'Type D',
+        date: '2025-07-11',
+        program: 'Program W',
+      },
+    },
+    {
+      title: 'Item 5',
+      to: '/item-5',
+      meta: {
+        type: 'Type E',
+        date: '2025-07-12',
+        program: 'Program V',
+      },
+    },
+  ],
 }
 
 // Story: Some items missing data
 export const ItemsWithMissingData = Template.bind({})
 ItemsWithMissingData.args = {
-    ...Default.args,
-    items: [
-        {
-            title: "Item with only title",
-            meta: {},
-        },
-        {
-            meta: {
-                type: "Type and date Only",
-                date: "2025-08-01",
-            },
-        },
-        {
-            title: "Item with link",
-            to: "/item-link",
-            meta: {},
-        },
-        {
-            title: "Full Item",
-            to: "/item-full",
-            meta: {
-                type: "Type F",
-                date: "2025-08-02",
-                program: "Program Q",
-            },
-        },
-    ],
+  ...Default.args,
+  items: [
+    {
+      title: 'Item with only title',
+      meta: {},
+    },
+    {
+      meta: {
+        type: 'Type and date Only',
+        date: '2025-08-01',
+      },
+    },
+    {
+      title: 'Item with link',
+      to: '/item-link',
+      meta: {},
+    },
+    {
+      title: 'Full Item',
+      to: '/item-full',
+      meta: {
+        type: 'Type F',
+        date: '2025-08-02',
+        program: 'Program Q',
+      },
+    },
+  ],
 }
 
 export const LessThanThreeItems = Template.bind({})
 LessThanThreeItems.args = {
-    ...Default.args,
-    items: [
-        {
-            title: "Item 1",
-            meta: {
-                type: "Type A",
-                date: "2025-07-08",
-                program: "Program X",
-            },
-        },
-        {
-            title: "Item 2",
-            meta: {
-                type: "Type B",
-                date: "2025-07-09",
-                program: "Program Y",
-            },
-        },
-    ],
+  ...Default.args,
+  items: [
+    {
+      title: 'Item 1',
+      meta: {
+        type: 'Type A',
+        date: '2025-07-08',
+        program: 'Program X',
+      },
+    },
+    {
+      title: 'Item 2',
+      meta: {
+        type: 'Type B',
+        date: '2025-07-09',
+        program: 'Program Y',
+      },
+    },
+  ],
 }
 
 export const ExactlyThreeItems = Template.bind({})
 ExactlyThreeItems.args = {
-    ...Default.args,
-    items: [
-        {
-            title: "Item 1",
-            meta: {
-                type: "Type A",
-                date: "2025-07-08",
-                program: "Program X",
-            },
-        },
-        {
-            title: "Item 2",
-            meta: {
-                type: "Type B",
-                date: "2025-07-09",
-                program: "Program Y",
-            },
-        },
-        {
-            title: "Item 3",
-            meta: {
-                type: "Type C",
-                date: "2025-07-10",
-                program: "Program Z",
-            },
-        },
-    ],
+  ...Default.args,
+  items: [
+    {
+      title: 'Item 1',
+      meta: {
+        type: 'Type A',
+        date: '2025-07-08',
+        program: 'Program X',
+      },
+    },
+    {
+      title: 'Item 2',
+      meta: {
+        type: 'Type B',
+        date: '2025-07-09',
+        program: 'Program Y',
+      },
+    },
+    {
+      title: 'Item 3',
+      meta: {
+        type: 'Type C',
+        date: '2025-07-10',
+        program: 'Program Z',
+      },
+    },
+  ],
 }
 
 export const NoItems = Template.bind({})
 NoItems.args = {
-    ...Default.args,
-    items: [],
+  ...Default.args,
+  items: [],
 }
 
 export const CustomLabels = Template.bind({})
 CustomLabels.args = {
-    ...Default.args,
-    labelOpen: "Collapse",
-    labelClose: "Expand",
+  ...Default.args,
+  labelOpen: 'Collapse',
+  labelClose: 'Expand',
 }
 
 export const RealContent = Template.bind({})
 RealContent.args = {
-    title: "UCLA Library Special Collections",
-    description:
-        "Explore a curated selection of rare books, manuscripts, photographs, and archives from the UCLA Library. These resources are available for research, teaching, and learning.",
-    buttonLabel: "Browse All Collections",
-    buttonLink: "/collections",
-    labelOpen: "Show Fewer Items",
-    labelClose: "Show More Items",
-    items: [
-        {
-            title: "Los Angeles Times Photographic Archive",
-            meta: {
-                type: "Photographs",
-                date: "January 01, 2039",
-                program: "News & Media",
-            },
-        },
-        {
-            title: "Charles E. Young Research Library",
-            meta: {
-                type: "Library",
-                date: "February 15, 2039",
-                program: "Research",
-            },
-        },
-        {
-            title: "Oral History Collection",
-            meta: {
-                type: "Audio",
-                date: "March 10, 2039",
-                program: "History",
-            },
-        },
-        {
-            title: "Medieval Manuscripts",
-            meta: {
-                type: "Manuscripts",
-                date: "April 22, 2039",
-                program: "Rare Books",
-            },
-        },
-        {
-            title: "LGBTQIA+ Activism Papers",
-            meta: {
-                type: "Archives",
-                date: "May 30, 2039",
-                program: "Social Movements",
-            },
-        },
-        {
-            title: "Japanese American Incarceration Materials",
-            meta: {
-                type: "Documents",
-                date: "June 18, 2039",
-                program: "History",
-            },
-        },
-    ],
+  title: 'UCLA Library Special Collections',
+  description:
+        'Explore a curated selection of rare books, manuscripts, photographs, and archives from the UCLA Library. These resources are available for research, teaching, and learning.',
+  buttonLabel: 'Browse All Collections',
+  buttonLink: '/collections',
+  labelOpen: 'Show Fewer Items',
+  labelClose: 'Show More Items',
+  items: [
+    {
+      title: 'Los Angeles Times Photographic Archive',
+      meta: {
+        type: 'Photographs',
+        date: 'January 01, 2039',
+        program: 'News & Media',
+      },
+    },
+    {
+      title: 'Charles E. Young Research Library',
+      meta: {
+        type: 'Library',
+        date: 'February 15, 2039',
+        program: 'Research',
+      },
+    },
+    {
+      title: 'Oral History Collection',
+      meta: {
+        type: 'Audio',
+        date: 'March 10, 2039',
+        program: 'History',
+      },
+    },
+    {
+      title: 'Medieval Manuscripts',
+      meta: {
+        type: 'Manuscripts',
+        date: 'April 22, 2039',
+        program: 'Rare Books',
+      },
+    },
+    {
+      title: 'LGBTQIA+ Activism Papers',
+      meta: {
+        type: 'Archives',
+        date: 'May 30, 2039',
+        program: 'Social Movements',
+      },
+    },
+    {
+      title: 'Japanese American Incarceration Materials',
+      meta: {
+        type: 'Documents',
+        date: 'June 18, 2039',
+        program: 'History',
+      },
+    },
+  ],
 }
