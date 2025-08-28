2954cb352eb2352ed20b1f8e84a519aeda531325
Luka Vuk <luka.vuk0909@gmail.com>
2025-08-28 13:45:40 +0200

    lint fix

--- packages/vue-component-library/src/stories/ButtonSubmit.stories.js
+++ packages/vue-component-library/src/stories/ButtonSubmit.stories.js
@@ -1,52 +1,61 @@
-import ButtonSubmit from "@/lib-components/ButtonSubmit"
-import SvgIconLocker from "ucla-library-design-tokens/assets/svgs/icon-locker.svg"
-import { computed } from "vue"
+import SvgIconEye from 'ucla-library-design-tokens/assets/svgs/icon-eye.svg'
+import { computed } from 'vue'
+import ButtonSubmit from '@/lib-components/ButtonSubmit'
 
 export default {
-    title: "Funkhaus / ButtonSubmit",
-    component: ButtonSubmit,
+  title: 'Funkhaus / ButtonSubmit',
+  component: ButtonSubmit,
 }
 
 const methods = {
-    onSubmit() {
-        alert("Button clicked!")
-    },
+  onSubmit() {
+    alert('Button clicked!')
+  },
 }
 
 // Variations of stories below
 export function Default() {
-    return {
-        components: { ButtonSubmit },
-        provide() {
-            return {
-                theme: computed(() => "dlc"),
-            }
-        },
-        methods: methods,
-        template: `<button-submit @click="onSubmit" />`,
-    }
+  return {
+    components: { ButtonSubmit },
+    provide() {
+      return {
+        theme: computed(() => 'dlc'),
+      }
+    },
+    methods,
+    template: '<button-submit @click="onSubmit" />',
+  }
 }
 
 export function WithCustomIcon() {
-    return {
-        components: { ButtonSubmit },
-        provide() {
-            return {
-                theme: computed(() => "dlc"),
-            }
-        },
-        data() {
-            return {
-                customIcon: SvgIconLocker,
-            }
-        },
-        methods: methods,
-        template: `
-      <button-submit @click="onSubmit">
-        <template #default>
-          <component :is="customIcon" />
-        </template>
-      </button-submit>
-    `,
-    }
+  return {
+    components: { ButtonSubmit },
+    provide() {
+      return {
+        theme: computed(() => 'dlc'),
+      }
+    },
+    data() {
+      return {
+        customIcon: SvgIconEye,
+      }
+    },
+    methods,
+    template: `
+            <button-submit @click="onSubmit">
+                <template #default>
+                    <component :is="customIcon" />
+                </template>
+            </button-submit>
+        `,
+  }
+}
+
+// Variant: No theme provided (falls back to base styles)
+export function NoThemeProvided() {
+  return {
+    components: { ButtonSubmit },
+    methods,
+    template: '<button-submit @click="onSubmit" />',
+  }
 }
