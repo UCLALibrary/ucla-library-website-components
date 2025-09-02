b0166ebfc9669f53b17e77a11eaeecbc993ab8c9
Luka Vuk <luka.vuk0909@gmail.com>
2025-09-02 20:17:09 +0200

    remove button submit files

--- packages/vue-component-library/src/stories/ButtonSubmit.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Funkhaus / ButtonSubmit', () => {
-  it('Default', () => {
-    cy.visit('/iframe.html?id=button-submit--default&args=&viewMode=story')
-    cy.get('.button-submit').should('exist')
-
-    cy.percySnapshot('BUTTON / Submit: Default')
-  })
-})
