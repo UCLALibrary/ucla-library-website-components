c0befd57de3f5bb47ec892fca6494b9097a642d3
Luka Vuk <luka.vuk0909@gmail.com>
2025-09-02 20:33:35 +0200

    merge entry field & button submit

--- packages/vue-component-library/src/stories/SearchField.spec.js
+++ packages/vue-component-library/src/stories/SearchField.spec.js
@@ -1,6 +1,6 @@
 describe('Funkhaus / Search Field', () => {
   it('Default', () => {
-    cy.visit('/iframe.html?id=search-field--default&args=&viewMode=story')
+    cy.visit('/iframe.html?id=funkhaus-search-field--default&args=&viewMode=story')
     cy.get('.search-field').should('exist')
     cy.percySnapshot('Funkhaus / Search Field')
   })
