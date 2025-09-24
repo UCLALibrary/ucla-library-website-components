499ad28498ffc7862773d83451f8d9ce945be49d
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-09-24 15:16:07 +0200

    feat: crrates the header main funkhaus component

---
+++ packages/vue-component-library/src/lib-components/GlobalHamburger.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    :class="classes"
+    @click="onClick"
+  >
+    <div class="hamburger-lines">
+      <span class="line line-1" />
+      <span class="line line-2" />
+      <span class="line line-3" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useTheme } from '@/composables/useTheme'
+
+// Data
+const theme = useTheme()
+
+// Props
+type GlobalHamburgerProps = {
+  isOpened: boolean
+}
+
+const props = defineProps<GlobalHamburgerProps>()
+
+// Emits
+const emit = defineEmits(['toggle-menu'])
+
+// Computed
+const classes = computed(() => {
+  return [
+    'global-hamburger',
+    theme?.value || '',
+    {
+      'is-opened': props.isOpened
+    }
+  ]
+})
+
+// Methods
+const onClick = () => {
+  emit('toggle-menu')
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/dlc/_global-hamburger.scss";
+</style>
---
+++ packages/vue-component-library/src/lib-components/HeaderMainFunkhaus.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+// Imports
+import SvgLibraryLogo from 'ucla-library-design-tokens/assets/svgs/logo-library-digital-collections.svg'
+import SmartLink from '@/lib-components/SmartLink.vue'
+import GlobalHamburger from '@/lib-components/GlobalHamburger.vue'
+
+import { useTheme } from '@/composables/useTheme'
+import { computed } from 'vue'
+
+// Props
+type HeaderMainFunkhausProps = {
+  menuOpened: boolean
+}
+const props = defineProps<HeaderMainFunkhausProps>()
+
+// Emits
+const emit = defineEmits(['toggle-menu'])
+
+// Data
+const theme = useTheme()
+
+// Computed
+const classes = computed(() => {
+  return ['header-main-funkahus', theme?.value || '', { 'menu-opened': props.menuOpened }]
+})
+
+// Methods
+const toggleMenu = () => {
+  emit('toggle-menu')
+}
+
+// Watchers
+
+// Lifecycle Hooks
+</script>
+
+<template>
+  <div :class="classes">
+    <SmartLink
+      to="/"
+      class="ucla-dlc-logo-link"
+    >
+      <SvgLibraryLogo
+        class="ucla-dlc-logo"
+        alt="UCLA Library Digital Collections logo"
+      />
+    </SmartLink>
+
+    <div class="header-links show-desktop">
+      <slot name="header-links" />
+    </div>
+
+    <GlobalHamburger
+      class="hamburger show-mobile"
+      :is-opened="menuOpened"
+      @toggle-menu="toggleMenu"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/styles/dlc/_header-main-funkahaus.scss";
+</style>
