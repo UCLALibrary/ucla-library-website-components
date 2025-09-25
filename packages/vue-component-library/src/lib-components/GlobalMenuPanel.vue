60b1a477b3d0b37c327b706be43d0c087e37cec4
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-09-25 18:11:21 +0200

    WIP

---
+++ packages/vue-component-library/src/lib-components/GlobalMenuPanel.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// Imports
+
+// Data
+
+// Computed
+
+// Methods
+
+// Watchers
+
+// Lifecycle Hooks
+</script>
+
+<template>
+  <div class="global-menu-panel"></div>
+</template>
+
+<style lang="scss" scoped>
+.global-menu-panel {
+
+  // Hovers
+  @media #{$has-hover} {}
+
+  // Breakpoints
+  @media #{$extra-large} {}
+
+  @media #{$medium} {}
+
+  @media #{$small} {}
+}
+</style>
