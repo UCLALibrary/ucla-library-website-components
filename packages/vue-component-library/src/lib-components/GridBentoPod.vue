0c6dc766ae9c0ca32b13be56f09a47b18cdcca68
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-10-01 14:07:10 +0200

    Adding grid bento pod

---
+++ packages/vue-component-library/src/lib-components/GridBentoPod.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+// Imports
+import { computed } from 'vue'
+import BentoPod from './BentoPod.vue'
+import { useTheme } from '@/composables/useTheme'
+
+// Props
+type GridBentoPodProps = {
+  items: Array<{
+    id: string
+    title: string
+    description: string
+    items: Array<Record<string, any>>
+  }>
+}
+defineProps<GridBentoPodProps>()
+
+// Data
+const { theme } = useTheme()
+// Computed
+const classes = computed(() => {
+  return ['grid-bento-pod', theme?.value || '']
+})
+// Methods
+
+// Watchers
+
+// Lifecycle Hooks
+</script>
+
+<template>
+  <div :class="classes">
+    <BentoPod
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+      :description="item.description"
+      :items="item.items"
+      class="bento-pod"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/styles/dlc/_grid-bento-pod.scss";
+</style>
