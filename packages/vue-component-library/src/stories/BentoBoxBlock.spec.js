350d3b676e972d1268a51e949ac23b075725155a
Svebor Sorić <74537302+Sveb@users.noreply.github.com>
2025-11-10 17:58:17 +0100

    prepare for PR

--- packages/vue-component-library/src/stories/BentoBoxBlock.spec.js
+++ packages/vue-component-library/src/stories/BentoBoxBlock.spec.js
@@ -4,34 +4,4 @@ describe('BentoBoxBlock Storybook', () => {
     cy.get('.bento-box-block').should('exist')
     cy.contains('Default Bento Block')
   })
-
-  it('No Image', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-block--no-image&viewMode=story')
-    cy.get('.bento-box-block').should('exist')
-    cy.contains('No Image Bento Block')
-  })
-
-  it('Long Text', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-block--long-text&viewMode=story')
-    cy.get('.bento-box-block').should('exist')
-    cy.contains('Bento Block With Very Long Title')
-  })
-
-  it('No Title', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-block--no-title&viewMode=story')
-    cy.get('.bento-box-block').should('exist')
-    cy.contains('Bento block with no title.')
-  })
-
-  it('No Summary', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-block--no-summary&viewMode=story')
-    cy.get('.bento-box-block').should('exist')
-    cy.contains('No Summary Bento Block')
-  })
-
-  it('No Link', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-block--no-link&viewMode=story')
-    cy.get('.bento-box-block').should('exist')
-    cy.contains('No Link Bento Block')
-  })
 })
--- packages/vue-component-library/src/stories/BentoBoxResult.spec.js
+++ packages/vue-component-library/src/stories/BentoBoxResult.spec.js
@@ -4,40 +4,4 @@ describe('BentoBoxResult Storybook', () => {
     cy.get('.bento-box-result').should('exist')
     cy.contains('Default Results')
   })
-
-  it('No Items', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-result--no-items&viewMode=story')
-    cy.get('.bento-box-result').should('exist')
-    cy.contains('No Results')
-  })
-
-  it('One Item', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-result--one-item&viewMode=story')
-    cy.get('.bento-box-result').should('exist')
-    cy.contains('One Result')
-  })
-
-  it('Long Text', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-result--long-text&viewMode=story')
-    cy.get('.bento-box-result').should('exist')
-    cy.contains('Long Text Result')
-  })
-
-  it('Missing Image', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-result--missing-image&viewMode=story')
-    cy.get('.bento-box-result').should('exist')
-    cy.contains('Missing Image')
-  })
-
-  it('Missing Count', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-result--missing-count&viewMode=story')
-    cy.get('.bento-box-result').should('exist')
-    cy.contains('Missing Count')
-  })
-
-  it('All Edge Cases', () => {
-    cy.visit('/iframe.html?id=funkhaus-bento-box-result--all-edge-cases&viewMode=story')
-    cy.get('.bento-box-result').should('exist')
-    cy.contains('All Edge Cases')
-  })
 })
