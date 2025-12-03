a4476b46de8c7944ad735bdd7df16b498d4cd549
Luka Vuk <luka.vuk0909@gmail.com>
2025-12-03 19:05:45 +0100

    feat: add panel anchor nav

---
+++ packages/vue-component-library/src/lib-components/PanelAnchorNav.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+// Imports
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue'
+import SvgIconClose from 'ucla-library-design-tokens/assets/svgs/icon-close.svg'
+import SvgIconFilter from 'ucla-library-design-tokens/assets/svgs/icon-dlc-filter.svg'
+import PageAnchor from './PageAnchor.vue'
+
+type routeColors = 'about' | 'help' | 'visit' | 'default' | ''
+
+interface PanelAnchorNavProps {
+  sectionTitles: string[]
+  color?: routeColors
+  hasBackToTop?: boolean
+  isOpened: boolean
+  showOpenIconAlways?: boolean
+}
+
+// Props
+const props = withDefaults(defineProps<PanelAnchorNavProps>(), {
+  color: '' as routeColors,
+  hasBackToTop: true,
+  isOpened: false,
+  showOpenIconAlways: true,
+})
+
+// Emits
+const emit = defineEmits(['closePanel', 'openPanel'])
+
+// Data
+const hasScrolled = ref(false)
+
+// Methods
+function openPanel() {
+  emit('openPanel')
+}
+function closePanel() {
+  emit('closePanel')
+}
+
+// Body scroll lock methods
+function lockBodyScroll() {
+  document.documentElement.style.overflow = 'hidden'
+}
+
+function unlockBodyScroll() {
+  document.documentElement.style.overflow = ''
+}
+
+// Scroll detection
+function handleScroll() {
+  if (!props.showOpenIconAlways) {
+    // Show open icon after scrolling 50% of the viewport height
+    hasScrolled.value = window.scrollY >= window.innerHeight * 0.5
+  }
+}
+
+// Watchers
+watch(
+  () => props.isOpened,
+  (isOpened) => {
+    if (isOpened)
+      lockBodyScroll()
+    else unlockBodyScroll()
+  }
+)
+
+// Lifecycle
+onMounted(() => {
+  if (!props.showOpenIconAlways) {
+    window.addEventListener('scroll', handleScroll)
+    handleScroll() // Check initial scroll position
+  }
+})
+
+onUnmounted(() => {
+  unlockBodyScroll()
+  if (!props.showOpenIconAlways)
+    window.removeEventListener('scroll', handleScroll)
+})
+
+// Computed
+const classes = computed(() => [
+  'panel-anchor-nav',
+  { 'is-opened': props.isOpened },
+  { 'show-open-icon': props.showOpenIconAlways || hasScrolled.value },
+])
+</script>
+
+<template>
+  <div :class="classes">
+    <SvgIconFilter class="icon-open" @click="openPanel" />
+
+    <div class="panel">
+      <PageAnchor
+        :section-titles="sectionTitles"
+        :color="color"
+        :has-back-to-top="hasBackToTop"
+        class="page-anchor"
+      />
+    </div>
+
+    <SvgIconClose class="icon-close" @click="closePanel" />
+    <div class="bg-color" @click="closePanel" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/styles/dlc/_panel-anchor-nav.scss";
+</style>
