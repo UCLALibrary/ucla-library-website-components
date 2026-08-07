b954827d0316ac548ef96480d7de6b3d914487d5
jendiamond <thejendiamond@gmail.com>
2026-08-07 16:45:41 -0700

    feat: mobile styles working on all browsers

--- packages/vue-component-library/src/lib-components/DateFilter.vue
+++ packages/vue-component-library/src/lib-components/DateFilter.vue
@@ -837,11 +837,11 @@ onMounted(() => {
       width: 166px;
       padding: 6px;
 
-      &:focus,
-      &:focus-visible {
-        outline: 2px solid ftvatokens.$accent-blue;
-        outline-offset: -2px;
-      }
+      // &:focus,
+      // &:focus-visible {
+      //   outline: 2px solid ftvatokens.$accent-blue;
+      //   outline-offset: -2px;
+      // }
 
       &:active {
         color: white;
