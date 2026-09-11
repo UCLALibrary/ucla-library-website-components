ce985d10242c8d10b12e59a4bb448ce92223f2f9
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2026-09-10 22:19:56 -0700

    feat: create helpers for YouTube url reformatting, oEmbed fetch call

---
+++ packages/vue-component-library/src/composables/useOEmbedFetch.ts
@@ -0,0 +1,40 @@
+/* 
+Explanation:
+
+*/
+
+import { onMounted, ref } from 'vue'
+
+type OEmbedResponse = {
+  title?: string
+}
+
+type YouTubeUrlObj = {
+  initialURL:string,
+  oEmbedURL: string,
+  videoTitle: string
+}
+
+export function useOEmbedFetch(urlObjs: YouTubeUrlObj[]) {
+
+  const data = ref<OEmbedResponse[] | null>(null)
+  const error = ref<Error | null>(null)
+
+  onMounted(async () => {
+    try {
+        data.value = await Promise.all(
+          urlObjs.map(async (obj) => {
+            const res = await fetch(`https://www.youtube.com/oembed?url=${obj.oEmbedURL}`)
+
+            if (!res.ok) throw new Error('Error fetching oEmbed data.')
+            
+            return res.json()
+          }),
+        )
+    } catch (err) {
+      error.value = err instanceof Error ? err : new Error('Error fetching oEmbed data.')
+    }
+  })
+
+  return { data, error }
+}
---
+++ packages/vue-component-library/src/utils/formatYouTubeUrlsForOembed.js
@@ -0,0 +1,29 @@
+/* 
+Explanation:
+
+URL input patterns:
+
+From Video Embed: [https://www.youtube.com/embed/uYr_SvIKKuI?si=ihenbmyE91KqyXK5]
+
+From Rich Text: ["//www.youtube.com/embed/RdAp0_iB7iU", "//www.youtube.com/embed/wYtQ3wH062c", ...]
+*/
+
+export default function formatYouTubeUrlsForOembed(urlArr) {
+  
+  const urlObjsArr = urlArr.map((url, index) => {
+    // Remove extra characters after '?' in url, inclusive 
+    const firstParse = url.replace(/\?.*$/, '')
+
+    // oEmbed only returns data with 'watch...' keyword
+    const secondParse = firstParse.replace('embed/', 'watch?v=')
+
+    return {
+        initialURL: url,
+        oEmbedURL: secondParse,
+        videoTitle: `YouTube Video Player ${index}` // Fallback title
+    }
+  })
+
+  return urlObjsArr
+
+}
