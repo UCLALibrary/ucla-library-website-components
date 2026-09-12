d576886c28709ecf7f6aeddf457aec888bb58bdc
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2026-09-11 19:49:15 -0700

    chore: linting

--- packages/vue-component-library/src/utils/escapeHtml.js
+++ packages/vue-component-library/src/utils/escapeHtml.js
@@ -3,7 +3,7 @@ Escapes HTML characters in content retrieved from external api
 
 Example: Tom & Jerry's "Great" Adventure
 
-"Great" will break when the whole string is used in an attribute: 
+"Great" will break when the whole string is used in an attribute:
 
 <iframe title="Tom & Jerry's "Great" Adventure" ...>
 
@@ -12,7 +12,7 @@ With this utility, the string gets escaped and is used as:
 <iframe title="Tom &amp; Jerry&#039;s &quot;Great&quot; Adventure" ...>
 */
 
-export default function escapeHtml (str) {
+export default function escapeHtml(str) {
   return str
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
