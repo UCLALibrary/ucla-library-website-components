3b6151433c79db7f0679da8c62dc635240cbf9ff
Tinuola 'Tinu' Awopetu <24995224+tinuola@users.noreply.github.com>
2026-09-22 18:55:39 -0700

    fix: LADI-5357 Update styling for Add-to-Calendar button (#986)
    * style: remove add-to-calendar dropdown offset
    
    * style: add ftva styling to add-to-calendar btn
    
    * fix: remove unneeded css important declaration
    
    * fix: add offset to button outline when focused
    
    * style: move calendar style to ftva scss

--- packages/vue-component-library/src/stories/ButtonDropdown.stories.js
+++ packages/vue-component-library/src/stories/ButtonDropdown.stories.js
@@ -75,7 +75,7 @@ Default.args = {
 }
 
 Default.parameters = {
-  chromatic: { disableSnapshot: false },
+  chromatic: { disableSnapshot: false }
 }
 
 const mockSocialList = {
@@ -182,8 +182,8 @@ const mockCalendarData = {
   eventDescription: '<p>Admission is free. No advance reservations. Your seat will be assigned to you when you pick up your ticket at the box office. Seats are assigned on a first come, first served basis. The box office opens one hour before the event.</p>\n<p>La Région Centrale</p>\n<p>Canada, 1971</p>\n<p>The late avant-garde master Michael Snow’s work explores the nature of perception, consciousness, participation, and in many ways is uncategorizable but relation to time is evident. He worked in many mediums and his films playfully and prolifically rethink the parameters while requiring audiences to actively participate in this work. La Région Centrale is a three-hour-long film that calls attention to the mechanics of filmmaking, using a computer-programmed, motorized tripod that rotates the camera 360 degrees in any direction, repeatedly, without dialogue or any other subject beyond the landscape, over the course of a day.'
 }
 
-export const AddToCalendar = TemplateAddToCalendar.bind({})
+export const FTVAAddToCalendar = TemplateAddToCalendar.bind({})
 
-AddToCalendar.args = {
+FTVAAddToCalendar.args = {
   ...mockCalendarData,
 }
